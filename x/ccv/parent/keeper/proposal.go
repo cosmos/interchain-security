686b325780df437b842dd9751edbb61cbb18234b
Jehan Tremback <hi@jehan.email>
2022-04-28 15:30:35 -0700

    fix params setting bug

--- x/ccv/parent/keeper/proposal.go
+++ x/ccv/parent/keeper/proposal.go
@@ -73,6 +73,8 @@ func (k Keeper) MakeChildGenesis(ctx sdk.Context) (gen childtypes.GenesisState, 
 		return gen, sdkerrors.Wrapf(clienttypes.ErrConsensusStateNotFound, "error %s getting self consensus state for: %s", err, height)
 	}
 
+	gen = *childtypes.DefaultGenesisState()
+
 	gen.Params.Enabled = true
 	gen.NewChain = true
 	gen.ParentClientState = clientState
