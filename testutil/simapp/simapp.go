5d761f2312928a15fe0eea43a1be0f00c877fedc
Daniel <danwtisdall@gmail.com>
2022-05-03 17:11:24 +0100

    Use distinct parent, child in simapp

--- testutil/simapp/simapp.go
+++ testutil/simapp/simapp.go
@@ -16,25 +16,9 @@ import (
 	tmtypes "github.com/tendermint/tendermint/types"
 	tmdb "github.com/tendermint/tm-db"
 
-	"github.com/cosmos/interchain-security/app"
+	childApp "github.com/cosmos/interchain-security/app_child"
+	parentApp "github.com/cosmos/interchain-security/app_parent"
 )
-
-// New creates application instance with in-memory database and disabled logging.
-func New(dir string) cosmoscmd.App {
-	db := tmdb.NewMemDB()
-	logger := log.NewNopLogger()
-
-	encoding := cosmoscmd.MakeEncodingConfig(app.ModuleBasics)
-
-	a := app.New(logger, db, nil, true, map[int64]bool{}, dir, 0, encoding,
-		simapp.EmptyAppOptions{})
-	// InitChain updates deliverState which is required when app.NewContext is called
-	a.InitChain(abci.RequestInitChain{
-		ConsensusParams: defaultConsensusParams,
-		AppStateBytes:   []byte("{}"),
-	})
-	return a
-}
 
 var defaultConsensusParams = &abci.ConsensusParams{
 	Block: &abci.BlockParams{
@@ -56,17 +40,17 @@ var defaultConsensusParams = &abci.ConsensusParams{
 func SetupTestingParentApp() (ibctesting.TestingApp, map[string]json.RawMessage) {
 	db := tmdb.NewMemDB()
 	// encCdc := app.MakeTestEncodingConfig()
-	encoding := cosmoscmd.MakeEncodingConfig(app.ModuleBasics)
-	testApp := app.New(log.NewNopLogger(), db, nil, true, map[int64]bool{}, simapp.DefaultNodeHome, 5, encoding, simapp.EmptyAppOptions{}).(ibctesting.TestingApp)
-	return testApp, app.NewDefaultGenesisState(encoding.Marshaler)
+	encoding := cosmoscmd.MakeEncodingConfig(parentApp.ModuleBasics)
+	testApp := parentApp.New(log.NewNopLogger(), db, nil, true, map[int64]bool{}, simapp.DefaultNodeHome, 5, encoding, simapp.EmptyAppOptions{}).(ibctesting.TestingApp)
+	return testApp, parentApp.NewDefaultGenesisState(encoding.Marshaler)
 }
 
 func SetupTestingChildApp() (ibctesting.TestingApp, map[string]json.RawMessage) {
 	db := tmdb.NewMemDB()
 	// encCdc := app.MakeTestEncodingConfig()
-	encoding := cosmoscmd.MakeEncodingConfig(app.ModuleBasics)
-	testApp := app.New(log.NewNopLogger(), db, nil, true, map[int64]bool{}, simapp.DefaultNodeHome, 5, encoding, simapp.EmptyAppOptions{}).(ibctesting.TestingApp)
-	return testApp, app.NewDefaultGenesisState(encoding.Marshaler)
+	encoding := cosmoscmd.MakeEncodingConfig(childApp.ModuleBasics)
+	testApp := childApp.New(log.NewNopLogger(), db, nil, true, map[int64]bool{}, simapp.DefaultNodeHome, 5, encoding, simapp.EmptyAppOptions{}).(ibctesting.TestingApp)
+	return testApp, childApp.NewDefaultGenesisState(encoding.Marshaler)
 }
 
 // NewCoordinator initializes Coordinator with 0 TestChains
