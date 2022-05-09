6f69cfe6361accd2d50704a5e554c193404c1c48
Simon <simon.ntz@gmail.com>
2022-05-09 20:30:27 +0200

    Fix tests after the 'UnbondingOp' renaming in cosmos-sdk

--- x/ccv/provider/provider_test.go
+++ x/ccv/provider/provider_test.go
@@ -46,7 +46,7 @@ type ProviderTestSuite struct {
 
 	// testing chains
 	providerChain *ibctesting.TestChain
-	consumerChain  *ibctesting.TestChain
+	consumerChain *ibctesting.TestChain
 
 	providerClient    *ibctmtypes.ClientState
 	providerConsState *ibctmtypes.ConsensusState
@@ -540,7 +540,7 @@ func (s *ProviderTestSuite) TestHandleConsumerDowntimeErrors() {
 	s.providerChain.App.GetStakingKeeper().BlockValidatorUpdates(providerCtx)
 
 	// set manually validator status from unbonding to unbonded
-	err = s.providerChain.App.GetStakingKeeper().UnbondingOpCanComplete(providerCtx, uint64(1))
+	err = s.providerChain.App.GetStakingKeeper().UnbondingCanComplete(providerCtx, uint64(1))
 	s.Require().NoError(err)
 
 	// replace validator address
