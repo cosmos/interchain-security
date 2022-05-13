e2475c2c31b8568d883fe2f6ede942a3b53ca178
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2022-05-13 09:29:56 -0400

    updates

--- x/ccv/staking/module.go
+++ x/ccv/staking/module.go
@@ -29,27 +29,40 @@ type AppModule struct {
 	// embed the Cosmos SDK's x/staking AppModule
 	staking.AppModule
 
-	keeper keeper.Keeper
+	keeper     keeper.Keeper
+	accKeeper  types.AccountKeeper
+	bankKeeper types.BankKeeper
 }
 
 // NewAppModule creates a new AppModule object using the native x/staking module
 // AppModule constructor.
 func NewAppModule(cdc codec.Codec, keeper keeper.Keeper, ak types.AccountKeeper, bk types.BankKeeper) AppModule {
 	stakingAppMod := staking.NewAppModule(cdc, keeper, ak, bk)
-	return AppModule{AppModule: stakingAppMod, keeper: keeper}
+	return AppModule{
+		AppModule:  stakingAppMod,
+		keeper:     keeper,
+		accKeeper:  ak,
+		bankKeeper: bk,
+	}
 }
 
-// InitGenesis performs a no-op in that it returns no validator set updates.
-// Recall, validators are tracked via the consumer chain's x/cvv/consumer module
-// and so this module is not responsible for returning the initial validator set.
+// InitGenesis delegates the InitGenesis call to the underlying x/staking module,
+// however, it returns no validator updates as validators are tracked via the
+// consumer chain's x/cvv/consumer module and so this module is not responsible
+// for returning the initial validator set.
 func (am AppModule) InitGenesis(ctx sdk.Context, cdc codec.JSONCodec, data json.RawMessage) []abci.ValidatorUpdate {
+	var genesisState types.GenesisState
+
+	cdc.MustUnmarshalJSON(data, &genesisState)
+	_ = staking.InitGenesis(ctx, am.keeper, am.accKeeper, am.bankKeeper, &genesisState)
+
 	return []abci.ValidatorUpdate{}
 }
 
-// EndBlock performs a no-op in that it returns no validator set updates. Recall,
-// validators are tracked via the consumer chain's x/cvv/consumer module. Validators
-// contained within this module act as "pseudo" validators, in that they may be
-// used in the consumer chain's governance system.
+// EndBlock delegates the EndBlock call to the underlying x/staking module,
+// however, it returns no validator updates as validators are tracked via the
+// consumer chain's x/cvv/consumer module and so this module is not responsible
+// for returning the initial validator set.
 func (am AppModule) EndBlock(ctx sdk.Context, _ abci.RequestEndBlock) []abci.ValidatorUpdate {
 	_ = am.keeper.BlockValidatorUpdates(ctx)
 	return []abci.ValidatorUpdate{}
