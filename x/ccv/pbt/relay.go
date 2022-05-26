c6833ea238250febe0b5e50ae9c17acb04dba68b
Daniel <danwtisdall@gmail.com>
2022-05-26 13:41:00 +0100

    In relay parameterize endpints

--- x/ccv/pbt/relay.go
+++ x/ccv/pbt/relay.go
@@ -8,51 +8,42 @@ import (
 	ibctesting "github.com/cosmos/ibc-go/v3/testing"
 )
 
+func tryRelay(sender *ibctesting.Endpoint, receiver *ibctesting.Endpoint, packet channeltypes.Packet) error {
+
+	pc := sender.Chain.App.GetIBCKeeper().ChannelKeeper.GetPacketCommitment(sender.Chain.GetContext(), packet.GetSourcePort(), packet.GetSourceChannel(), packet.GetSequence())
+
+	if bytes.Equal(pc, channeltypes.CommitPacket(sender.Chain.App.AppCodec(), packet)) {
+
+		receiver.UpdateClient()
+
+		res, err := receiver.RecvPacketWithResult(packet)
+		if err != nil {
+			return err
+		}
+
+		ack, err := ibctesting.ParseAckFromEvents(res.GetEvents())
+		if err != nil {
+			return err
+		}
+
+		if err := sender.AcknowledgePacket(packet, ack); err != nil {
+			return err
+		}
+
+	}
+
+	return nil
+
+}
+
 func RelayPacket(path *ibctesting.Path, packet channeltypes.Packet) error {
-	pc := path.EndpointA.Chain.App.GetIBCKeeper().ChannelKeeper.GetPacketCommitment(path.EndpointA.Chain.GetContext(), packet.GetSourcePort(), packet.GetSourceChannel(), packet.GetSequence())
-	if bytes.Equal(pc, channeltypes.CommitPacket(path.EndpointA.Chain.App.AppCodec(), packet)) {
-
-		// packet found, relay from A to B
-		path.EndpointB.UpdateClient()
-
-		res, err := path.EndpointB.RecvPacketWithResult(packet)
-		if err != nil {
-			return err
-		}
-
-		ack, err := ibctesting.ParseAckFromEvents(res.GetEvents())
-		if err != nil {
-			return err
-		}
-
-		if err := path.EndpointA.AcknowledgePacket(packet, ack); err != nil {
-			return err
-		}
-		return nil
-
+	err := tryRelay(path.EndpointA, path.EndpointB, packet)
+	if err != nil {
+		return err
 	}
-
-	pc = path.EndpointB.Chain.App.GetIBCKeeper().ChannelKeeper.GetPacketCommitment(path.EndpointB.Chain.GetContext(), packet.GetSourcePort(), packet.GetSourceChannel(), packet.GetSequence())
-	if bytes.Equal(pc, channeltypes.CommitPacket(path.EndpointB.Chain.App.AppCodec(), packet)) {
-
-		// packet found, relay B to A
-		path.EndpointA.UpdateClient()
-
-		res, err := path.EndpointA.RecvPacketWithResult(packet)
-		if err != nil {
-			return err
-		}
-
-		ack, err := ibctesting.ParseAckFromEvents(res.GetEvents())
-		if err != nil {
-			return err
-		}
-
-		if err := path.EndpointB.AcknowledgePacket(packet, ack); err != nil {
-			return err
-		}
-		return nil
+	err = tryRelay(path.EndpointB, path.EndpointA, packet)
+	if err != nil {
+		return err
 	}
-
 	return fmt.Errorf("packet commitment does not exist on either endpoint for provided packet")
 }
