9db17f8acb7b68940d8f2ce84dacb7c417ddcad3
mpoke <marius.poke@posteo.de>
2022-06-27 11:50:44 +0200

    add found return to GetPendingVSCs

--- x/ccv/provider/unbonding_test.go
+++ x/ccv/provider/unbonding_test.go
@@ -189,7 +189,7 @@ func (s *ProviderTestSuite) TestUndelegationDuringInit() {
 	s.providerChain.NextBlock()
 
 	// check that the VSC packet is stored in state as pending
-	pendingVSCs := s.providerChain.App.(*appProvider.App).ProviderKeeper.GetPendingVSCs(s.providerCtx(), s.consumerChain.ChainID)
+	pendingVSCs, _ := s.providerChain.App.(*appProvider.App).ProviderKeeper.GetPendingVSCs(s.providerCtx(), s.consumerChain.ChainID)
 	s.Require().True(len(pendingVSCs) == 1, "no pending VSC packet found")
 
 	// delegate again to create another VSC packet
@@ -199,7 +199,7 @@ func (s *ProviderTestSuite) TestUndelegationDuringInit() {
 	s.providerChain.NextBlock()
 
 	// check that the VSC packet is stored in state as pending
-	pendingVSCs = s.providerChain.App.(*appProvider.App).ProviderKeeper.GetPendingVSCs(s.providerCtx(), s.consumerChain.ChainID)
+	pendingVSCs, _ = s.providerChain.App.(*appProvider.App).ProviderKeeper.GetPendingVSCs(s.providerCtx(), s.consumerChain.ChainID)
 	s.Require().True(len(pendingVSCs) == 2, "only one pending VSC packet found")
 
 	// increment time so that the unbonding period ends on the provider
