162c308e1a0ccd116ba29fb4f1d47b13d999c73c
Daniel <danwtisdall@gmail.com>
2022-06-28 18:37:25 +0100

    Work on tool install

---
+++ scripts/install_tools.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e -o pipefail
+
+if ! command -v tomlv &> /dev/null ; then
+    echo "tomlv not installed or available in the PATH, installing" >&2
+    go install github.com/BurntSushi/toml/cmd/tomlv@latest;
+fi
+
+if ! command -v gocyclo &> /dev/null ; then
+    echo "gocyclo not installed or available in the PATH, installing" >&2
+    go install github.com/fzipp/gocyclo/cmd/gocyclo@latest;
+fi
+
+if ! command -v gocritic &> /dev/null ; then
+    echo "gocritic not installed or available in the PATH, installing" >&2
+    go install github.com/go-critic/go-critic/cmd/gocritic@latest;
+fi
+
+if ! command -v goimports &> /dev/null ; then
+    echo "goimports not installed or available in the PATH, installing" >&2
+    go install golang.org/x/tools/cmd/goimports@latest;
+fi
+
+exit 0
