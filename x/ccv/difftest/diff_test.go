f7b8cda571e1f832aae65e3516202ad4fe3c6f98
Daniel <danwtisdall@gmail.com>
2022-07-05 16:54:42 +0100

    Adds network to diff_test.go

--- x/ccv/difftest/diff_test.go
+++ x/ccv/difftest/diff_test.go
@@ -49,6 +49,8 @@ In the model, height begins at 0 for both chains because init
 is not modelled.
 */
 const MODEL_HEIGHT_OFFSET = int64(20)
+const SUT_TIME_OFFSET = 1577923357
+const DELEGATOR_INITIAL_BALANCE = 1000000000000000
 
 /*
 TODO: do I need a particular denom here or different denoms for each
@@ -75,6 +77,66 @@ type Ack struct {
 	commits int
 }
 
+type Packet struct {
+	packet  channeltypes.Packet
+	commits int
+}
+
+type Network struct {
+	outboxPackets map[string][]Packet
+	outboxAcks    map[string][]Ack
+}
+
+func makeNetwork() Network {
+	return Network{
+		outboxPackets: map[string][]Packet{},
+		outboxAcks:    map[string][]Ack{},
+	}
+}
+
+func (n Network) addPacket(sender string, packet channeltypes.Packet) {
+	n.outboxPackets[sender] = append(n.outboxPackets[sender], Packet{packet, 0})
+}
+
+func (n Network) addAck(sender string, ack []byte, packet channeltypes.Packet) {
+	n.outboxAcks[sender] = append(n.outboxAcks[sender], Ack{ack, packet, 0})
+}
+
+func (n Network) consumePackets(sender string) []Packet {
+	var ret = make([]Packet, 0)
+	for _, p := range n.outboxPackets[sender] {
+		if 1 < p.commits {
+			ret = append(ret, p)
+		} else {
+			break
+		}
+	}
+	n.outboxPackets[sender] = n.outboxPackets[sender][len(ret):]
+	return ret
+}
+
+func (n Network) consumeAcks(sender string) []Ack {
+	var ret = make([]Ack, 0)
+	for _, a := range n.outboxAcks[sender] {
+		if 1 < a.commits {
+			ret = append(ret, a)
+		} else {
+			break
+		}
+	}
+	n.outboxAcks[sender] = n.outboxAcks[sender][len(ret):]
+	return ret
+}
+
+func (n Network) commit(sender string) {
+	for _, p := range n.outboxPackets[sender] {
+		p.commits += 1
+	}
+	for _, a := range n.outboxAcks[sender] {
+		a.commits += 1
+	}
+}
+
 type DTTestSuite struct {
 	suite.Suite
 
@@ -90,24 +152,13 @@ type DTTestSuite struct {
 
 	valAddresses []sdk.ValAddress
 
-	outbox map[string][]channeltypes.Packet
-	acks   map[string][]Ack
+	network Network
 
 	heightLastClientUpdate map[string]int64
 }
 
 func TestDTTestSuite(t *testing.T) {
 	suite.Run(t, new(DTTestSuite))
-}
-
-func (s *DTTestSuite) addAck(receiver string, ack []byte, packet channeltypes.Packet) {
-	s.acks[receiver] = append(s.acks[receiver], Ack{ack, packet, 0})
-}
-
-func (s *DTTestSuite) commitAcks(committer string) {
-	for _, ack := range s.acks[s.other(committer)] {
-		ack.commits += 1
-	}
 }
 
 func (s *DTTestSuite) createValidator() (tmtypes.PrivValidator, sdk.ValAddress) {
@@ -220,8 +271,7 @@ func (s *DTTestSuite) SetupTest() {
 
 	s.coordinator, s.providerChain, s.consumerChain, s.valAddresses = difftest.NewDTProviderConsumerCoordinator(s.T())
 	s.mustBeginBlock = map[string]bool{P: true, C: true}
-	s.outbox = map[string][]channeltypes.Packet{P: {}, C: {}}
-	s.acks = map[string][]Ack{P: {}, C: {}}
+	s.network = makeNetwork()
 	s.heightLastClientUpdate = map[string]int64{P: 0, C: 0}
 
 	/*
@@ -290,7 +340,6 @@ func (s *DTTestSuite) SetupTest() {
 
 	s.path.EndpointA.ClientID = providerClientId
 
-	// TODO: No one knows why these lines are needed.
 	s.path.EndpointB.Chain.SenderAccount.SetAccountNumber(6)
 	s.path.EndpointA.Chain.SenderAccount.SetAccountNumber(1)
 
@@ -382,7 +431,7 @@ func (s *DTTestSuite) stakingKeeperP() stakingkeeper.Keeper {
 func (s *DTTestSuite) isJailed(i int64) bool {
 	val, found := s.stakingKeeperP().GetValidator(s.ctx(P), s.validator(i))
 	if !found {
-		s.T().Fatal("Couldn't GetValidator")
+		s.T().Fatal("GetValidator() -> !found")
 	}
 	return val.IsJailed()
 }
@@ -391,7 +440,7 @@ func (s *DTTestSuite) consumerPower(i int64) (int64, error) {
 	ck := s.consumerChain.App.(*appConsumer.App).ConsumerKeeper
 	v, found := ck.GetCCValidator(s.ctx(C), s.validator(i))
 	if !found {
-		return 0, fmt.Errorf("CCValidator not found")
+		return 0, fmt.Errorf("GetCCValidator() -> !found")
 	}
 	return v.Power, nil
 }
@@ -399,7 +448,7 @@ func (s *DTTestSuite) consumerPower(i int64) (int64, error) {
 func (s *DTTestSuite) delegation(i int64) int64 {
 	d, found := s.stakingKeeperP().GetDelegation(s.ctx(P), s.delegator(), s.validator(i))
 	if !found {
-		s.T().Fatal("Couldn't GetDelegation")
+		s.T().Fatal("GetDelegation() -> !found")
 	}
 	return d.Shares.TruncateInt64()
 }
@@ -407,7 +456,7 @@ func (s *DTTestSuite) delegation(i int64) int64 {
 func (s *DTTestSuite) validatorStatus(i int64) stakingtypes.BondStatus {
 	v, found := s.stakingKeeperP().GetValidator(s.ctx(P), s.validator(i))
 	if !found {
-		s.T().Fatal("Couldn't GetValidator")
+		s.T().Fatal("GetValidator() -> !found")
 	}
 	return v.GetStatus()
 }
@@ -415,7 +464,7 @@ func (s *DTTestSuite) validatorStatus(i int64) stakingtypes.BondStatus {
 func (s *DTTestSuite) providerTokens(i int64) int64 {
 	v, found := s.stakingKeeperP().GetValidator(s.ctx(P), s.validator(i))
 	if !found {
-		s.T().Fatal("Couldn't GetValidator")
+		s.T().Fatal("GetValidator() -> !found")
 	}
 	return v.Tokens.Int64()
 }
@@ -456,26 +505,19 @@ func (s *DTTestSuite) idempotentBeginBlock(chain string) {
 }
 
 func (s *DTTestSuite) idempotentDeliverAcks(receiver string) error {
-	acks := s.acks[receiver]
-	replacement := []Ack{}
-	for _, ack := range acks {
-		if 2 <= ack.commits {
-			// TODO: reintroduce?
-			// s.idempotentUpdateClient(receiver)
-			// p := ack.packet
-			// difftest.TryRelayAck(s.endpoint(s.other(receiver)), s.endpoint(receiver), p, ack.ack)
-		} else {
-			replacement = append(replacement, ack)
+	for _, ack := range s.network.consumeAcks(s.other(receiver)) {
+		s.idempotentUpdateClient(receiver)
+		err := difftest.TryRecvAck(s.endpoint(s.other(receiver)), s.endpoint(receiver), ack.packet, ack.ack)
+		if err != nil {
+			return err
 		}
 	}
-	s.acks[receiver] = replacement
-
 	return nil
 }
 
 func (s DTTestSuite) idempotentUpdateClient(chain string) {
 	otherHeight := s.height(s.other(chain))
-	if int64(s.heightLastClientUpdate[chain]) < otherHeight {
+	if s.heightLastClientUpdate[chain] < otherHeight {
 		err := difftest.UpdateReceiverClient(s.endpoint(s.other(chain)), s.endpoint(chain))
 		if err != nil {
 			s.FailNow("Bad test")
@@ -541,19 +583,17 @@ func (s *DTTestSuite) endBlock(chain string) {
 
 	c.NextVals = ibctesting.ApplyValSetChanges(c.T, c.Vals, ebRes.ValidatorUpdates)
 
-	s.mustBeginBlock[chain] = true
-
 	for _, e := range ebRes.Events {
 		if e.Type == channeltypes.EventTypeSendPacket {
 			packet, err := channelkeeper.ReconstructPacketFromEvent(e)
 			s.Require().NoError(err)
-
-			s.outbox[chain] = append(s.outbox[chain], packet)
-			fmt.Println("Outbox ", chain, ", len: ", len(s.outbox[chain]))
+			s.network.addPacket(chain, packet)
 		}
 	}
 
-	s.commitAcks(chain)
+	s.network.commit(chain)
+
+	s.mustBeginBlock[chain] = true
 
 	/*
 		This is a hack!!!
@@ -572,7 +612,6 @@ func (s *DTTestSuite) jumpNBlocks(chains []string, n int64, secondsPerBlock int6
 	for i := int64(0); i < n; i++ {
 		for _, c := range chains { // [P] or [P, C] or [C]
 			s.endBlock(c)
-			fmt.Println("endBlock", c)
 		}
 		s.coordinator.CurrentTime = s.coordinator.CurrentTime.Add(time.Second * time.Duration(secondsPerBlock)).UTC()
 	}
@@ -582,19 +621,15 @@ func (s *DTTestSuite) deliver(chain string) {
 	s.idempotentBeginBlock(chain)
 	s.idempotentDeliverAcks(chain)
 	s.idempotentUpdateClient(chain)
-	other := s.other(chain)
-	fmt.Println("outbox size before deliver(", chain, "):", len(s.outbox[other]))
-	for _, p := range s.outbox[other] {
+	for _, p := range s.network.consumePackets(s.other(chain)) {
 		receiver := s.endpoint(chain)
 		sender := receiver.Counterparty
-		ack, err := difftest.TryRecvPacket(sender, receiver, p)
+		ack, err := difftest.TryRecvPacket(sender, receiver, p.packet)
 		if err != nil {
 			s.FailNow("Relay failed", err)
 		}
-		fmt.Println("Done TryRelay without err")
-		s.addAck(s.other(chain), ack, p)
+		s.network.addAck(chain, ack, p.packet)
 	}
-	s.outbox[other] = []channeltypes.Packet{}
 }
 
 func (s *DTTestSuite) providerSlash(
@@ -609,9 +644,9 @@ func (s *DTTestSuite) consumerSlash(val sdk.ConsAddress, h int64,
 	power int64, isDowntime bool) {
 	s.idempotentBeginBlock(C)
 	s.idempotentDeliverAcks(C)
-	kind := stakingtypes.Downtime
+	kind := stakingtypes.DoubleSign
 	if isDowntime {
-		kind = stakingtypes.DoubleSign
+		kind = stakingtypes.Downtime
 	}
 	ctx := s.ctx(C)
 	before := len(ctx.EventManager().Events())
@@ -622,137 +657,10 @@ func (s *DTTestSuite) consumerSlash(val sdk.ConsAddress, h int64,
 		if e.Type == channeltypes.EventTypeSendPacket {
 			packet, err := channelkeeper.ReconstructPacketFromEvent(e)
 			s.Require().NoError(err)
-			s.outbox[C] = append(s.outbox[C], packet)
+			s.network.addPacket(C, packet)
 		}
 	}
 
-}
-
-/*
-~~~~~~~~~~~~
-ASSUMPTIONS TEST
-~~~~~~~~~~~~
-*/
-
-func (s *DTTestSuite) TestAssumptions() {
-
-	for i := 0; i < 4; i++ {
-		_, found := s.providerChain.App.(*appProvider.App).SlashingKeeper.GetValidatorSigningInfo(s.ctx(P), s.consAddr(int64(i)))
-		if !found {
-			s.Require().FailNow("Bad test , i=", i)
-		}
-	}
-
-	s.Require().Equal(SLASH_DOWNTIME, s.providerChain.App.(*appProvider.App).SlashingKeeper.SlashFractionDowntime(s.ctx(P)))
-	s.Require().Equal(SLASH_DOUBLESIGN, s.providerChain.App.(*appProvider.App).SlashingKeeper.SlashFractionDoubleSign(s.ctx(P)))
-
-	s.Require().Equal(false, s.mustBeginBlock[P])
-	s.Require().Equal(false, s.mustBeginBlock[C])
-
-	/*
-		Adding a 1 is needed here because the model always increases
-		the height by one immediately before the first action.
-	*/
-	s.Require().Equal(0+1+MODEL_HEIGHT_OFFSET, s.height(P))
-	s.Require().Equal(0+1+MODEL_HEIGHT_OFFSET, s.height(C))
-
-	s.Require().Empty(s.outbox[P])
-	s.Require().Empty(s.outbox[C])
-
-	const TIME_AFTER_SUT_INIT = 1577923357
-	s.Require().Equal(int64(TIME_AFTER_SUT_INIT), s.time(P).Unix())
-	s.Require().Equal(int64(TIME_AFTER_SUT_INIT), s.time(C).Unix())
-	s.Require().Equal(int64(TIME_AFTER_SUT_INIT), s.globalTime().Unix())
-
-	const DELEGATOR_INITIAL_BALANCE = 1000000000000000
-	s.Require().Equal(int64(DELEGATOR_INITIAL_BALANCE), s.delegatorBalance())
-
-	maxValsE := uint32(2)
-	maxVals := s.stakingKeeperP().GetParams(s.ctx(P)).MaxValidators
-
-	if maxValsE != maxVals {
-		s.T().Fatal("Bad test")
-	}
-
-	initialModelState := difftest.InitialModelState{
-		Delegation: []int64{4 * difftest.TOKEN_SCALAR, 3 * difftest.TOKEN_SCALAR, 2 * difftest.TOKEN_SCALAR, 1 * difftest.TOKEN_SCALAR},
-		Status:     []stakingtypes.BondStatus{stakingtypes.Bonded, stakingtypes.Bonded, stakingtypes.Unbonded, stakingtypes.Unbonded},
-	}
-
-	for i := 0; i < 4; i++ {
-		E := initialModelState.Delegation[i]
-		A := s.delegation(int64(i))
-		if E != A {
-			s.T().Fatal("Bad test")
-		}
-	}
-	for i := 0; i < 4; i++ {
-		E := initialModelState.Delegation[i] + difftest.TOKEN_SCALAR
-		A := s.providerTokens(int64(i))
-		if E != A {
-			s.T().Fatal("Bad test")
-		}
-	}
-	for i := 0; i < 4; i++ {
-		E := initialModelState.Status[i]
-		A := s.validatorStatus(int64(i))
-		if E != A {
-			s.T().Fatal("Bad test")
-		}
-	}
-
-	sk := s.stakingKeeperP()
-
-	sk.IterateUnbondingDelegations(s.ctx(P),
-		func(index int64, ubd stakingtypes.UnbondingDelegation) bool {
-			s.T().Fatal("Bad test")
-			return false // Don't stop
-		})
-
-	sk.IterateRedelegations(s.ctx(P),
-		func(index int64, ubd stakingtypes.Redelegation) bool {
-			s.T().Fatal("Bad test")
-			return false // Don't stop
-		})
-
-	endTime := time.Unix(math.MaxInt64, 0)
-	endHeight := int64(math.MaxInt64) // is this borked?
-	// TODO: these are supposed to be MAX and not MIN right?
-	unbondingValIterator := sk.ValidatorQueueIterator(s.ctx(P), endTime, endHeight)
-	defer unbondingValIterator.Close()
-	for ; unbondingValIterator.Valid(); unbondingValIterator.Next() {
-		s.T().Fatal("Bad test")
-	}
-
-	eFound := []bool{true, true, false, false}
-	ePower := []int64{5 * difftest.TOKEN_SCALAR, 4 * difftest.TOKEN_SCALAR}
-
-	ck := s.consumerChain.App.(*appConsumer.App).ConsumerKeeper
-
-	for i := 0; i < 4; i++ {
-		addr := s.validator(int64(i))
-		val, found := ck.GetCCValidator(s.ctx(C), addr)
-		s.Require().Equal(eFound[i], found)
-		if eFound[i] {
-			if ePower[i] != val.Power {
-				s.T().Fatal("Bad test")
-			}
-		}
-	}
-
-	s.Require().Empty(ck.GetPendingSlashRequests(s.ctx(C)))
-
-	var numUnbondingTimes = 0
-	ck.IteratePacketMaturityTime(s.ctx(C),
-		func(vscId uint64, timeNs uint64) bool {
-			numUnbondingTimes += 1
-			if 1 < numUnbondingTimes {
-				s.T().Fatal("Bad test")
-			}
-			return false // Don't stop
-		})
-
-	s.T().Fatal("Good test! (Sanity check)")
 }
 
 /*
@@ -761,20 +669,16 @@ TRACE TEST
 ~~~~~~~~~~~~
 */
 
-func (s *DTTestSuite) matchState(chain string, c difftest.Consequence, i int) {
-	SUTStartTime := time.Unix(1577923357, 0).UTC()
+func (s *DTTestSuite) matchState(chain string, c difftest.Consequence) {
+	SUTStartTime := time.Unix(SUT_TIME_OFFSET, 0).UTC()
 	modelOffset := time.Second * time.Duration(-5)
 	timeOffset := SUTStartTime.Add(modelOffset)
 
 	if chain == P {
-		s.Require().Equal(int64(c.H.Provider+int(MODEL_HEIGHT_OFFSET)), s.height(P), i)
+		s.Require().Equal(int64(c.H.Provider+int(MODEL_HEIGHT_OFFSET)), s.height(P))
 		s.Require().Equal(int64(c.DelegatorTokens), s.delegatorBalance())
 		for j, jailedUntilTimestamp := range c.Jailed {
-			isJailed := false
-			if jailedUntilTimestamp != nil {
-				isJailed = true
-			}
-			s.Require().Equal(isJailed, s.isJailed(int64(j)))
+			s.Require().Equal(jailedUntilTimestamp != nil, s.isJailed(int64(j)))
 		}
 		t := time.Second * time.Duration(c.T.Provider)
 		s.Require().Equal(timeOffset.Add(t), s.time(P))
@@ -783,7 +687,7 @@ func (s *DTTestSuite) matchState(chain string, c difftest.Consequence, i int) {
 		}
 	}
 	if chain == C {
-		s.Require().Equal(int64(c.H.Consumer+int(MODEL_HEIGHT_OFFSET)), s.height(C), i)
+		s.Require().Equal(int64(c.H.Consumer+int(MODEL_HEIGHT_OFFSET)), s.height(C))
 		for j, power := range c.Power {
 			actual, err := s.consumerPower(int64(j))
 			if power != nil {
@@ -887,4 +791,131 @@ func executeTraces(s *DTTestSuite, traces []difftest.Trace) {
 
 func (s *DTTestSuite) TestTracesCovering() {
 	executeTraces(s, loadTraces("covering.json"))
+}
+
+/*
+~~~~~~~~~~~~
+ASSUMPTIONS TEST
+~~~~~~~~~~~~
+*/
+
+func (s *DTTestSuite) TestAssumptions() {
+
+	const FAIL_MESSAGE = "Diff test assumptions failed. There is a problem with the test driver."
+
+	for i := 0; i < 4; i++ {
+		_, found := s.providerChain.App.(*appProvider.App).SlashingKeeper.GetValidatorSigningInfo(s.ctx(P), s.consAddr(int64(i)))
+		if !found {
+			s.Require().FailNow(FAIL_MESSAGE)
+		}
+	}
+
+	s.Require().Equal(SLASH_DOWNTIME, s.providerChain.App.(*appProvider.App).SlashingKeeper.SlashFractionDowntime(s.ctx(P)))
+	s.Require().Equal(SLASH_DOUBLESIGN, s.providerChain.App.(*appProvider.App).SlashingKeeper.SlashFractionDoubleSign(s.ctx(P)))
+
+	s.Require().Equal(false, s.mustBeginBlock[P])
+	s.Require().Equal(false, s.mustBeginBlock[C])
+
+	/*
+		Adding a 1 is needed here because the model always increases
+		the height by one immediately before the first action.
+	*/
+	s.Require().Equal(0+1+MODEL_HEIGHT_OFFSET, s.height(P))
+	s.Require().Equal(0+1+MODEL_HEIGHT_OFFSET, s.height(C))
+
+	s.Require().Empty(s.network.outboxPackets[P])
+	s.Require().Empty(s.network.outboxPackets[C])
+
+	s.Require().Equal(int64(SUT_TIME_OFFSET), s.time(P).Unix())
+	s.Require().Equal(int64(SUT_TIME_OFFSET), s.time(C).Unix())
+	s.Require().Equal(int64(SUT_TIME_OFFSET), s.globalTime().Unix())
+
+	s.Require().Equal(int64(DELEGATOR_INITIAL_BALANCE), s.delegatorBalance())
+
+	maxValsE := uint32(2)
+	maxVals := s.stakingKeeperP().GetParams(s.ctx(P)).MaxValidators
+
+	if maxValsE != maxVals {
+		s.T().Fatal(FAIL_MESSAGE)
+	}
+
+	initialModelState := difftest.InitialModelState{
+		Delegation: []int64{4 * difftest.TOKEN_SCALAR, 3 * difftest.TOKEN_SCALAR, 2 * difftest.TOKEN_SCALAR, 1 * difftest.TOKEN_SCALAR},
+		Status:     []stakingtypes.BondStatus{stakingtypes.Bonded, stakingtypes.Bonded, stakingtypes.Unbonded, stakingtypes.Unbonded},
+	}
+
+	for i := 0; i < 4; i++ {
+		E := initialModelState.Delegation[i]
+		A := s.delegation(int64(i))
+		if E != A {
+			s.T().Fatal(FAIL_MESSAGE)
+		}
+	}
+	for i := 0; i < 4; i++ {
+		E := initialModelState.Delegation[i] + difftest.TOKEN_SCALAR
+		A := s.providerTokens(int64(i))
+		if E != A {
+			s.T().Fatal(FAIL_MESSAGE)
+		}
+	}
+	for i := 0; i < 4; i++ {
+		E := initialModelState.Status[i]
+		A := s.validatorStatus(int64(i))
+		if E != A {
+			s.T().Fatal(FAIL_MESSAGE)
+		}
+	}
+
+	sk := s.stakingKeeperP()
+
+	sk.IterateUnbondingDelegations(s.ctx(P),
+		func(index int64, ubd stakingtypes.UnbondingDelegation) bool {
+			s.T().Fatal(FAIL_MESSAGE)
+			return false // Don't stop
+		})
+
+	sk.IterateRedelegations(s.ctx(P),
+		func(index int64, ubd stakingtypes.Redelegation) bool {
+			s.T().Fatal(FAIL_MESSAGE)
+			return false // Don't stop
+		})
+
+	endTime := time.Unix(math.MaxInt64, 0)
+	endHeight := int64(math.MaxInt64)
+	unbondingValIterator := sk.ValidatorQueueIterator(s.ctx(P), endTime, endHeight)
+	defer unbondingValIterator.Close()
+	for ; unbondingValIterator.Valid(); unbondingValIterator.Next() {
+		s.T().Fatal(FAIL_MESSAGE)
+	}
+
+	eFound := []bool{true, true, false, false}
+	ePower := []int64{5 * difftest.TOKEN_SCALAR, 4 * difftest.TOKEN_SCALAR}
+
+	ck := s.consumerChain.App.(*appConsumer.App).ConsumerKeeper
+
+	for i := 0; i < 4; i++ {
+		addr := s.validator(int64(i))
+		val, found := ck.GetCCValidator(s.ctx(C), addr)
+		s.Require().Equal(eFound[i], found)
+		if eFound[i] {
+			if ePower[i] != val.Power {
+				s.T().Fatal(FAIL_MESSAGE)
+			}
+		}
+	}
+
+	s.Require().Empty(ck.GetPendingSlashRequests(s.ctx(C)))
+
+	var numUnbondingTimes = 0
+	ck.IteratePacketMaturityTime(s.ctx(C),
+		func(vscId uint64, timeNs uint64) bool {
+			numUnbondingTimes += 1
+			if 1 < numUnbondingTimes {
+				s.T().Fatal(FAIL_MESSAGE)
+			}
+			return false // Don't stop
+		})
+
+	// TODO: replace this
+	s.T().Fatal("Good test! (Sanity check)")
 }
