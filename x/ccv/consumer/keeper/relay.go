d98aeecd515fb7cf93abdd0058a2b5802938229d
Daniel <danwtisdall@gmail.com>
2022-07-13 14:12:23 +0100

    Checkpoint, success! Find critical bug. (Contains debug statements)

--- x/ccv/consumer/keeper/relay.go
+++ x/ccv/consumer/keeper/relay.go
@@ -49,6 +49,7 @@ func (k Keeper) OnRecvVSCPacket(ctx sdk.Context, packet channeltypes.Packet, new
 	if !found {
 		panic("the unbonding period is not set on the consumer chain")
 	}
+
 	maturityTime := ctx.BlockTime().Add(unbondingPeriod)
 	k.SetPacketMaturityTime(ctx, newChanges.ValsetUpdateId, uint64(maturityTime.UnixNano()))
 
