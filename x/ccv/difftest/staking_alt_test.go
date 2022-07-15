3047579b155a715073e2228afb74473f85a846ec
Daniel <danwtisdall@gmail.com>
2022-07-15 21:08:11 +0100

    Working on slash bug

--- x/ccv/difftest/staking_alt_test.go
+++ x/ccv/difftest/staking_alt_test.go
@@ -1,7 +1,8 @@
 package difftest_test
 
 import (
-	"math/big"
+	"math/rand"
+	"strconv"
 	"testing"
 	"time"
 
@@ -17,10 +18,6 @@ import (
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 )
 
-func init() {
-	sdk.DefaultPowerReduction = sdk.NewIntFromBigInt(new(big.Int).Exp(big.NewInt(10), big.NewInt(18), nil))
-}
-
 const (
 	v0 = "v0"
 	d0 = "d0"
@@ -32,13 +29,8 @@ var (
 	PKs = simapp.CreateTestPubKeys(500)
 )
 
-func scaledAmt(modelAmt int) sdk.Int {
-	return sdk.TokensFromConsensusPower(int64(modelAmt), sdk.DefaultPowerReduction)
-}
-
-// constructs a  rates with all zeros.
-func zeroCommission() stakingtypes.CommissionRates {
-	return stakingtypes.NewCommissionRates(sdk.ZeroDec(), sdk.ZeroDec(), sdk.ZeroDec())
+func tokensFromPower(power int64) sdk.Int {
+	return sdk.TokensFromConsensusPower(int64(power), sdk.DefaultPowerReduction)
 }
 
 func getBaseSimappWithCustomKeeper() (*codec.LegacyAmino, *simapp.SimApp, sdk.Context) {
@@ -96,42 +88,30 @@ func newHelperx(t *testing.T, ctx sdk.Context, k keeper.Keeper, bk types.BankKee
 	return &helperx{t, staking.NewHandler(k), k, bk, ctx, sdk.DefaultBondDenom}
 }
 
-// handle calls staking handler on a given message
-func (h *helperx) handle(msg sdk.Msg, ok bool) *sdk.Result {
-	res, err := h.h(h.ctx, msg)
-	if ok {
-		require.NoError(h.t, err)
-		require.NotNil(h.t, res)
-	} else {
-		require.Error(h.t, err)
-		require.Nil(h.t, res)
-	}
-	return res
-}
-
-func (h *helperx) createValidator(addr sdk.ValAddress, pk cryptotypes.PubKey, stakeAmt int, ok bool) {
-	coin := sdk.NewCoin(h.denom, scaledAmt(stakeAmt))
-	msg, err := stakingtypes.NewMsgCreateValidator(addr, pk, coin, stakingtypes.Description{}, zeroCommission(), sdk.ZeroInt())
+func (h *helperx) createValidator(addr sdk.ValAddress, pk cryptotypes.PubKey, tokens int64) {
+	coin := sdk.NewCoin(h.denom, sdk.NewInt(tokens))
+	zeroCommission := stakingtypes.NewCommissionRates(sdk.ZeroDec(), sdk.ZeroDec(), sdk.ZeroDec())
+	msg, err := stakingtypes.NewMsgCreateValidator(addr, pk, coin, stakingtypes.Description{}, zeroCommission, sdk.ZeroInt())
 	require.NoError(h.t, err)
-	h.handle(msg, ok)
+	h.h(h.ctx, msg)
 }
 
-func (h *helperx) delegate(delegator sdk.AccAddress, val sdk.ValAddress, amt int, ok bool) {
-	coin := sdk.NewCoin(h.denom, scaledAmt(amt))
+func (h *helperx) delegate(delegator sdk.AccAddress, val sdk.ValAddress, tokens int64) {
+	coin := sdk.NewCoin(h.denom, sdk.NewInt(tokens))
 	msg := stakingtypes.NewMsgDelegate(delegator, val, coin)
-	h.handle(msg, ok)
+	h.h(h.ctx, msg)
 }
 
-func (h *helperx) undelegate(delegator sdk.AccAddress, val sdk.ValAddress, amt int, ok bool) *sdk.Result {
-	coin := sdk.NewCoin(h.denom, scaledAmt(amt))
+func (h *helperx) undelegate(delegator sdk.AccAddress, val sdk.ValAddress, tokens int64) {
+	coin := sdk.NewCoin(h.denom, sdk.NewInt(tokens))
 	msg := stakingtypes.NewMsgUndelegate(delegator, val, coin)
-	return h.handle(msg, ok)
+	h.h(h.ctx, msg)
 }
 
-func (h *helperx) beginRedelegate(delegator sdk.AccAddress, src sdk.ValAddress, dst sdk.ValAddress, amt int, ok bool) *sdk.Result {
-	coin := sdk.NewCoin(h.denom, scaledAmt(amt))
+func (h *helperx) beginRedelegate(delegator sdk.AccAddress, src sdk.ValAddress, dst sdk.ValAddress, tokens int64) {
+	coin := sdk.NewCoin(h.denom, sdk.NewInt(tokens))
 	msg := stakingtypes.NewMsgBeginRedelegate(delegator, src, dst, coin)
-	return h.handle(msg, ok)
+	h.h(h.ctx, msg)
 }
 
 func (h *helperx) slash(val sdk.ValAddress, height int64, power int64, factor sdk.Dec) {
@@ -139,63 +119,99 @@ func (h *helperx) slash(val sdk.ValAddress, height int64, power int64, factor sd
 	h.k.Slash(h.ctx, cons, height, power, factor, -1)
 }
 
-func (h *helperx) matchValidatorStatus(val sdk.ValAddress, status string) {
+func (h *helperx) matchValidatorStatus(val sdk.ValAddress, expect stakingtypes.BondStatus) {
 	validator, _ := h.k.GetValidator(h.ctx, val)
-	actual := validator.GetStatus()
-	if status == "bonded" {
-		require.Equalf(h.t, types.Bonded, actual, "exp: %s ", status)
-	}
-	if status == "unbonding" {
-		require.Equalf(h.t, types.Unbonding, actual, "exp: %s ", status)
-	}
-	if status == "unbonded" {
-		require.Equalf(h.t, types.Unbonded, actual, "exp: %s ", status)
-	}
+	require.Equalf(h.t, expect, validator.GetStatus(), "exp: %s ", expect.String())
 }
 
-func (h *helperx) matchBalance(addr sdk.AccAddress, amt int) {
-	bal := h.bk.GetBalance(h.ctx, addr, h.denom)
-	exp := sdk.NewCoin(h.denom, scaledAmt(amt))
-	require.Equal(h.t, exp, bal)
+func (h *helperx) balance(addr sdk.AccAddress) int64 {
+	return h.bk.GetBalance(h.ctx, addr, h.denom).Amount.Int64()
+}
+
+func (h *helperx) blocks(numBlocks int, secondsPerBlock int) {
+	for i := 0; i < numBlocks; i++ {
+		var dt = time.Duration(int64(secondsPerBlock) * int64(time.Second))
+		staking.EndBlocker(h.ctx, h.k)
+		h.ctx = h.ctx.WithBlockTime(h.ctx.BlockHeader().Time.Add(dt))
+	}
 }
 
 func Adversarial(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+	// sdk.DefaultPowerReduction = sdk.NewIntFromBigInt(new(big.Int).Exp(big.NewInt(10), big.NewInt(18), nil))
+	sdk.DefaultPowerReduction = sdk.NewInt(1)
+
 	ix := make(map[string]int)
 	ix[v0] = 0
 	ix[d0] = 1
 	ix[d1] = 2
 	ix[d2] = 3
 
-	initPower := int64(0)
 	numAddresses := 4
-	app, ctx, delAddrs, valAddrs := bootstrapHandlerGenesisTest(t, initPower, numAddresses, sdk.TokensFromConsensusPower(initPower, sdk.DefaultPowerReduction))
+	app, ctx, delAddrs, valAddrs := bootstrapHandlerGenesisTest(t, int64(0), numAddresses, sdk.TokensFromConsensusPower(int64(0), sdk.DefaultPowerReduction))
 	validators, delegators := valAddrs[:1], delAddrs
 
 	params := app.StakingKeeper.GetParams(ctx)
-	params.UnbondingTime = 2 * time.Second
+	params.UnbondingTime = 20 * time.Second
 	params.MaxValidators = 1
 	app.StakingKeeper.SetParams(ctx, params)
 
 	h := newHelperx(t, ctx, app.StakingKeeper, app.BankKeeper)
 
-	initTokens := scaledAmt(1000000000000)
+	initPower := int64(100000)
+	initTokens := tokensFromPower(initPower)
 	for _, delegator := range []string{d0, d1, d2} {
 		require.NoError(t, simapp.FundAccount(app.BankKeeper, ctx, delegators[ix[delegator]], sdk.NewCoins(sdk.NewCoin(params.BondDenom, initTokens))))
 	}
 
-	h.createValidator(validators[0], PKs[0], 0, true)
+	h.createValidator(validators[0], PKs[0], 0)
 
-	h.matchValidatorStatus(validators[ix[v0]], "unbonded")
+	h.matchValidatorStatus(validators[ix[v0]], types.Unbonded)
 
-	h.delegate(delegators[ix[d0]], validators[ix[v0]], 10000, true)
+	h.delegate(delegators[ix[d0]], validators[ix[v0]], 10000)
 
-	for i := 0; i < 100; i++ {
-		var dt = time.Duration(int64(5) * int64(time.Second))
-		staking.EndBlocker(h.ctx, h.k)
-		h.ctx = h.ctx.WithBlockTime(h.ctx.BlockHeader().Time.Add(dt))
+	h.blocks(1, 5)
+
+	h.matchValidatorStatus(validators[ix[v0]], types.Bonded)
+	require.Equal(h.t, h.balance(delegators[ix[d0]]), int64(90000))
+	require.Equal(h.t, h.balance(delegators[ix[d1]]), int64(100000))
+	require.Equal(h.t, h.balance(delegators[ix[d2]]), int64(100000))
+
+	randomDelegator := func(i int) string {
+		if rand.Float64() < 0.5 && 100 < i {
+			return d1
+		}
+		return d2
 	}
 
-	h.matchValidatorStatus(validators[ix[v0]], "bonded")
+	for i := 0; i < 100000000; i++ {
+		j := rand.Intn(4)
+		if j == 0 {
+			h.blocks(rand.Intn(20), 5)
+		}
+		if j == 1 {
+			delegator := randomDelegator(i)
+			tokens := rand.Int63n(initTokens.Int64() + 10)
+			h.delegate(delegators[ix[delegator]], validators[ix[v0]], tokens)
+		}
+		if j == 2 {
+			delegator := randomDelegator(i)
+			tokens := rand.Int63n(initTokens.Int64() + 10)
+			h.undelegate(delegators[ix[delegator]], validators[ix[v0]], tokens)
+		}
+		if j == 3 {
+			tokens := rand.Int63n(100)
+			power := sdk.TokensToConsensusPower(sdk.NewInt(tokens), sdk.DefaultPowerReduction)
+			height := rand.Int63n(h.ctx.BlockHeight() + 1)
+			factor := sdk.MustNewDecFromStr("0." + strconv.Itoa(rand.Intn(1000)))
+			h.slash(validators[ix[v0]], height, power, factor)
+		}
+		require.LessOrEqual(h.t, h.balance(delegators[ix[d1]]), initTokens.Int64())
+		h.matchValidatorStatus(validators[ix[v0]], types.Bonded)
+		if 1000 < i {
+			break
+		}
+	}
 }
 
 func TestAdversarial(t *testing.T) {
