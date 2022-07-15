5c9867b1ed825ee905d7305ca25cdb84e6c84325
Daniel <danwtisdall@gmail.com>
2022-07-15 12:31:01 +0100

    Reach slashing bug

--- x/ccv/provider/module.go
+++ x/ccv/provider/module.go
@@ -342,6 +342,8 @@ func (am AppModule) OnRecvPacket(
 	packet channeltypes.Packet,
 	_ sdk.AccAddress,
 ) ibcexported.Acknowledgement {
+	fmt.Println("CALL OnRecvPacket")
+
 	var (
 		ack            ibcexported.Acknowledgement
 		vscMaturedData ccv.VSCMaturedPacketData
@@ -352,6 +354,8 @@ func (am AppModule) OnRecvPacket(
 		ack = am.keeper.OnRecvVSCMaturedPacket(ctx, packet, vscMaturedData)
 	} else if err := ccv.ModuleCdc.UnmarshalJSON(packet.GetData(), &slashData); err == nil {
 		// handle SlashPacket
+		fmt.Println("CALL OnRecvSlashPacket")
+
 		ack = am.keeper.OnRecvSlashPacket(ctx, packet, slashData)
 	} else {
 		errAck := channeltypes.NewErrorAcknowledgement(fmt.Sprintf("cannot unmarshal CCV packet data: %s", err.Error()))
