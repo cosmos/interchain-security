bf7521e9902318d42ba0d0accfcd41f12ac2a5b8
Daniel <danwtisdall@gmail.com>
2022-07-18 15:39:43 +0100

    DELETE sdk dec test

--- x/ccv/difftest/mimic.py
+++
@@ -1,6 +0,0 @@
-vTokens = 27500
-vDelegatorShares = 50000.000000000000000000
-delShares = 18181.818181818181818181
-issued = int((delShares * vTokens) / vDelegatorShares)
-print(issued)
-# prints 9999
