4efe18aa5dee91b4fcee79c44c95f81da1380270
Shawn Marshall-Spitzbart <44221603+smarshall-spitzbart@users.noreply.github.com>
2022-07-25 10:39:05 -0700

    remove channel status

--- x/ccv/types/keys.go
+++ x/ccv/types/keys.go
@@ -10,18 +10,3 @@ const (
 
 	RouterKey = ModuleName
 )
-
-// Iota generated keys/byte prefixes (as a byte), supports 256 possible values
-const (
-	// ChannelStatusKeyPrefix is the byte prefix for storing the validation status of the CCV channel
-	ChannelStatusBytePrefix byte = iota
-)
-
-func ChannelStatusPrefix() []byte {
-	return []byte{ChannelStatusBytePrefix}
-}
-
-// ChannelStatusKey returns the key under which the Status of a consumer chain is stored.
-func ChannelStatusKey(channelID string) []byte {
-	return append(ChannelStatusPrefix(), []byte("/"+channelID)...)
-}
