676b458841bf426d420b7b93eb0553064fecbf84
Daniel <danwtisdall@gmail.com>
2022-08-19 12:38:31 +0100

    Comments gen.test.ts

--- diff-test/core/__tests__/gen.test.ts
+++ diff-test/core/__tests__/gen.test.ts
@@ -1,6 +1,10 @@
 import { gen } from '../src/main.js';
 
-describe('tests are working', () => {
+/**
+ * This test is useful to check how much coverage
+ * trace generation actually gets over the model.
+ */
+describe('generate traces', () => {
   it('dt', () => {
     gen(2);
     expect(true).toBeTruthy();
