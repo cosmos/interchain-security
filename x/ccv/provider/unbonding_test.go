9e13c936e1d9ce06784ebc39da70a68618eb0685
Shawn Marshall-Spitzbart <44221603+smarshall-spitzbart@users.noreply.github.com>
2022-08-22 13:16:28 -0700

    e2e test

--- x/ccv/provider/unbonding_test.go
+++ x/ccv/provider/unbonding_test.go
@@ -236,23 +236,67 @@ func (s *ProviderTestSuite) TestUnbondingNoConsumer() {
 	// - check that half the coins have been returned
 	s.Require().True(getBalance(s, s.providerCtx(), delAddr).Equal(initBalance.Sub(bondAmt.Quo(sdk.NewInt(2)))))
 }
+
 func (s *ProviderTestSuite) TestRedelegationNoConsumer() {
+	providerKeeper := s.providerChain.App.(*appProvider.App).ProviderKeeper
+	stakingKeeper := s.providerChain.App.(*appProvider.App).StakingKeeper
+
 	// remove the consumer chain, which was already registered during setup
-	s.providerChain.App.(*appProvider.App).ProviderKeeper.DeleteConsumerClientId(s.providerCtx(), s.consumerChain.ChainID)
+	providerKeeper.DeleteConsumerClientId(s.providerCtx(), s.consumerChain.ChainID)
 
-	bondAmt := sdk.NewInt(1000000000)
+	// Setup delegator and bond amount
+	bondAmt := sdk.NewInt(10000000)
 	delAddr := s.providerChain.SenderAccount.GetAddress()
-	// Delegate to first validator
-	_, shares, validatorAddr := delegate(s, delAddr, bondAmt)
 
-	// TODO: deal with denoms
-	s.Require().Equal(shares.Mul(sdk.NewDec(1000000)), bondAmt.ToDec())
+	// Delegate to validator 0
+	_, val0Addr := s.getVal(0)
+	val0TokensBefore := stakingKeeper.Validator(s.providerCtx(), val0Addr).GetBondedTokens()
+	val0PowerBefore := stakingKeeper.GetLastValidatorPower(s.providerCtx(), val0Addr)
+	_, shares, _ := delegate(s, delAddr, bondAmt)
+
+	// Assert expected amount was bonded to validator 0
+	val0TokensAfter := stakingKeeper.Validator(s.providerCtx(), val0Addr).GetBondedTokens()
+	s.Require().Equal(val0TokensAfter.Sub(val0TokensBefore), bondAmt)
+
+	s.providerChain.NextBlock()
+
+	// Check validator power
+	val0PowerAfter := stakingKeeper.GetLastValidatorPower(s.providerCtx(), val0Addr)
+	s.Require().Greater(val0PowerAfter, val0PowerBefore)
 
 	// Pick new validator for redelegation
-	_, newValidatorAddr := s.getVal(1)
+	_, val1Addr := s.getVal(1)
+	val1TokensBefore := stakingKeeper.Validator(s.providerCtx(), val1Addr).GetBondedTokens()
+	val1PowerBefore := stakingKeeper.GetLastValidatorPower(s.providerCtx(), val1Addr)
 
 	// redelegate shares to new validator
-	redelegate(s, delAddr, validatorAddr, newValidatorAddr, shares)
+	redelegate(s, delAddr,
+		val0Addr, // Source
+		val1Addr, // Dest
+		shares)
+
+	// Assert expected amount was delegated to validator 1
+	val1TokensAfter := stakingKeeper.Validator(s.providerCtx(), val1Addr).GetBondedTokens()
+	s.Require().Equal(val1TokensAfter.Sub(val1TokensBefore), bondAmt)
+
+	// Assert delegated tokens amount returned to original value for validator 0
+	s.Require().Equal(val0TokensBefore, stakingKeeper.Validator(s.providerCtx(), val0Addr).GetBondedTokens())
+
+	s.providerChain.NextBlock()
+
+	// 1 redelegation record should exist with appropriate maturation time
+	redelegations := stakingKeeper.GetRedelegations(s.providerCtx(), delAddr, 2)
+	s.Require().Len(redelegations, 1)
+	redelgation := redelegations[0]
+	providerUnbondingPeriod := stakingKeeper.UnbondingTime(s.providerCtx())
+	s.Require().Equal(
+		s.providerCtx().BlockTime().Add(providerUnbondingPeriod),
+		redelgation.Entries[0].CompletionTime)
+
+	// Check validator powers
+	val1PowerAfter := stakingKeeper.GetLastValidatorPower(s.providerCtx(), val1Addr)
+	s.Require().Greater(val1PowerAfter, val1PowerBefore)
+	s.Require().Equal(val0PowerBefore, stakingKeeper.GetLastValidatorPower(s.providerCtx(), val0Addr))
 }
 
 func getBalance(s *ProviderTestSuite, providerCtx sdk.Context, delAddr sdk.AccAddress) sdk.Int {
