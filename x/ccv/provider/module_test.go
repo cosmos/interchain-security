01da1460259f605ab949e5c530521c9105b1b11d
Shawn Marshall-Spitzbart <44221603+smarshall-spitzbart@users.noreply.github.com>
2022-09-06 17:40:49 -0700

    test init genesis

---
+++ x/ccv/provider/module_test.go
@@ -0,0 +1,147 @@
+package provider_test
+
+import (
+	"testing"
+
+	capabilitytypes "github.com/cosmos/cosmos-sdk/x/capability/types"
+	host "github.com/cosmos/ibc-go/v3/modules/core/24-host"
+	testkeeper "github.com/cosmos/interchain-security/testutil/keeper"
+	"github.com/cosmos/interchain-security/x/ccv/provider"
+	"github.com/cosmos/interchain-security/x/ccv/provider/types"
+	ccv "github.com/cosmos/interchain-security/x/ccv/types"
+	"github.com/stretchr/testify/require"
+
+	"github.com/golang/mock/gomock"
+)
+
+// Tests the provider's InitGenesis implementation against the spec:
+// https://github.com/cosmos/ibc/blob/main/spec/app/ics-028-cross-chain-validation/methods.md#ccv-pcf-initg1
+func TestInitGenesis(t *testing.T) {
+
+	type testCase struct {
+		name string
+		// Whether port capability is bound already
+		isBound bool
+		// Provider's storage of consumer state to test against
+		consumerStates []types.ConsumerState
+	}
+
+	tests := []testCase{
+		{
+			name:           "already bound port, no consumer states",
+			isBound:        true,
+			consumerStates: []types.ConsumerState{},
+		},
+		{
+			name:           "no bound port, no consumer states",
+			isBound:        false,
+			consumerStates: []types.ConsumerState{},
+		},
+		{
+			name:    "no bound port, multiple consumer states",
+			isBound: false,
+			consumerStates: []types.ConsumerState{
+				{
+					ChainId:   "chainId1",
+					ChannelId: "channelIdToChain1",
+				},
+				{
+					ChainId:   "chainId2",
+					ChannelId: "channelIdToChain2",
+				},
+				{
+					ChainId:   "chainId3",
+					ChannelId: "channelIdToChain3",
+				},
+			},
+		},
+		{
+			name:    "already bound port, one consumer state",
+			isBound: true,
+			consumerStates: []types.ConsumerState{
+				{
+					ChainId:   "chainId77",
+					ChannelId: "channelIdToChain77",
+				},
+			},
+		},
+	}
+
+	// TODO: Test "invalid channel id"?
+
+	for _, tc := range tests {
+		//
+		// Setup
+		//
+		ctrl := gomock.NewController(t)
+		cdc, storeKey, paramsSubspace, ctx := testkeeper.SetupInMemKeeper(t)
+
+		mockScopedKeeper := testkeeper.NewMockScopedKeeper(ctrl)
+		mockPortKeeper := testkeeper.NewMockPortKeeper(ctrl)
+
+		providerKeeper := testkeeper.GetProviderKeeperWithMocks(
+			cdc,
+			storeKey,
+			paramsSubspace,
+			mockScopedKeeper,
+			testkeeper.NewMockChannelKeeper(ctrl),
+			mockPortKeeper,
+			testkeeper.NewMockConnectionKeeper(ctrl),
+			testkeeper.NewMockClientKeeper(ctrl),
+			testkeeper.NewMockStakingKeeper(ctrl),
+			testkeeper.NewMockSlashingKeeper(ctrl),
+			testkeeper.NewMockAccountKeeper(ctrl),
+		)
+
+		appModule := provider.NewAppModule(&providerKeeper)
+		genState := types.NewGenesisState(tc.consumerStates, types.DefaultParams())
+		jsonBytes := cdc.MustMarshalJSON(genState)
+
+		//
+		// Assert mocked logic before method executes
+		//
+		orderedCalls := []*gomock.Call{
+			mockScopedKeeper.EXPECT().GetCapability(
+				ctx, host.PortPath(ccv.ProviderPortID),
+			).Return(
+				&capabilitytypes.Capability{},
+				tc.isBound, // Capability is returned successfully if port capability is already bound
+			),
+		}
+
+		// If port capability is not already bound, port will be binded and capability claimed.
+		if !tc.isBound {
+			dummyCap := &capabilitytypes.Capability{}
+
+			orderedCalls = append(orderedCalls,
+				mockPortKeeper.EXPECT().BindPort(ctx, ccv.ProviderPortID).Return(dummyCap),
+				mockScopedKeeper.EXPECT().ClaimCapability(
+					ctx, dummyCap, host.PortPath(ccv.ProviderPortID)).Return(nil),
+			)
+		}
+
+		gomock.InOrder(orderedCalls...)
+
+		//
+		// Execute method, then assert expected results
+		//
+		valUpdates := appModule.InitGenesis(ctx, cdc, jsonBytes)
+
+		numStatesCounted := 0
+		for _, state := range tc.consumerStates {
+			numStatesCounted += 1
+			channelID, found := providerKeeper.GetChainToChannel(ctx, state.ChainId)
+			require.True(t, found)
+			require.Equal(t, state.ChannelId, channelID)
+
+			chainID, found := providerKeeper.GetChannelToChain(ctx, state.ChannelId)
+			require.True(t, found)
+			require.Equal(t, state.ChainId, chainID)
+		}
+		require.Equal(t, len(tc.consumerStates), numStatesCounted)
+
+		require.Empty(t, valUpdates, "InitGenesis should return no validator updates")
+
+		ctrl.Finish()
+	}
+}
