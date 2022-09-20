46e54f965c7c20bce6fb865a1cd165d267546bbc
Daniel <danwtisdall@gmail.com>
2022-09-20 13:59:59 +0100

    Write more code for KeyDelegation

--- x/ccv/provider/keeper/key_delegation.go
+++ x/ccv/provider/keeper/key_delegation.go
@@ -29,10 +29,12 @@ func MakeKeyDelegation() KeyDelegation {
 	}
 }
 
-func (m *KeyDelegation) SetKey(lk LK, fk FK) {
+func (m *KeyDelegation) SetForeignKey(lk LK, fk FK) {
 	m.localKeyToCurrentForeignKey[lk] = fk
 	if u, ok := m.localKeyToLastUpdate[lk]; ok {
 		if 0 < u.power {
+			// If last update had positive power then the consumer is aware of the old key
+			// so a deletion update must be sent.
 			m.localKeysForWhichUpdateMustBeSent = append(m.localKeysForWhichUpdateMustBeSent, lk)
 		}
 	}
@@ -46,7 +48,50 @@ func (m *KeyDelegation) GetLocalKey(fk FK) (LK, error) {
 	}
 }
 
+func (m *KeyDelegation) Prune(mostRecentlyMaturedVscid VSCID) {
+	toRemove := []FK{}
+	for fk, vscid := range m.foreignKeyToVscidWhenLastSent {
+		if vscid <= mostRecentlyMaturedVscid {
+			toRemove = append(toRemove, fk)
+		}
+	}
+	for _, fk := range toRemove {
+		delete(m.foreignKeyToVscidWhenLastSent, fk)
+		delete(m.foreignKeyToLocalKey, fk)
+	}
+}
+
 func (m *KeyDelegation) ComputeUpdates(vscid VSCID, localUpdates []update) (foreignUpdates []update) {
 	foreignUpdates = []update{}
 
+	// Create any updates for validators whose power did not change
+	for _, lk := range m.localKeysForWhichUpdateMustBeSent {
+		currKey := m.localKeyToCurrentForeignKey[lk]
+		u := m.localKeyToLastUpdate[lk]
+		// Create an update which will delete the validator for the old key
+		foreignUpdates = append(foreignUpdates, update{key: u.key, power: 0})
+		// Create an update which will add the validator for the new key
+		foreignUpdates = append(foreignUpdates, update{key: currKey, power: u.power})
+	}
+	m.localKeysForWhichUpdateMustBeSent = []LK{}
+
+	// Create any updates for validators whose powers did change
+	for _, u := range localUpdates {
+		// Check if the consumer has an old key
+		if lastU, ok := m.localKeyToLastUpdate[u.key]; ok {
+			// Create an update which will delete the validator for the old key
+			foreignUpdates = append(foreignUpdates, update{key: lastU.key, power: 0})
+		}
+		currKey := m.localKeyToCurrentForeignKey[u.key]
+		// Create an update which will add/update the validator for the current key
+		foreignUpdates = append(foreignUpdates, update{key: currKey, power: u.power})
+	}
+
+	// Update internal bookkeeping
+	for _, u := range foreignUpdates {
+		m.foreignKeyToVscidWhenLastSent[u.key] = vscid
+		m.localKeyToLastUpdate[m.foreignKeyToLocalKey[u.key]] = u
+	}
+
+	return foreignUpdates
 }
