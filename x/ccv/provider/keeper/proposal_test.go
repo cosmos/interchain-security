62db93d48aa1603849b1d6d79f695eae2f68a7cf
Shawn Marshall-Spitzbart <44221603+smarshall-spitzbart@users.noreply.github.com>
2022-10-04 15:34:57 -0700

    Clarify proposal nomenclature (#370)
    * comments
    
    * comments
    
    Co-authored-by: Jehan <jehan.tremback@gmail.com>

--- x/ccv/provider/keeper/proposal_test.go
+++ x/ccv/provider/keeper/proposal_test.go
@@ -224,7 +224,7 @@ func TestPendingConsumerAdditionPropDeletion(t *testing.T) {
 	ctx = ctx.WithBlockTime(time.Now().UTC())
 
 	propsToExecute := providerKeeper.ConsumerAdditionPropsToExecute(ctx)
-	// Delete consumer addition proposals, same as what would be done by IteratePendingConsumerAdditionProps
+	// Delete consumer addition proposals, same as what would be done by BeginBlockInit
 	providerKeeper.DeletePendingConsumerAdditionProps(ctx, propsToExecute...)
 	numDeleted := 0
 	for _, tc := range testCases {
@@ -496,7 +496,7 @@ func TestPendingConsumerRemovalPropDeletion(t *testing.T) {
 	ctx = ctx.WithBlockTime(time.Now().UTC())
 
 	propsToExecute := providerKeeper.ConsumerRemovalPropsToExecute(ctx)
-	// Delete consumer removal proposals, same as what would be done by IterateMatureConsumerRemovalProps
+	// Delete consumer removal proposals, same as what would be done by BeginBlockCCR
 	providerKeeper.DeletePendingConsumerRemovalProps(ctx, propsToExecute...)
 	numDeleted := 0
 	for _, tc := range testCases {
