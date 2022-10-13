05daac5943a3dffdf425f927b5ef0f56e6efd1d6
Shawn Marshall-Spitzbart <44221603+smarshall-spitzbart@users.noreply.github.com>
2022-10-13 12:12:21 -0700

    Create params_test.go

---
+++ x/ccv/consumer/types/params_test.go
@@ -0,0 +1,33 @@
+package types_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	consumertypes "github.com/cosmos/interchain-security/x/ccv/consumer/types"
+)
+
+// Tests the validation of consumer params that happens at genesis
+func TestValidateParams(t *testing.T) {
+
+	testCases := []struct {
+		name    string
+		params  consumertypes.Params
+		expPass bool
+	}{
+		{"default params", consumertypes.DefaultParams(), true},
+		{"custom valid params", consumertypes.NewParams(true, 5, "", "", 5), true},
+		{"custom invalid params, block per dist transmission", consumertypes.NewParams(true, -5, "", "", 5), false},
+		{"custom invalid params, ccv timeout", consumertypes.NewParams(true, 5, "", "", -5), false},
+	}
+
+	for _, tc := range testCases {
+		err := tc.params.Validate()
+		if tc.expPass {
+			require.Nil(t, err, "expected error to be nil for test case: %s", tc.name)
+		} else {
+			require.NotNil(t, err, "expected error but got nil for test case: %s", tc.name)
+		}
+	}
+}
