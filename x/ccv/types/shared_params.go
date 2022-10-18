27e55b4f5cb2a087ce002d42dc8d99cf1154f0c5
Shawn Marshall-Spitzbart <44221603+smarshall-spitzbart@users.noreply.github.com>
2022-10-18 04:48:26 -0700

    Consistent params validation (#394)
    * changes
    
    * Update shared_params.go
    
    * Create params_test.go
    
    * corrections
    
    * remove println
    
    * Update x/ccv/consumer/types/params_test.go
    
    Co-authored-by: Simon Noetzlin <simon.ntz@gmail.com>
    
    * Update x/ccv/consumer/types/params_test.go
    
    Co-authored-by: Simon Noetzlin <simon.ntz@gmail.com>
    
    * fixing TestValidateParams
    
    Co-authored-by: Marius Poke <marius.poke@posteo.de>
    Co-authored-by: Simon Noetzlin <simon.ntz@gmail.com>

--- x/ccv/types/shared_params.go
+++ x/ccv/types/shared_params.go
@@ -3,6 +3,9 @@ package types
 import (
 	fmt "fmt"
 	"time"
+
+	sdktypes "github.com/cosmos/cosmos-sdk/types"
+	ibchost "github.com/cosmos/ibc-go/v3/modules/core/24-host"
 )
 
 const (
@@ -14,13 +17,61 @@ var (
 	KeyCCVTimeoutPeriod = []byte("CcvTimeoutPeriod")
 )
 
-func ValidateCCVTimeoutPeriod(i interface{}) error {
+func ValidateDuration(i interface{}) error {
 	period, ok := i.(time.Duration)
 	if !ok {
 		return fmt.Errorf("invalid parameter type: %T", i)
 	}
 	if period <= time.Duration(0) {
-		return fmt.Errorf("ibc timeout period is not positive")
+		return fmt.Errorf("duration must be positive")
 	}
 	return nil
+}
+
+func ValidateBool(i interface{}) error {
+	if _, ok := i.(bool); !ok {
+		return fmt.Errorf("invalid parameter type: %T", i)
+	}
+	return nil
+}
+
+func ValidateInt64(i interface{}) error {
+	if _, ok := i.(int64); !ok {
+		return fmt.Errorf("invalid parameter type: %T", i)
+	}
+	return nil
+}
+
+func ValidatePositiveInt64(i interface{}) error {
+	if err := ValidateInt64(i); err != nil {
+		return err
+	}
+	if i.(int64) <= int64(0) {
+		return fmt.Errorf("int must be positive")
+	}
+	return nil
+}
+
+func ValidateString(i interface{}) error {
+	if _, ok := i.(string); !ok {
+		return fmt.Errorf("invalid parameter type: %T", i)
+	}
+	return nil
+}
+
+func ValidateChannelIdentifier(i interface{}) error {
+	value, ok := i.(string)
+	if !ok {
+		return fmt.Errorf("invalid parameter type: %T", i)
+	}
+	return ibchost.ChannelIdentifierValidator(value)
+}
+
+func ValidateBech32(i interface{}) error {
+	value, ok := i.(string)
+	if !ok {
+		return fmt.Errorf("invalid parameter type: %T", i)
+	}
+	_, err := sdktypes.AccAddressFromBech32(value)
+	return err
 }
