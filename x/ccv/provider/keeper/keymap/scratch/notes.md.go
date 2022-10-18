d745e2f710a78cf6ef9ba9b2eacf716610cd7497
Daniel <danwtisdall@gmail.com>
2022-10-18 12:57:12 -0500

    notes

--- x/ccv/provider/keeper/keymap/scratch/notes.md.go
+++ x/ccv/provider/keeper/keymap/scratch/notes.md.go
@@ -45,3 +45,19 @@ func GetConsAddress(pubkey cryptotypes.PubKey) ConsAddress {
 }
 
 // inside keymap to implement the reverse lookup
+
+
+/////////////////////// OK
+// When a consumer receives an update, it receives the tendermint public key
+// it does THIS
+addr := utils.GetChangePubKeyAddress(change)
+consAddr := sdk.ConsAddress(addr)
+// to get the cons address
+
+// if I change keymap to store a map of cons addresses, then
+// the slash will send a cons address
+// the reverse cons address can be looked up
+
+
+// in order to send updates, convert the output of the staking module
+// to the address using the above function
