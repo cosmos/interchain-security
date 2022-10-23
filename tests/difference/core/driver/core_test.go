61650c82aad368a55742cc671262b77f45dc54b4
Daniel <danwtisdall@gmail.com>
2022-10-23 15:12:21 +0100

    debuggin

--- tests/difference/core/driver/core_test.go
+++ tests/difference/core/driver/core_test.go
@@ -428,7 +428,8 @@ func (s *CoreSuite) TestTraces() {
 	s.traces = Traces{
 		Data: LoadTraces("traces.json"),
 	}
-	// s.traces.Data = []TraceData{s.traces.Data[69]}
+	s.traces.Data = []TraceData{s.traces.Data[1]}
+	// s.traces.Data = s.traces.Data[:100]
 	for i := range s.traces.Data {
 		s.Run(fmt.Sprintf("Trace num: %d", i), func() {
 			// Setup a new pair of chains for each trace
