48bbdfe3a7742b8054456630491deb310276f64e
Daniel <danwtisdall@gmail.com>
2022-11-01 18:03:09 +0000

    Squashed commit of the following:
    commit cfc4569384a8aa234e2b7ccea49bee086774520c
    Author: Shawn Marshall-Spitzbart <44221603+smarshall-spitzbart@users.noreply.github.com>
    Date:   Tue Nov 1 08:10:52 2022 -0700
    
        Remove unneeded const variables (#418)
    
        * wip
    
        * diff test patch (#412)
    
        Patch
    
        Co-authored-by: Daniel <danwtisdall@gmail.com>
    
        * add back setter
    
        * comment
    
        * remove compute consumer unbonding
    
        * Update params.go
    
        Co-authored-by: Daniel T <30197399+danwt@users.noreply.github.com>
        Co-authored-by: Daniel <danwtisdall@gmail.com>
    
    commit 602b7ed47785ba3d820a01f336a2dc863b9b8bd7
    Author: Shawn Marshall-Spitzbart <44221603+smarshall-spitzbart@users.noreply.github.com>
    Date:   Fri Oct 28 08:34:26 2022 -0700
    
        Consumer Unbonding As Param (#410)
    
        Co-authored-by: Daniel T <30197399+danwt@users.noreply.github.com>
        Co-authored-by: Daniel <danwtisdall@gmail.com>

--- tests/difference/core/driver/setup.go
+++ tests/difference/core/driver/setup.go
@@ -489,6 +489,7 @@ func (b *Builder) createConsumerGenesis(tmConfig *ibctesting.TendermintConfig) *
 		consumertypes.DefaultTransferTimeoutPeriod,
 		consumertypes.DefaultConsumerRedistributeFrac,
 		consumertypes.DefaultHistoricalEntries,
+		consumertypes.DefaultConsumerUnbondingPeriod,
 	)
 
 	{
@@ -534,9 +535,7 @@ func (b *Builder) doIBCHandshake() {
 
 	// Configure and create the consumer Client
 	tmConfig := b.path.EndpointB.ClientConfig.(*ibctesting.TendermintConfig)
-	// TODO: This is intentionally set to unbonding period for P (provider)
-	// TODO: Not sure why it breaks without this.
-	tmConfig.UnbondingPeriod = b.initState.UnbondingP
+	tmConfig.UnbondingPeriod = b.initState.UnbondingC
 	tmConfig.TrustingPeriod = b.initState.Trusting
 	tmConfig.MaxClockDrift = b.initState.MaxClockDrift
 	err = b.path.EndpointB.CreateClient()
@@ -714,11 +713,11 @@ func (b *Builder) build() {
 	// Create a simulated network link link
 	b.createLink()
 
+	// Set the unbonding time on the consumer to the model value
+	b.consumerKeeper().SetUnbondingPeriod(b.ctx(C), b.initState.UnbondingC)
+
 	// Establish connection, channel
 	b.doIBCHandshake()
-
-	// Set the unbonding time on the consumer to the model value
-	b.consumerKeeper().SetUnbondingTime(b.ctx(C), b.initState.UnbondingC)
 
 	// Send an empty VSC packet from the provider to the consumer to finish
 	// the handshake. This is necessary because the model starts from a
@@ -774,6 +773,10 @@ func (b *Builder) build() {
 	b.updateClient(b.chainID(C))
 }
 
+// The state of the data returned is equivalent to the state of two chains
+// after a full handshake, but the precise order of steps used to reach the
+// state does not necessarily mimic the order of steps that happen in a
+// live scenario.
 func GetZeroState(suite *suite.Suite, initState InitState) (
 	*ibctesting.Path, []sdk.ValAddress, int64, int64) {
 	b := Builder{initState: initState, suite: suite}
