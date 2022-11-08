491a02abacde649e576308763919a033e7d8d4ff
Daniel <danwtisdall@gmail.com>
2022-11-08 12:36:15 +0000

    Deletes unused import in provider keeper test

--- x/ccv/provider/keeper/keeper_test.go
+++ x/ccv/provider/keeper/keeper_test.go
@@ -8,7 +8,6 @@ import (
 	"github.com/golang/mock/gomock"
 
 	cryptocodec "github.com/cosmos/cosmos-sdk/crypto/codec"
-	"github.com/cosmos/cosmos-sdk/crypto/keys/ed25519"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 
