e46c46bfaca280c284ca99dce5fa6cb0966eb4c2
mpoke <marius.poke@posteo.de>
2022-11-08 12:05:33 +0100

    add upgradeExpiredClient to e2e tests

--- tests/e2e/expired_client.go
+++ tests/e2e/expired_client.go
@@ -4,11 +4,10 @@ import (
 	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 	clienttypes "github.com/cosmos/ibc-go/v3/modules/core/02-client/types"
-	commitmenttypes "github.com/cosmos/ibc-go/v3/modules/core/23-commitment/types"
 	ibctm "github.com/cosmos/ibc-go/v3/modules/light-clients/07-tendermint/types"
 	ibctesting "github.com/cosmos/ibc-go/v3/testing"
+	ccv "github.com/cosmos/interchain-security/x/ccv/types"
 )
 
 // TestVSCPacketSendWithExpiredClient tests the providers sending VSCPackets
@@ -19,14 +18,13 @@ func (s *CCVTestSuite) TestVSCPacketSendExpiredClient() {
 	s.SetupCCVChannel()
 	s.SetupTransferChannel()
 
-	// upgrade the trusting period of the client to the consumer;
-	// as a result, we can expire this client w/o expiring the
-	// counterparty client
-	upgradeClient(s, Consumer, time.Hour)
+	cs, ok := s.providerApp.GetIBCKeeper().ClientKeeper.GetClientState(s.providerCtx(), s.path.EndpointB.ClientID)
+	s.Require().True(ok)
+	trustingPeriod := cs.(*ibctm.ClientState).TrustingPeriod
 
 	// increment time without updating the client;
 	// this will result in the client to the consumer to expire
-	s.coordinator.IncrementTimeBy(2 * time.Hour)
+	s.coordinator.IncrementTimeBy(trustingPeriod + time.Hour)
 
 	// bond some tokens on provider to change validator powers
 	bondAmt := sdk.NewInt(1000000)
@@ -60,89 +58,93 @@ func (s *CCVTestSuite) TestVSCPacketSendExpiredClient() {
 	s.Require().True(found, "no pending VSC packets found")
 	s.Require().Equal(2, len(packets), "unexpected number of pending VSC packets")
 
-	// TODO: upgrade expired client and check that the packets are sent
-	// see https://github.com/cosmos/ibc-go/blob/46e020640e66f9043c14c53a4d215a5b457d6703/modules/core/02-client/proposal_handler_test.go#L14
+	// upgrade expired client to the consumer
+	upgradeExpiredClient(s, Consumer)
+
+	// go to next block
+	s.providerChain.NextBlock()
+
+	// check that the packets are not in the list of pending VSC packets
+	_, found = providerKeeper.GetPendingVSCs(s.providerCtx(), s.consumerChain.ChainID)
+	s.Require().False(found, "pending VSC packets found")
+
+	// bond more tokens on provider to change validator powers
+	delegate(s, delAddr, bondAmt)
+
+	// send CCV packet to consumer
+	s.providerChain.NextBlock()
+
+	// Relay 3 VSC packet from provider to consumer
+	relayAllCommittedPackets(s, s.providerChain, s.path, ccv.ProviderPortID, s.path.EndpointB.ChannelID, 3)
+
+	// Increment time so that the unbonding period ends on the provider
+	incrementTimeByUnbondingPeriod(s, Provider)
+
+	// Relay 3 VSCMatured packet from consumer to provider
+	relayAllCommittedPackets(s, s.consumerChain, s.path, ccv.ConsumerPortID, s.path.EndpointA.ChannelID, 3)
 }
 
-func upgradeClient(s *CCVTestSuite, clientTo ChainType, trustingPeriod time.Duration) error {
-	var hostEndpoint *ibctesting.Endpoint
+// upgradeExpiredClient upgrades an expired client to `clientTo`
+func upgradeExpiredClient(s *CCVTestSuite, clientTo ChainType) {
+	subjectPath := s.path
+	substitutePath := ibctesting.NewPath(s.consumerChain, s.providerChain)
+	var subject, subjectCounterparty string
+	var hostNewEndpoint, targetNewEndpoint *ibctesting.Endpoint
 	var hostChain *ibctesting.TestChain
 	var targetChain *ibctesting.TestChain
 	if clientTo == Consumer {
-		hostEndpoint = s.path.EndpointB
+		subject = subjectPath.EndpointB.ClientID             // client to consumer
+		subjectCounterparty = subjectPath.EndpointA.ClientID // client to provider
+		hostNewEndpoint = substitutePath.EndpointB
+		targetNewEndpoint = substitutePath.EndpointA
 		hostChain = s.providerChain
 		targetChain = s.consumerChain
 	} else {
-		hostEndpoint = s.path.EndpointA
+		subject = subjectPath.EndpointA.ClientID             // client to provider
+		subjectCounterparty = subjectPath.EndpointB.ClientID // client to consumer
+		hostNewEndpoint = substitutePath.EndpointA
+		targetNewEndpoint = substitutePath.EndpointB
 		hostChain = s.consumerChain
 		targetChain = s.providerChain
 	}
-	clientState := hostEndpoint.GetClientState().(*ibctm.ClientState)
-	newClientHeight := clienttypes.NewHeight(0, clientState.GetLatestHeight().GetRevisionHeight()+4)
 
-	// new client state
-	upgradedClient := ibctm.NewClientState(
-		clientState.ChainId,
-		ibctm.DefaultTrustLevel,
-		trustingPeriod,
-		ibctesting.UnbondingPeriod,
-		ibctesting.MaxClockDrift,
-		newClientHeight,
-		commitmenttypes.GetSDKSpecs(),
-		ibctesting.UpgradePath,
-		false,
-		false,
-	)
-	// new consensus state
-	upgradedConsState := &ibctm.ConsensusState{
-		NextValidatorsHash: targetChain.GetContext().BlockHeader().NextValidatorsHash,
-	}
+	subjectClientState := hostChain.GetClientState(subject)
 
-	upgradedClientBz, err := clienttypes.MarshalClientState(hostChain.App.AppCodec(), upgradedClient)
+	// create substitute client with same unbonding period
+	hostTmConfig, ok := hostNewEndpoint.ClientConfig.(*ibctesting.TendermintConfig)
+	s.Require().True(ok)
+	hostTmConfig.UnbondingPeriod = subjectClientState.(*ibctm.ClientState).UnbondingPeriod
+	hostTmConfig.TrustingPeriod = subjectClientState.(*ibctm.ClientState).TrustingPeriod
+	targetTmConfig, ok := targetNewEndpoint.ClientConfig.(*ibctesting.TendermintConfig)
+	s.Require().True(ok)
+	subjectCounterpartyCS := targetChain.GetClientState(subjectCounterparty)
+	targetTmConfig.UnbondingPeriod = subjectCounterpartyCS.(*ibctm.ClientState).UnbondingPeriod
+	targetTmConfig.TrustingPeriod = subjectCounterpartyCS.(*ibctm.ClientState).TrustingPeriod
+	s.coordinator.SetupClients(substitutePath)
+	substitute := hostNewEndpoint.ClientID
+
+	// update substitute twice
+	hostNewEndpoint.UpdateClient()
+	hostNewEndpoint.UpdateClient()
+	substituteClientState := hostChain.GetClientState(substitute)
+
+	tmClientState, ok := subjectClientState.(*ibctm.ClientState)
+	s.Require().True(ok)
+	tmClientState.AllowUpdateAfterMisbehaviour = true
+	tmClientState.AllowUpdateAfterExpiry = true
+	tmClientState.FrozenHeight = tmClientState.LatestHeight
+	hostChain.App.GetIBCKeeper().ClientKeeper.SetClientState(hostChain.GetContext(), subject, tmClientState)
+
+	tmClientState, ok = substituteClientState.(*ibctm.ClientState)
+	s.Require().True(ok)
+	tmClientState.AllowUpdateAfterMisbehaviour = true
+	tmClientState.AllowUpdateAfterExpiry = true
+	hostChain.App.GetIBCKeeper().ClientKeeper.SetClientState(hostChain.GetContext(), substitute, tmClientState)
+
+	content := clienttypes.NewClientUpdateProposal(ibctesting.Title, ibctesting.Description, subject, substitute)
+
+	updateProp, ok := content.(*clienttypes.ClientUpdateProposal)
+	s.Require().True(ok)
+	err := hostChain.App.GetIBCKeeper().ClientKeeper.ClientUpdateProposal(hostChain.GetContext(), updateProp)
 	s.Require().NoError(err)
-	upgradedConsStateBz, err := clienttypes.MarshalConsensusState(hostChain.App.AppCodec(), upgradedConsState)
-	s.Require().NoError(err)
-
-	// zero custom fields and store in upgrade store
-	lastHeight := clienttypes.NewHeight(0, uint64(targetChain.GetContext().BlockHeight()+1))
-	s.consumerApp.GetUpgradeKeeper().SetUpgradedClient(
-		targetChain.GetContext(),
-		int64(lastHeight.GetRevisionHeight()),
-		upgradedClientBz,
-	)
-	s.consumerApp.GetUpgradeKeeper().SetUpgradedConsensusState(
-		targetChain.GetContext(),
-		int64(lastHeight.GetRevisionHeight()),
-		upgradedConsStateBz,
-	)
-
-	// commit upgrade store changes and update clients
-	s.coordinator.CommitBlock(targetChain)
-	err = hostEndpoint.UpdateClient()
-	s.Require().NoError(err)
-
-	cs, found := hostChain.App.GetIBCKeeper().ClientKeeper.GetClientState(hostChain.GetContext(), hostEndpoint.ClientID)
-	s.Require().True(found)
-
-	proofUpgradeClient, _ := targetChain.QueryUpgradeProof(
-		upgradetypes.UpgradedClientKey(int64(lastHeight.GetRevisionHeight())),
-		cs.GetLatestHeight().GetRevisionHeight(),
-	)
-	proofUpgradedConsState, _ := targetChain.QueryUpgradeProof(
-		upgradetypes.UpgradedConsStateKey(int64(lastHeight.GetRevisionHeight())),
-		cs.GetLatestHeight().GetRevisionHeight(),
-	)
-
-	msg, err := clienttypes.NewMsgUpgradeClient(
-		hostEndpoint.ClientID,
-		upgradedClient,
-		upgradedConsState,
-		proofUpgradeClient,
-		proofUpgradedConsState,
-		hostChain.SenderAccount.GetAddress().String(),
-	)
-	s.Require().NoError(err)
-
-	_, err = hostChain.SendMsgs(msg)
-	return err
 }
