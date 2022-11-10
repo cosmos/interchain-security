87c646253dd0b12d00f7764dd5435ffae33d59bf
Shawn Marshall-Spitzbart <44221603+smarshall-spitzbart@users.noreply.github.com>
2022-11-10 13:04:07 -0800

    final

--- testutil/ibc_testing_utils/setup.go
+++ testutil/ibc_testing_utils/setup.go
@@ -23,6 +23,7 @@ var (
 	provChainID = ibctesting.GetChainID(1)
 )
 
+// ProviderAppIniter implements ibctesting.AppIniter for a provider app
 func ProviderAppIniter() (ibctesting.TestingApp, map[string]json.RawMessage) {
 	encoding := cosmoscmd.MakeEncodingConfig(appProvider.ModuleBasics)
 	testApp := appProvider.New(log.NewNopLogger(), tmdb.NewMemDB(), nil, true, map[int64]bool{},
@@ -30,18 +31,20 @@ func ProviderAppIniter() (ibctesting.TestingApp, map[string]json.RawMessage) {
 	return testApp, appProvider.NewDefaultGenesisState(encoding.Marshaler)
 }
 
-func DemocracyConsumerAppIniter() (ibctesting.TestingApp, map[string]json.RawMessage) {
-	encoding := cosmoscmd.MakeEncodingConfig(appConsumerDemocracy.ModuleBasics)
-	testApp := appConsumerDemocracy.New(log.NewNopLogger(), tmdb.NewMemDB(), nil, true, map[int64]bool{},
-		simapp.DefaultNodeHome, 5, encoding, simapp.EmptyAppOptions{}).(ibctesting.TestingApp)
-	return testApp, appConsumerDemocracy.NewDefaultGenesisState(encoding.Marshaler)
-}
-
+// ConsumerAppIniter implements ibctesting.AppIniter for a consumer app
 func ConsumerAppIniter() (ibctesting.TestingApp, map[string]json.RawMessage) {
 	encoding := cosmoscmd.MakeEncodingConfig(appConsumer.ModuleBasics)
 	testApp := appConsumer.New(log.NewNopLogger(), tmdb.NewMemDB(), nil, true, map[int64]bool{},
 		simapp.DefaultNodeHome, 5, encoding, simapp.EmptyAppOptions{}).(ibctesting.TestingApp)
 	return testApp, appConsumer.NewDefaultGenesisState(encoding.Marshaler)
+}
+
+// DemocracyConsumerAppIniter implements ibctesting.AppIniter for a democracy consumer app
+func DemocracyConsumerAppIniter() (ibctesting.TestingApp, map[string]json.RawMessage) {
+	encoding := cosmoscmd.MakeEncodingConfig(appConsumerDemocracy.ModuleBasics)
+	testApp := appConsumerDemocracy.New(log.NewNopLogger(), tmdb.NewMemDB(), nil, true, map[int64]bool{},
+		simapp.DefaultNodeHome, 5, encoding, simapp.EmptyAppOptions{}).(ibctesting.TestingApp)
+	return testApp, appConsumerDemocracy.NewDefaultGenesisState(encoding.Marshaler)
 }
 
 // NewCoordinatorWithProvider initializes an IBC testing Coordinator with a properly setup provider
