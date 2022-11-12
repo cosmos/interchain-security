69596b6a5bb689044e5165b2c05330601b1b9753
Shawn Marshall-Spitzbart <44221603+smarshall-spitzbart@users.noreply.github.com>
2022-11-11 20:24:07 -0800

    sorry for the friday night emails

--- tests/e2e/instance_test.go
+++ tests/e2e/instance_test.go
@@ -3,12 +3,10 @@ package e2e_test
 import (
 	"testing"
 
-	ibctesting "github.com/cosmos/ibc-go/v3/testing"
 	appConsumer "github.com/cosmos/interchain-security/app/consumer"
 	appConsumerDemocracy "github.com/cosmos/interchain-security/app/consumer-democracy"
 	appProvider "github.com/cosmos/interchain-security/app/provider"
 	"github.com/cosmos/interchain-security/tests/e2e"
-	e2etestutil "github.com/cosmos/interchain-security/testutil/e2e"
 	ibctestingutils "github.com/cosmos/interchain-security/testutil/ibc_testing"
 	"github.com/stretchr/testify/suite"
 )
@@ -16,61 +14,32 @@ import (
 // This file can be used as an example e2e testing instance for any provider/consumer applications.
 // In the case of this repo, we're testing the dummy provider/consumer applications,
 // but to test any arbitrary app, one only needs to replicate this file and "specific_setup.go",
-// then pass in the appropriate callback to the testing suites. Note that any provider/consumer
-// applications must implement the interfaces defined in /testutil/e2e/interfaces.go to compile.
+// then pass in the appropriate types and parameters to the suite. Note that provider consumer
+// applications types must implement the interfaces defined in /testutil/e2e/interfaces.go to compile.
 
 // Executes the standard group of ccv tests against a consumer and provider app.go implementation.
 func TestCCVTestSuite(t *testing.T) {
 
-	ccvSuite := e2e.NewCCVTestSuite(
-		func(t *testing.T) (
-			*ibctesting.Coordinator,
-			*ibctesting.TestChain,
-			*ibctesting.TestChain,
-			e2etestutil.ProviderApp,
-			e2etestutil.ConsumerApp,
-		) {
-			// Instantiate the test coordinator.
-			coordinator := ibctesting.NewCoordinator(t, 0)
+	// Pass in concrete app types that implement the interfaces defined in /testutil/e2e/interfaces.go
+	ccvSuite := e2e.NewCCVTestSuite[*appProvider.App, *appConsumer.App](
+		// Pass in ibctesting.AppIniters for provider and consumer.
+		ibctestingutils.ProviderAppIniter, ibctestingutils.ConsumerAppIniter)
 
-			// Add provider to coordinator, store returned test chain and app.
-			// Concrete provider app type is passed to the generic function here.
-			provider, providerApp := ibctestingutils.AddProvider[*appProvider.App](
-				coordinator, t, ibctestingutils.ProviderAppIniter)
-
-			// Add specified number of consumers to coordinator, store returned test chains and apps.
-			// Concrete consumer app type is passed to the generic function here.
-			consumers, consumerApps := ibctestingutils.AddConsumers[*appConsumer.App](
-				coordinator, t, 1, ibctestingutils.ConsumerAppIniter)
-
-			// Pass variables to suite.
-			// TODO: accept multiple consumers here
-			return coordinator, provider, consumers[0], providerApp, consumerApps[0]
-		},
-	)
+	// Run tests
 	suite.Run(t, ccvSuite)
 }
 
 // TODO: Run the gov enabled consumer against the standard suite of tests: https://github.com/cosmos/interchain-security/issues/397
 
-// Executes a specialized group of tests specific to a democracy consumer, against a democracy consumer app.go implementation.
+// Executes a specialized group of tests specific to a democracy consumer,
+// against a democracy consumer app.go implementation.
 func TestConsumerDemocracyTestSuite(t *testing.T) {
-	democSuite := e2e.NewConsumerDemocracyTestSuite(
-		func(t *testing.T) (
-			*ibctesting.Coordinator,
-			*ibctesting.TestChain,
-			e2etestutil.DemocConsumerApp,
-		) {
-			// Instantiate the test coordinator
-			coordinator := ibctesting.NewCoordinator(t, 0)
 
-			// Add single democracy consumer to coordinator, store returned test chain and app.
-			democConsumer, democConsumerApp := ibctestingutils.AddDemocracyConsumer[*appConsumerDemocracy.App](
-				coordinator, t, ibctestingutils.DemocracyConsumerAppIniter)
+	// Pass in concrete app type that implement the interface defined in /testutil/e2e/interfaces.go
+	democSuite := e2e.NewConsumerDemocracyTestSuite[*appConsumerDemocracy.App](
+		// Pass in ibctesting.AppIniter for democracy consumer.
+		ibctestingutils.DemocracyConsumerAppIniter)
 
-			// Pass variables to suite.
-			return coordinator, democConsumer, democConsumerApp
-		},
-	)
+	// Run tests
 	suite.Run(t, democSuite)
 }
