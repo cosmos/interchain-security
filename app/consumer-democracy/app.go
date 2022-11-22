9ae735be468b2d4282be6e61738284a906649066
MSalopek <35486649+MSalopek@users.noreply.github.com>
2022-11-22 17:52:45 +0100

    refactor changes in expired consumer handling (#507)
    * refactor keeper and app
    
    * reintroduce slash and vsc matured events on consumer
    
    Co-authored-by: Shawn Marshall-Spitzbart <44221603+smarshall-spitzbart@users.noreply.github.com>

--- app/consumer-democracy/app.go
+++ app/consumer-democracy/app.go
@@ -785,11 +785,6 @@ func (app *App) GetE2eEvidenceKeeper() e2e.E2eEvidenceKeeper {
 	return app.EvidenceKeeper
 }
 
-// GetUpgradeKeeper implements the ConsumerApp interface.
-func (app *App) GetUpgradeKeeper() upgradekeeper.Keeper {
-	return app.UpgradeKeeper
-}
-
 // GetE2eStakingKeeper implements the ConsumerApp interface.
 func (app *App) GetE2eStakingKeeper() e2e.E2eStakingKeeper {
 	return app.StakingKeeper
