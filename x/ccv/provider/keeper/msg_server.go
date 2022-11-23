084ef6210e232d78a994bfa525c96b35d42f19b0
mpoke <marius.poke@posteo.de>
2022-11-23 11:46:03 +0100

    add MsgAssignConsumerKey

---
+++ x/ccv/provider/keeper/msg_server.go
@@ -0,0 +1,90 @@
+package keeper
+
+import (
+	"context"
+
+	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	"github.com/cosmos/interchain-security/x/ccv/provider/types"
+	ccvtypes "github.com/cosmos/interchain-security/x/ccv/types"
+	tmstrings "github.com/tendermint/tendermint/libs/strings"
+)
+
+type msgServer struct {
+	*Keeper
+}
+
+// NewMsgServerImpl returns an implementation of the bank MsgServer interface
+// for the provided Keeper.
+func NewMsgServerImpl(keeper *Keeper) types.MsgServer {
+	return &msgServer{Keeper: keeper}
+}
+
+var _ types.MsgServer = msgServer{}
+
+// CreateValidator defines a method for creating a new validator
+func (k msgServer) AssignConsumerKey(goCtx context.Context, msg *types.MsgAssignConsumerKey) (*types.MsgAssignConsumerKeyResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	// It is possible to assign keys for consumer chains that are not yet approved.
+	// TODO: In future, a mechanism will be added to limit assigning keys to chains
+	// which are approved or pending approval, only.
+	// Note that current attack potential is restricted because validators must sign
+	// the transaction, and the chainID size is limited.
+
+	// providerValidatorAddr, err := sdk.ValAddressFromBech32(msg.ProviderAddr)
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	// validator must already be registered
+	// validator, found := k.stakingKeeper.GetValidator(ctx, providerValidatorAddr)
+	// if !found {
+	// 	return nil, stakingtypes.ErrNoValidatorFound
+	// }
+	// providerTMPublicKey, err := validator.TmConsPublicKey()
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	consumerSDKPublicKey, ok := msg.ConsumerKey.GetCachedValue().(cryptotypes.PubKey)
+	if !ok {
+		return nil, sdkerrors.Wrapf(sdkerrors.ErrInvalidType, "Expecting cryptotypes.PubKey, got %T", consumerSDKPublicKey)
+	}
+
+	cp := ctx.ConsensusParams()
+	if cp != nil && cp.Validator != nil {
+		if !tmstrings.StringInSlice(consumerSDKPublicKey.Type(), cp.Validator.PubKeyTypes) {
+			return nil, sdkerrors.Wrapf(
+				stakingtypes.ErrValidatorPubKeyTypeNotSupported,
+				"got: %s, expected: %s", consumerSDKPublicKey.Type(), cp.Validator.PubKeyTypes,
+			)
+		}
+	}
+
+	// consumerTMPublicKey, err := cryptocodec.ToTmProtoPublicKey(consumerSDKPublicKey)
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	// TODO mpoke: replace with new proto
+	// err = k.KeyAssignment(ctx, msg.ChainId).SetProviderPubKeyToConsumerPubKey(
+	// 	providerTMPublicKey,
+	// 	consumerTMPublicKey,
+	// )
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	ctx.EventManager().EmitEvents(sdk.Events{
+		sdk.NewEvent(
+			ccvtypes.EventTypeAssignConsumerKey,
+			sdk.NewAttribute(ccvtypes.AttributeProviderValidatorAddress, msg.ProviderAddr),
+			sdk.NewAttribute(ccvtypes.AttributeConsumerConsensusPubKey, consumerSDKPublicKey.String()),
+		),
+	})
+
+	return &types.MsgAssignConsumerKeyResponse{}, nil
+}
