bc750f7fc5435d1034529a128ae1bee6cd15b4ab
Shawn Marshall-Spitzbart <44221603+smarshall-spitzbart@users.noreply.github.com>
2022-12-06 14:52:06 -0800

    add log

--- x/ccv/provider/keeper/proposal.go
+++ x/ccv/provider/keeper/proposal.go
@@ -222,6 +222,11 @@ func (k Keeper) StopConsumerChain(ctx sdk.Context, chainID string, lockUbd, clos
 	// global queue for this consumer.
 	k.DeletePendingSlashPacketEntriesForConsumer(ctx, chainID)
 
+	if k.GetPendingPacketDataSize(ctx, chainID) > 0 {
+		k.Logger(ctx).Info("There are pending slash packets queued,"+
+			" from a consumer that is being removed. Those slash packets will be thrown out!", "chainID", chainID)
+	}
+
 	// Remove all pending slash packets and vsc matured packets queued for this consumer.
 	// Note: queued VSC matured packets can be safely removed from the per-chain queue,
 	// since all unbonding operations for this consumer are release above.
