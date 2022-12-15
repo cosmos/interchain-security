44f12864524e9016cfa1bfcd13713ce0bf3d2c2b
Daniel <danwtisdall@gmail.com>
2022-12-15 12:35:53 +0000

    Modify sort

--- tests/pbt/sort_test.go
+++ tests/pbt/sort_test.go
@@ -1,60 +1,43 @@
 package pbt_test
 
 import (
-	"fmt"
-	"strconv"
+	"sort"
 	"testing"
 
+	"github.com/stretchr/testify/require"
 	"pgregory.net/rapid"
 )
 
-// ParseDate parses dates in the YYYY-MM-DD format.
-func ParseDate(s string) (int, int, int, error) {
-	if len(s) != 10 {
-		return 0, 0, 0, fmt.Errorf("%q has wrong length: %v instead of 10", s, len(s))
-	}
-
-	if s[4] != '-' || s[7] != '-' {
-		return 0, 0, 0, fmt.Errorf("'-' separators expected in %q", s)
-	}
-
-	y, err := strconv.Atoi(s[0:4])
-	if err != nil {
-		return 0, 0, 0, fmt.Errorf("failed to parse year: %v", err)
-	}
-
-	m, err := strconv.Atoi(s[6:7])
-	if err != nil {
-		return 0, 0, 0, fmt.Errorf("failed to parse month: %v", err)
-	}
-
-	d, err := strconv.Atoi(s[8:10])
-	if err != nil {
-		return 0, 0, 0, fmt.Errorf("failed to parse day: %v", err)
-	}
-
-	return y, m, d, nil
-}
-
-func testParseDate(t *rapid.T) {
-	y := rapid.IntRange(0, 9999).Draw(t, "y")
-	m := rapid.IntRange(1, 12).Draw(t, "m")
-	d := rapid.IntRange(1, 31).Draw(t, "d")
-
-	s := fmt.Sprintf("%04d-%02d-%02d", y, m, d)
-
-	y_, m_, d_, err := ParseDate(s)
-	if err != nil {
-		t.Fatalf("failed to parse date %q: %v", s, err)
-	}
-
-	if y_ != y || m_ != m || d_ != d {
-		t.Fatalf("got back wrong date: (%d, %d, %d)", y_, m_, d_)
+func fooSort(arr []int) {
+	for i := 0; i < len(arr); i++ {
+		for j := 0; j < len(arr); j++ {
+			if arr[j] < arr[i] {
+				arr[i], arr[j] = arr[j], arr[i]
+			}
+		}
 	}
 }
 
-// Rename to TestParseDate(t *testing.T) to make an actual (failing) test.
-func ExampleCheck_parseDate() {
-	var t *testing.T
-	rapid.Check(t, testParseDate)
+func testSort(t *rapid.T) {
+	data := rapid.SliceOf(rapid.Int()).Draw(t, "data")
+
+	fooSort(data)
+
+	sorted := func(data []int) bool {
+		for i := 0; i < len(data)-1; i++ {
+			if data[i] > data[i+1] {
+				return false
+			}
+		}
+		return true
+	}
+
+	require.True(t, sorted(data), "data is not sorted")
+
+	// Alternative using stdlib
+	require.True(t, sort.IntsAreSorted(data), "data is not sorted")
+}
+
+func TestSort(t *testing.T) {
+	rapid.Check(t, testSort)
 }
