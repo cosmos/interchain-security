ebc965194c3874adbdcfc74af931bce281a546ab
Daniel <danwtisdall@gmail.com>
2022-12-15 11:44:18 +0000

    Adds sort test

---
+++ tests/pbt/sort_test.go
@@ -0,0 +1,60 @@
+package pbt_test
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+
+	"pgregory.net/rapid"
+)
+
+// ParseDate parses dates in the YYYY-MM-DD format.
+func ParseDate(s string) (int, int, int, error) {
+	if len(s) != 10 {
+		return 0, 0, 0, fmt.Errorf("%q has wrong length: %v instead of 10", s, len(s))
+	}
+
+	if s[4] != '-' || s[7] != '-' {
+		return 0, 0, 0, fmt.Errorf("'-' separators expected in %q", s)
+	}
+
+	y, err := strconv.Atoi(s[0:4])
+	if err != nil {
+		return 0, 0, 0, fmt.Errorf("failed to parse year: %v", err)
+	}
+
+	m, err := strconv.Atoi(s[6:7])
+	if err != nil {
+		return 0, 0, 0, fmt.Errorf("failed to parse month: %v", err)
+	}
+
+	d, err := strconv.Atoi(s[8:10])
+	if err != nil {
+		return 0, 0, 0, fmt.Errorf("failed to parse day: %v", err)
+	}
+
+	return y, m, d, nil
+}
+
+func testParseDate(t *rapid.T) {
+	y := rapid.IntRange(0, 9999).Draw(t, "y")
+	m := rapid.IntRange(1, 12).Draw(t, "m")
+	d := rapid.IntRange(1, 31).Draw(t, "d")
+
+	s := fmt.Sprintf("%04d-%02d-%02d", y, m, d)
+
+	y_, m_, d_, err := ParseDate(s)
+	if err != nil {
+		t.Fatalf("failed to parse date %q: %v", s, err)
+	}
+
+	if y_ != y || m_ != m || d_ != d {
+		t.Fatalf("got back wrong date: (%d, %d, %d)", y_, m_, d_)
+	}
+}
+
+// Rename to TestParseDate(t *testing.T) to make an actual (failing) test.
+func ExampleCheck_parseDate() {
+	var t *testing.T
+	rapid.Check(t, testParseDate)
+}
