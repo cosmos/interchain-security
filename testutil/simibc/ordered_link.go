60d9b3acfb95831decfaa610896cdb544ff17722
Daniel <danwtisdall@gmail.com>
2023-01-12 10:57:35 +0000

    Revert to string in ordered link

--- testutil/simibc/ordered_link.go
+++ testutil/simibc/ordered_link.go
@@ -2,8 +2,6 @@ package simibc
 
 import channeltypes "github.com/cosmos/ibc-go/v3/modules/core/04-channel/types"
 
-type ChainID string
-
 // Ack represents and ack committed to block state
 type Ack struct {
 	Ack     []byte
@@ -21,32 +19,32 @@ type Packet struct {
 // allows fine-grained control over delivery of acks and packets
 // to mimic a real relaying relationship between two chains.
 type OrderedLink struct {
-	OutboxPackets map[ChainID][]Packet
-	OutboxAcks    map[ChainID][]Ack
+	OutboxPackets map[string][]Packet
+	OutboxAcks    map[string][]Ack
 }
 
 // MakeOrderedLink creates a new empty network link.
 func MakeOrderedLink() OrderedLink {
 	return OrderedLink{
-		OutboxPackets: map[ChainID][]Packet{},
-		OutboxAcks:    map[ChainID][]Ack{},
+		OutboxPackets: map[string][]Packet{},
+		OutboxAcks:    map[string][]Ack{},
 	}
 }
 
 // AddPacket adds an outbound packet from the sender to the counterparty.
-func (n OrderedLink) AddPacket(sender ChainID, packet channeltypes.Packet) {
+func (n OrderedLink) AddPacket(sender string, packet channeltypes.Packet) {
 	n.OutboxPackets[sender] = append(n.OutboxPackets[sender], Packet{packet, 0})
 }
 
 // AddAck adds an outbound ack, for future delivery to the sender of the packet
 // being acked.
-func (n OrderedLink) AddAck(sender ChainID, ack []byte, packet channeltypes.Packet) {
+func (n OrderedLink) AddAck(sender string, ack []byte, packet channeltypes.Packet) {
 	n.OutboxAcks[sender] = append(n.OutboxAcks[sender], Ack{ack, packet, 0})
 }
 
 // ConsumePackets returns the first num packets with 2 or more commits. Returned
 // packets are removed from the outbox and will not be returned again (consumed).
-func (n OrderedLink) ConsumePackets(sender ChainID, num int) []Packet {
+func (n OrderedLink) ConsumePackets(sender string, num int) []Packet {
 	ret := []Packet{}
 	sz := len(n.OutboxPackets[sender])
 	if sz < num {
@@ -65,7 +63,7 @@ func (n OrderedLink) ConsumePackets(sender ChainID, num int) []Packet {
 
 // ConsumerAcks returns the first num packets with 2 or more commits. Returned
 // acks are removed from the outbox and will not be returned again (consumed).
-func (n OrderedLink) ConsumeAcks(sender ChainID, num int) []Ack {
+func (n OrderedLink) ConsumeAcks(sender string, num int) []Ack {
 	ret := []Ack{}
 	sz := len(n.OutboxAcks[sender])
 	if sz < num {
@@ -90,7 +88,7 @@ func (n OrderedLink) ConsumeAcks(sender ChainID, num int) []Ack {
 //   - 1st commit is necessary for the packet to included in the block
 //   - 2nd commit is necessary because in practice the ibc light client
 //     needs to have block h + 1 to be able to verify the packet in block h.
-func (n OrderedLink) Commit(sender ChainID) {
+func (n OrderedLink) Commit(sender string) {
 	for i := range n.OutboxPackets[sender] {
 		n.OutboxPackets[sender][i].Commits += 1
 	}
