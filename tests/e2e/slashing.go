69f832444fd8e881da5b629016a77aef926ca4e2
Shawn <44221603+smarshall-spitzbart@users.noreply.github.com>
2023-02-09 10:59:08 -0800

    nit

--- tests/e2e/slashing.go
+++ tests/e2e/slashing.go
@@ -46,7 +46,7 @@ func (s *CCVTestSuite) TestRelayAndApplyDowntimePacket() {
 	s.Require().NoError(err)
 	pubkey, err := cryptocodec.FromTmProtoPublicKey(val.GetPubKey())
 	s.Require().Nil(err)
-	consumerConsAddr := providertypes.NewConsumerConsAddress(sdk.ConsAddress(pubkey.Address()))
+	consumerConsAddr := providertypes.NewConsumerConsAddress(sdk.GetConsAddress(pubkey))
 	// map consumer consensus address to provider consensus address
 	providerConsAddr, found := providerKeeper.GetValidatorByConsumerAddr(
 		s.providerCtx(),
