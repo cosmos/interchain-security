1269163b9c75885ac4df83914b5e166a79de6a62
Shawn <44221603+smarshall-spitzbart@users.noreply.github.com>
2023-03-10 15:35:14 -0800

    remove democracy staking changes

--- x/ccv/democracy/staking/module.go
+++ x/ccv/democracy/staking/module.go
@@ -29,22 +29,20 @@ type AppModule struct {
 	// embed the Cosmos SDK's x/staking AppModule
 	staking.AppModule
 
-	keeper         keeper.Keeper
-	accKeeper      types.AccountKeeper
-	bankKeeper     types.BankKeeper
-	consumerKeeper ConsumerKeeper
+	keeper     keeper.Keeper
+	accKeeper  types.AccountKeeper
+	bankKeeper types.BankKeeper
 }
 
 // NewAppModule creates a new AppModule object using the native x/staking module
 // AppModule constructor.
-func NewAppModule(cdc codec.Codec, keeper keeper.Keeper, ak types.AccountKeeper, bk types.BankKeeper, ck ConsumerKeeper) AppModule {
+func NewAppModule(cdc codec.Codec, keeper keeper.Keeper, ak types.AccountKeeper, bk types.BankKeeper) AppModule {
 	stakingAppMod := staking.NewAppModule(cdc, keeper, ak, bk)
 	return AppModule{
-		AppModule:      stakingAppMod,
-		keeper:         keeper,
-		accKeeper:      ak,
-		bankKeeper:     bk,
-		consumerKeeper: ck,
+		AppModule:  stakingAppMod,
+		keeper:     keeper,
+		accKeeper:  ak,
+		bankKeeper: bk,
 	}
 }
 
@@ -52,14 +50,14 @@ func NewAppModule(cdc codec.Codec, keeper keeper.Keeper, ak types.AccountKeeper,
 // however, it returns no validator updates as validators are tracked via the
 // consumer chain's x/cvv/consumer module and so this module is not responsible
 // for returning the initial validator set.
+//
+// Note: InitGenesis is not called during the soft upgrade of a module (as a part of a migration from sovereign -> consumer chain),
+// so there is no special handling needed in this method for a consumer being in the pre-CCV state.
 func (am AppModule) InitGenesis(ctx sdk.Context, cdc codec.JSONCodec, data json.RawMessage) []abci.ValidatorUpdate {
 	var genesisState types.GenesisState
 
 	cdc.MustUnmarshalJSON(data, &genesisState)
-	valUpdates := staking.InitGenesis(ctx, am.keeper, am.accKeeper, am.bankKeeper, &genesisState)
-	if am.consumerKeeper.IsPreCCV(ctx) {
-		return valUpdates
-	}
+	_ = staking.InitGenesis(ctx, am.keeper, am.accKeeper, am.bankKeeper, &genesisState)
 
 	return []abci.ValidatorUpdate{}
 }
@@ -68,10 +66,13 @@ func (am AppModule) InitGenesis(ctx sdk.Context, cdc codec.JSONCodec, data json.
 // however, it returns no validator updates as validators are tracked via the
 // consumer chain's x/cvv/consumer module and so this module is not responsible
 // for returning the initial validator set.
+//
+// Note: This method does not require any special handling for PreCCV being true
+// (as a part of the migration from sovereign -> consumer chain).
+// The ccv consumer Endblocker is ordered to run before the staking Endblocker,
+// so if PreCCV is true during one block, the ccv consumer Enblocker will return the proper validator updates,
+// the PreCCV flag will be toggled to false, and no validator updates should be returned by this method.
 func (am AppModule) EndBlock(ctx sdk.Context, _ abci.RequestEndBlock) []abci.ValidatorUpdate {
-	valUpdates := am.keeper.BlockValidatorUpdates(ctx)
-	if am.consumerKeeper.IsPreCCV(ctx) {
-		return valUpdates
-	}
+	_ = am.keeper.BlockValidatorUpdates(ctx)
 	return []abci.ValidatorUpdate{}
 }
