7fa624bc7a8fba01b6020209a821f687ced558ed
Shawn <44221603+smarshall-spitzbart@users.noreply.github.com>
2023-03-16 22:20:32 -0700

    final nits before non-draft

--- x/ccv/democracy/staking/module.go
+++ x/ccv/democracy/staking/module.go
@@ -54,7 +54,8 @@ func NewAppModule(cdc codec.Codec, keeper keeper.Keeper, ak types.AccountKeeper,
 // consumer chain's x/cvv/consumer module and so this module is not responsible
 // for returning the initial validator set.
 //
-// Note: InitGenesis is not called during the soft upgrade of a module (as a part of a migration from sovereign -> consumer chain),
+// Note: InitGenesis is not called during the soft upgrade of a module
+// (as a part of a changeover from sovereign -> consumer chain),
 // so there is no special handling needed in this method for a consumer being in the pre-CCV state.
 func (am AppModule) InitGenesis(ctx sdk.Context, cdc codec.JSONCodec, data json.RawMessage) []abci.ValidatorUpdate {
 	var genesisState types.GenesisState
@@ -71,7 +72,7 @@ func (am AppModule) InitGenesis(ctx sdk.Context, cdc codec.JSONCodec, data json.
 // for returning the initial validator set.
 //
 // Note: This method does not require any special handling for PreCCV being true
-// (as a part of the migration from sovereign -> consumer chain).
+// (as a part of the changeover from sovereign -> consumer chain).
 // The ccv consumer Endblocker is ordered to run before the staking Endblocker,
 // so if PreCCV is true during one block, the ccv consumer Enblocker will return the proper validator updates,
 // the PreCCV flag will be toggled to false, and no validator updates should be returned by this method.
