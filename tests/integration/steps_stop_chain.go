fa4df87c23a459f88562a0b2a7244fe7594fff58
Matija Salopek <matija.salopek994@gmail.com>
2023-03-24 12:52:02 +0100

    start hermes in docker tests

--- tests/integration/steps_stop_chain.go
+++ tests/integration/steps_stop_chain.go
@@ -6,6 +6,11 @@ import "time"
 func stepsStopChain(consumerName string, propNumber uint) []Step {
 	s := []Step{
 		{
+			// start hermes so that all messages are relayed
+			action: startHermesAction{},
+			state:  State{},
+		},
+		{
 			action: submitConsumerRemovalProposalAction{
 				chain:          chainID("provi"),
 				from:           validatorID("bob"),
@@ -51,6 +56,65 @@ func stepsStopChain(consumerName string, propNumber uint) []Step {
 						validatorID("bob"): 9500000000,
 					},
 					ConsumerChains: &map[chainID]bool{}, // Consumer chain is now removed
+				},
+			},
+		},
+	}
+
+	return s
+}
+
+// submits a consumer-removal proposal and votes no on it
+// the chain should not be removed
+func stepsConsumerRemovalPropNotPassing(consumerName string, propNumber uint) []Step {
+	s := []Step{
+
+		{
+			action: submitConsumerRemovalProposalAction{
+				chain:          chainID("provi"),
+				from:           validatorID("bob"),
+				deposit:        10000001,
+				consumerChain:  chainID(consumerName),
+				stopTimeOffset: 0 * time.Millisecond,
+			},
+			state: State{
+				chainID("provi"): ChainState{
+					ValBalances: &map[validatorID]uint{
+						validatorID("bob"): 9489999999,
+					},
+					Proposals: &map[uint]Proposal{
+						propNumber: ConsumerRemovalProposal{
+							Deposit:  10000001,
+							Chain:    chainID(consumerName),
+							StopTime: 0,
+							Status:   "PROPOSAL_STATUS_VOTING_PERIOD",
+						},
+					},
+					ConsumerChains: &map[chainID]bool{"consu": true}, // consumer chain not removed
+				},
+			},
+		},
+		{
+			action: voteGovProposalAction{
+				chain:      chainID("provi"),
+				from:       []validatorID{validatorID("alice"), validatorID("bob"), validatorID("carol")},
+				vote:       []string{"no", "no", "no"},
+				propNumber: propNumber,
+			},
+			state: State{
+				chainID("provi"): ChainState{
+					Proposals: &map[uint]Proposal{
+						propNumber: ConsumerRemovalProposal{
+							Deposit:  10000001,
+							Chain:    chainID(consumerName),
+							StopTime: 0,
+							Status:   "PROPOSAL_STATUS_REJECTED",
+						},
+					},
+					ValBalances: &map[validatorID]uint{
+						validatorID("bob"): 9500000000,
+					},
+					ConsumerChains: &map[chainID]bool{"consu": true}, // consumer chain not removed
 				},
 			},
 		},
