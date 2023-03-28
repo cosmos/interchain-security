286ecf69ebbc835e60b447377e594957c96df805
Jacob Gadikian <jacobgadikian@gmail.com>
2023-03-28 20:11:58 +0700

    fix main.go 3x

--- cmd/interchain-security-cdd/main.go
+++ cmd/interchain-security-cdd/main.go
@@ -5,13 +5,14 @@ import (
 
 	"github.com/cosmos/cosmos-sdk/server"
 	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
+	app "github.com/cosmos/interchain-security/app/consumer-democracy"
 	"github.com/cosmos/interchain-security/cmd/interchain-security-cdd/cmd"
 )
 
 func main() {
 	rootCmd, _ := cmd.NewRootCmd()
 
-	if err := svrcmd.Execute(rootCmd, ""); err != nil {
+	if err := svrcmd.Execute(rootCmd, app.DefaultNodeHome); err != nil {
 		switch e := err.(type) {
 		case server.ErrorCode:
 			os.Exit(e.Code)
