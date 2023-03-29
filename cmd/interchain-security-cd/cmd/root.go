8bb98156d073d04d0be33d8dd81a36c844704bbd
Jacob Gadikian <jacobgadikian@gmail.com>
2023-03-29 09:51:04 +0700

    remove the name of the last chain I removed ICS from

--- cmd/interchain-security-cd/cmd/root.go
+++ cmd/interchain-security-cd/cmd/root.go
@@ -56,7 +56,7 @@ func NewRootCmd() (*cobra.Command, params.EncodingConfig) {
 
 	rootCmd := &cobra.Command{
 		Use:   version.AppName,
-		Short: "Kujira Daemon (server)",
+		Short: "Consumer Daemon (server)",
 		PersistentPreRunE: func(cmd *cobra.Command, _ []string) error {
 			// set the default command outputs
 			cmd.SetOut(cmd.OutOrStdout())
