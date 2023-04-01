c226968945d1752950430642c676dc4de35e0123
Matija Salopek <matija.salopek994@gmail.com>
2023-04-01 16:25:12 +0100

    update protocgen script; leave old in repo

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -4,19 +4,18 @@ set -eo pipefail
 
 echo "Generating gogo proto code"
 cd proto
-proto_dirs=$(find ./interchain_security -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
-  for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
-    if grep "option go_package" $file &> /dev/null ; then
-      buf generate --template buf.gen.gogo.yaml $file
-    fi
-  done
-done
+  buf protoc \
+    -I "proto" \
+    -I "third_party/proto" \
+    --gocosmos_out=plugins=interfacetype+grpc,\
+Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
+    --grpc-gateway_out=logtostderr=true,allow_colon_final_segments=true:. \
+  $(find "${dir}" -maxdepth 1 -name '*.proto')
 
-cd ..
+done
 
 # move proto files to the right places
 cp -r github.com/cosmos/interchain-security/* ./
 rm -rf github.com
-
-go mod tidy -compat=1.18
