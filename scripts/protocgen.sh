c0a8196962b08e751562a2d4347838a7460ccfea
Matija Salopek <matija.salopek994@gmail.com>
2023-04-01 18:10:29 +0100

    checkpoint: fix make proto-gen; make proto-gen runs without issues

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -4,18 +4,22 @@ set -eo pipefail
 
 echo "Generating gogo proto code"
 cd proto
-proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+proto_dirs=$(find ./interchain_security -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
-  buf protoc \
-    -I "proto" \
-    -I "third_party/proto" \
-    --gocosmos_out=plugins=interfacetype+grpc,\
-Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
-    --grpc-gateway_out=logtostderr=true,allow_colon_final_segments=true:. \
-  $(find "${dir}" -maxdepth 1 -name '*.proto')
-
+  for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
+    if grep "option go_package" $file &> /dev/null ; then
+      buf generate --template buf.gen.gogo.yaml $file
+    fi
+  done
 done
+
+cd ..
 
 # move proto files to the right places
 cp -r github.com/cosmos/interchain-security/* ./
+
+# copy shared types to consumer
+# previous command puts them in x/ccv/types
+cp github.com/cosmos/interchain-security/x/ccv/types/ccv.pb.go ./x/ccv/consumer/types/
+
 rm -rf github.com
