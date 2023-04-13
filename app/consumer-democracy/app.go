d686044b46fd5f91d6fb3ee4f910a6a30d0e1410
Simon Noetzlin <simon.ntz@gmail.com>
2023-04-13 15:20:06 +0200

    add comment about beginblocker order requirement for soft opt-out

--- app/consumer-democracy/app.go
+++ app/consumer-democracy/app.go
@@ -503,6 +503,7 @@ func New(
 	// CanWithdrawInvariant invariant.
 	// NOTE: staking module is required if HistoricalEntries param > 0
 	// NOTE: capability module's beginblocker must come before any modules using capabilities (e.g. IBC)
+	// NOTE: the soft opt-out requires that the consumer module's beginblocker comes after the slashing module's beginblocker
 	app.MM.SetOrderBeginBlockers(
 		// upgrades should be run first
 		upgradetypes.ModuleName,
