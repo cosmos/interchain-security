df925cd27216f59a5853b4ebc9d986799c2f28dc
sontrinh16 <trinhleson2000@gmail.com>
2023-04-17 21:45:07 +0700

    remove depricated key

--- app/consumer-democracy/proposals_whitelisting.go
+++ app/consumer-democracy/proposals_whitelisting.go
@@ -4,6 +4,7 @@ import (
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	distrtypes "github.com/cosmos/cosmos-sdk/x/distribution/types"
 	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
+	govv1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
 	govv1beta1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1beta1"
 	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
 	"github.com/cosmos/cosmos-sdk/x/params/types/proposal"
@@ -37,31 +38,29 @@ type paramChangeKey struct {
 
 var WhitelistedParams = map[paramChangeKey]struct{}{
 	// bank
-	{Subspace: banktypes.ModuleName, Key: "SendEnabled"}: {},
+	{Subspace: banktypes.ModuleName, Key: string(banktypes.KeySendEnabled)}: {},
 	// governance
-	{Subspace: govtypes.ModuleName, Key: "depositparams"}: {}, // min_deposit, max_deposit_period
-	{Subspace: govtypes.ModuleName, Key: "votingparams"}:  {}, // voting_period
-	{Subspace: govtypes.ModuleName, Key: "tallyparams"}:   {}, // quorum,threshold,veto_threshold
+	{Subspace: govtypes.ModuleName, Key: string(govv1.ParamStoreKeyDepositParams)}: {}, // min_deposit, max_deposit_period
+	{Subspace: govtypes.ModuleName, Key: string(govv1.ParamStoreKeyVotingParams)}:  {}, // voting_period
+	{Subspace: govtypes.ModuleName, Key: string(govv1.ParamStoreKeyTallyParams)}:   {}, // quorum,threshold,veto_threshold
 	// staking
-	{Subspace: stakingtypes.ModuleName, Key: "UnbondingTime"}:     {},
-	{Subspace: stakingtypes.ModuleName, Key: "MaxValidators"}:     {},
-	{Subspace: stakingtypes.ModuleName, Key: "MaxEntries"}:        {},
-	{Subspace: stakingtypes.ModuleName, Key: "HistoricalEntries"}: {},
-	{Subspace: stakingtypes.ModuleName, Key: "BondDenom"}:         {},
+	{Subspace: stakingtypes.ModuleName, Key: string(stakingtypes.KeyUnbondingTime)}:     {},
+	{Subspace: stakingtypes.ModuleName, Key: string(stakingtypes.KeyMaxValidators)}:     {},
+	{Subspace: stakingtypes.ModuleName, Key: string(stakingtypes.KeyMaxEntries)}:        {},
+	{Subspace: stakingtypes.ModuleName, Key: string(stakingtypes.KeyHistoricalEntries)}: {},
+	{Subspace: stakingtypes.ModuleName, Key: string(stakingtypes.KeyBondDenom)}:         {},
 	// distribution
-	{Subspace: distrtypes.ModuleName, Key: "communitytax"}:        {},
-	{Subspace: distrtypes.ModuleName, Key: "baseproposerreward"}:  {},
-	{Subspace: distrtypes.ModuleName, Key: "bonusproposerreward"}: {},
-	{Subspace: distrtypes.ModuleName, Key: "withdrawaddrenabled"}: {},
+	{Subspace: distrtypes.ModuleName, Key: string(distrtypes.ParamStoreKeyCommunityTax)}:        {},
+	{Subspace: distrtypes.ModuleName, Key: string(distrtypes.ParamStoreKeyWithdrawAddrEnabled)}: {},
 	// mint
-	{Subspace: minttypes.ModuleName, Key: "MintDenom"}:           {},
-	{Subspace: minttypes.ModuleName, Key: "InflationRateChange"}: {},
-	{Subspace: minttypes.ModuleName, Key: "InflationMax"}:        {},
-	{Subspace: minttypes.ModuleName, Key: "InflationMin"}:        {},
-	{Subspace: minttypes.ModuleName, Key: "GoalBonded"}:          {},
-	{Subspace: minttypes.ModuleName, Key: "BlocksPerYear"}:       {},
+	{Subspace: minttypes.ModuleName, Key: string(minttypes.KeyMintDenom)}:           {},
+	{Subspace: minttypes.ModuleName, Key: string(minttypes.KeyInflationRateChange)}: {},
+	{Subspace: minttypes.ModuleName, Key: string(minttypes.KeyInflationMax)}:        {},
+	{Subspace: minttypes.ModuleName, Key: string(minttypes.KeyInflationMin)}:        {},
+	{Subspace: minttypes.ModuleName, Key: string(minttypes.KeyGoalBonded)}:          {},
+	{Subspace: minttypes.ModuleName, Key: string(minttypes.KeyBlocksPerYear)}:       {},
 	// ibc transfer
-	{Subspace: ibctransfertypes.ModuleName, Key: "SendEnabled"}:    {},
-	{Subspace: ibctransfertypes.ModuleName, Key: "ReceiveEnabled"}: {},
+	{Subspace: ibctransfertypes.ModuleName, Key: string(ibctransfertypes.KeySendEnabled)}:    {},
+	{Subspace: ibctransfertypes.ModuleName, Key: string(ibctransfertypes.KeyReceiveEnabled)}: {},
 	// add interchain account params(HostEnabled, AllowedMessages) once the module is added to the consumer app
 }
