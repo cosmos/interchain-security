65b22ffaf39f00c6f557ad10f8f00a469283ece9
Jehan Tremback <hi@jehan.email>
2023-05-10 12:30:21 -0700

    update prefix checker

--- scripts/prefix_checker.sh
+++ scripts/prefix_checker.sh
@@ -7,23 +7,25 @@ if [[ -z $1 ]]; then
 fi
 
 # Extract keys from a given file
-extract_keys() {
+# this awk command extracts the first word from each line between a line containing
+# "const (" and a line containing ")" in the Go file, but only if that word starts
+# with a letter. This should correspond to the constant names in the const block.
+extract_prefixes() {
     awk '/const \(/,/\)/ { if ($1 ~ /^[A-Za-z]/ && $1 != "const") print $1 }' $1
 }
-
 
 # Get the previous commit hash
 prev_commit=$(git rev-parse HEAD~1)
 
 # Extract keys from the current and previous versions of the file
-current_keys=$(extract_keys $1)
-prev_keys=$(extract_keys <(git show $prev_commit:$1))
+current_prefixes=$(extract_prefixes $1)
+prev_prefixes=$(extract_prefixes <(git show $prev_commit:$1))
 
-echo $current_keys
-echo $prev_keys
+echo $current_prefixes
+echo $prev_prefixes
 
 # Compare the keys
-if ! [[ $current_keys == $prev_keys* ]]; then
-    echo "Error: keys order changed or a key was deleted"
+if ! [[ $current_prefixes == $prev_prefixes* ]]; then
+    echo "Error: prefixes order changed or a prefix was deleted"
     exit 1
 fi
