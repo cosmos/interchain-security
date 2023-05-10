7d272e877289224d73e7e46103de6bbb77ec0f74
Jehan Tremback <hi@jehan.email>
2023-05-10 11:21:33 -0700

    improve prefix checker

--- scripts/prefix_checker.sh
+++ scripts/prefix_checker.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Check if file name is provided
+if [[ -z $1 ]]; then
+    echo "Error: No file name provided"
+    exit 1
+fi
+
 # Extract keys from a given file
 extract_keys() {
     grep -oP '\b[A-Za-z_][A-Za-z0-9_]*\b' $1
@@ -9,8 +15,8 @@ extract_keys() {
 prev_commit=$(git rev-parse HEAD~1)
 
 # Extract keys from the current and previous versions of the file
-current_keys=$(extract_keys prefixes.go)
-prev_keys=$(extract_keys <(git show $prev_commit:prefixes.go))
+current_keys=$(extract_keys $1)
+prev_keys=$(extract_keys <(git show $prev_commit:$1))
 
 # Compare the keys
 if ! [[ $current_keys == $prev_keys* ]]; then
