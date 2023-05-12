930e7b1bb701510cea4b894048b943e55aa32fdc
Philip Offtermatt <57488781+p-offtermatt@users.noreply.github.com>
2023-05-12 15:50:06 +0200

    test: add rapid pbt for trace parser (#913)
    * Add other steps to read/write test
    
    * Improve error logging in trace handler test
    
    * Push ActionType handling into Unmarshal/Marshal and out of own class
    
    * Rename generic trace handler files to be clear they are for json
    
    * Rename to marshalling to be more generic
    
    * Add marshal/unmarshal for proposals
    
    * Export unexported field
    
    * Add test for marshal/unmarshalling chain state
    
    * Fix pointer issues
    
    * Fix typo: action -> proposal
    
    * Log proposal string in test
    
    * Use json.RawMessage instead of map[string]any
    
    * For uniformity, also use RawMessage for step unmarshalling
    
    * Add tests for extra proposal types
    
    * Add more proposal types to test and unify names
    
    * Add handling for ParamsProposal
    
    * Regenerate traces
    
    * Chore: Export forgotten field
    
    * Use string, not int, to help marshal/unmarshal
    
    * Add rapid to go.mod and .sum
    
    * Add rapidpbt for chainState marshalling
    
    * Rename file to make clear it only relates to chainState
    
    * Add error return to TraceWriter and TraceParser
    
    * gst
    
    * Add generators  for actions and steps, utilize in test driver
    
    * Restrict range for time
    
    * Add test for time marshal/unmarshal
    
    * Make time have a lower bound, since negative numbers are not supported by RFC3339
    
    * Improve label string for argument to time.Unix
    
    * Correct lower bound for time: 1900 years negative instead of 2000
    
    * Convert timestamp to utc
    
    * Format file
    
    * Ignore testdata folder
    
    * Add go comment
    
    * Add docstring to GetTraceGen

--- tests/e2e/json_parser.go
+++ tests/e2e/json_parser.go
@@ -7,7 +7,7 @@ import (
 
 // TraceParser provides an interface for parsers that read sequences of Steps from files.
 type TraceParser interface {
-	ReadTraceFromFile(filepath string) []Step
+	ReadTraceFromFile(filepath string) ([]Step, error)
 }
 
 // JSONParser is a simple parser that reads steps by unmarshalling from a file.
