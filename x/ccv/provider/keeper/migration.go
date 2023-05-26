e9b71fdf6a67ac1af874f7c169924729cafc2365
Shawn <44221603+smarshall-spitzbart@users.noreply.github.com>
2023-05-26 14:39:15 -0700

    test and cleans

--- x/ccv/provider/keeper/migration.go
+++ x/ccv/provider/keeper/migration.go
@@ -24,18 +24,23 @@ func NewMigrator(ccvProviderKeeper Keeper, ccvProviderParamSpace paramtypes.Subs
 
 func (m Migrator) Migratev1Tov2(ctx sdk.Context) error {
 	// Migrate params
-	m.MigrateParamsv1Tov2(ctx)
+	MigrateParamsv1Tov2(ctx,
+		m.ccvProviderParamSpace,
+		// See https://github.com/cosmos/interchain-security/blob/7861804cb311507ec6aebebbfad60ea42eb8ed4b/x/ccv/provider/keeper/params.go#L84
+		// The v1.1.0-multiden version of ICS hardcodes this param as 10 of bond type: k.stakingKeeper.BondDenom(ctx).
+		// Here we use the same starting value, but the param can now be changed through governance.
+		sdk.NewCoin(m.ccvProviderKeeper.BondDenom(ctx), sdk.NewInt(10000000)),
+	)
 
 	// Delete select consumer genesis states for consumers that're launched
-	m.migrateConsumerGenesisStates(ctx)
+	MigrateConsumerGenesisStates(ctx, m.ccvProviderKeeper)
 
 	return nil
 }
 
 // MigrateParamsv1Tov2 migrates the provider CCV module params from v1.0.0 to v2.0.0,
 // setting default values for new params.
-func (m Migrator) MigrateParamsv1Tov2(ctx sdk.Context) {
-	paramsSubspace := m.ccvProviderParamSpace
+func MigrateParamsv1Tov2(ctx sdk.Context, paramsSubspace paramtypes.Subspace, consumerRewardDenomRegistrationFee sdk.Coin) {
 
 	// Get old params
 	var templateClient ibctmtypes.ClientState
@@ -55,11 +60,6 @@ func (m Migrator) MigrateParamsv1Tov2(ctx sdk.Context) {
 	var maxThrottledPackets int64
 	paramsSubspace.Get(ctx, providertypes.KeyMaxThrottledPackets, &maxThrottledPackets)
 
-	// See https://github.com/cosmos/interchain-security/blob/7861804cb311507ec6aebebbfad60ea42eb8ed4b/x/ccv/provider/keeper/params.go#L84
-	// The v1.1.0-multiden version of ICS hardcodes this param as 10 of bond type: k.stakingKeeper.BondDenom(ctx).
-	// Here we use the same starting value, but the param can now be changed through governance.
-	consumerRewardDenomRegistrationFee := sdk.NewCoin(m.ccvProviderKeeper.BondDenom(ctx), sdk.NewInt(10000000))
-
 	// Recycle old params, set new param to input value
 	newParams := providertypes.NewParams(
 		&templateClient,
@@ -77,10 +77,10 @@ func (m Migrator) MigrateParamsv1Tov2(ctx sdk.Context) {
 	paramsSubspace.SetParamSet(ctx, &newParams)
 }
 
-func (m Migrator) migrateConsumerGenesisStates(ctx sdk.Context) {
+func MigrateConsumerGenesisStates(ctx sdk.Context, providerKeeper Keeper) {
 	// We could try to migrate existing ConsumerGenesisStates, but they're not needed after consumer launch.
 	// Hence we delete them strategically.
-	m.ccvProviderKeeper.DeleteConsumerGenesis(ctx, "neutron-1") // See https://github.com/neutron-org/mainnet-assets#parameters
+	providerKeeper.DeleteConsumerGenesis(ctx, "neutron-1") // See https://github.com/neutron-org/mainnet-assets#parameters
 
 	// TODO: determine if any other ConsumerGenesisStates need to be deleted, or actually migrated!
 }
