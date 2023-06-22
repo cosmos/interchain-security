0974bdc162c3e4302a5b372333465f1473808935
Shawn <44221603+smarshall-spitzbart@users.noreply.github.com>
2023-06-22 13:23:55 -0700

    no circ dep

--- x/ccv/consumer/types/consumer.pb.go
+++ x/ccv/consumer/types/consumer.pb.go
@@ -9,75 +9,23 @@ import (
 	types "github.com/cosmos/cosmos-sdk/codec/types"
 	_ "github.com/cosmos/gogoproto/gogoproto"
 	proto "github.com/cosmos/gogoproto/proto"
-	github_com_cosmos_gogoproto_types "github.com/cosmos/gogoproto/types"
 	_ "github.com/cosmos/interchain-security/v3/x/ccv/types"
 	_ "google.golang.org/protobuf/types/known/durationpb"
-	_ "google.golang.org/protobuf/types/known/timestamppb"
 	io "io"
 	math "math"
 	math_bits "math/bits"
-	time "time"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ = proto.Marshal
 var _ = fmt.Errorf
 var _ = math.Inf
-var _ = time.Kitchen
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the proto package it is being compiled against.
 // A compilation error at this line likely means your copy of the
 // proto package needs to be updated.
 const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
-
-// LastTransmissionBlockHeight is the last time validator holding
-// pools were transmitted to the provider chain.
-//
-// This type is only used internally to the consumer CCV module.
-type LastTransmissionBlockHeight struct {
-	Height int64 `protobuf:"varint,1,opt,name=height,proto3" json:"height,omitempty"`
-}
-
-func (m *LastTransmissionBlockHeight) Reset()         { *m = LastTransmissionBlockHeight{} }
-func (m *LastTransmissionBlockHeight) String() string { return proto.CompactTextString(m) }
-func (*LastTransmissionBlockHeight) ProtoMessage()    {}
-func (*LastTransmissionBlockHeight) Descriptor() ([]byte, []int) {
-	return fileDescriptor_5b27a82b276e7f93, []int{0}
-}
-func (m *LastTransmissionBlockHeight) XXX_Unmarshal(b []byte) error {
-	return m.Unmarshal(b)
-}
-func (m *LastTransmissionBlockHeight) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	if deterministic {
-		return xxx_messageInfo_LastTransmissionBlockHeight.Marshal(b, m, deterministic)
-	} else {
-		b = b[:cap(b)]
-		n, err := m.MarshalToSizedBuffer(b)
-		if err != nil {
-			return nil, err
-		}
-		return b[:n], nil
-	}
-}
-func (m *LastTransmissionBlockHeight) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_LastTransmissionBlockHeight.Merge(m, src)
-}
-func (m *LastTransmissionBlockHeight) XXX_Size() int {
-	return m.Size()
-}
-func (m *LastTransmissionBlockHeight) XXX_DiscardUnknown() {
-	xxx_messageInfo_LastTransmissionBlockHeight.DiscardUnknown(m)
-}
-
-var xxx_messageInfo_LastTransmissionBlockHeight proto.InternalMessageInfo
-
-func (m *LastTransmissionBlockHeight) GetHeight() int64 {
-	if m != nil {
-		return m.Height
-	}
-	return 0
-}
 
 // CrossChainValidator defines the type used to store validator information internal
 // to the consumer CCV module.  Note one cross chain validator entry is persisted for
@@ -94,7 +42,7 @@ func (m *CrossChainValidator) Reset()         { *m = CrossChainValidator{} }
 func (m *CrossChainValidator) String() string { return proto.CompactTextString(m) }
 func (*CrossChainValidator) ProtoMessage()    {}
 func (*CrossChainValidator) Descriptor() ([]byte, []int) {
-	return fileDescriptor_5b27a82b276e7f93, []int{1}
+	return fileDescriptor_5b27a82b276e7f93, []int{0}
 }
 func (m *CrossChainValidator) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -144,65 +92,8 @@ func (m *CrossChainValidator) GetPubkey() *types.Any {
 	return nil
 }
 
-// MaturingVSCPacket represents a vsc packet that is maturing internal to the
-// consumer CCV module, where the consumer has not yet relayed a VSCMatured packet
-// back to the provider.
-type MaturingVSCPacket struct {
-	VscId        uint64    `protobuf:"varint,1,opt,name=vscId,proto3" json:"vscId,omitempty"`
-	MaturityTime time.Time `protobuf:"bytes,2,opt,name=maturity_time,json=maturityTime,proto3,stdtime" json:"maturity_time"`
-}
-
-func (m *MaturingVSCPacket) Reset()         { *m = MaturingVSCPacket{} }
-func (m *MaturingVSCPacket) String() string { return proto.CompactTextString(m) }
-func (*MaturingVSCPacket) ProtoMessage()    {}
-func (*MaturingVSCPacket) Descriptor() ([]byte, []int) {
-	return fileDescriptor_5b27a82b276e7f93, []int{2}
-}
-func (m *MaturingVSCPacket) XXX_Unmarshal(b []byte) error {
-	return m.Unmarshal(b)
-}
-func (m *MaturingVSCPacket) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	if deterministic {
-		return xxx_messageInfo_MaturingVSCPacket.Marshal(b, m, deterministic)
-	} else {
-		b = b[:cap(b)]
-		n, err := m.MarshalToSizedBuffer(b)
-		if err != nil {
-			return nil, err
-		}
-		return b[:n], nil
-	}
-}
-func (m *MaturingVSCPacket) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_MaturingVSCPacket.Merge(m, src)
-}
-func (m *MaturingVSCPacket) XXX_Size() int {
-	return m.Size()
-}
-func (m *MaturingVSCPacket) XXX_DiscardUnknown() {
-	xxx_messageInfo_MaturingVSCPacket.DiscardUnknown(m)
-}
-
-var xxx_messageInfo_MaturingVSCPacket proto.InternalMessageInfo
-
-func (m *MaturingVSCPacket) GetVscId() uint64 {
-	if m != nil {
-		return m.VscId
-	}
-	return 0
-}
-
-func (m *MaturingVSCPacket) GetMaturityTime() time.Time {
-	if m != nil {
-		return m.MaturityTime
-	}
-	return time.Time{}
-}
-
 func init() {
-	proto.RegisterType((*LastTransmissionBlockHeight)(nil), "interchain_security.ccv.consumer.v1.LastTransmissionBlockHeight")
 	proto.RegisterType((*CrossChainValidator)(nil), "interchain_security.ccv.consumer.v1.CrossChainValidator")
-	proto.RegisterType((*MaturingVSCPacket)(nil), "interchain_security.ccv.consumer.v1.MaturingVSCPacket")
 }
 
 func init() {
@@ -210,64 +101,29 @@ func init() {
 }
 
 var fileDescriptor_5b27a82b276e7f93 = []byte{
-	// 458 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x74, 0x52, 0x41, 0x6e, 0xd3, 0x40,
-	0x14, 0x8d, 0x29, 0x04, 0x34, 0x2d, 0x0b, 0x4c, 0x04, 0x21, 0x48, 0x4e, 0x65, 0x84, 0xd4, 0x4d,
-	0x3d, 0x4a, 0x22, 0x36, 0x48, 0x2c, 0x48, 0x36, 0x54, 0x80, 0x54, 0x99, 0xaa, 0x48, 0x6c, 0xa2,
-	0xf1, 0x78, 0x70, 0x46, 0x89, 0x67, 0xac, 0xf9, 0x33, 0x2e, 0x73, 0x8b, 0x1e, 0x83, 0x03, 0x70,
-	0x88, 0x8a, 0x55, 0x97, 0xac, 0x0a, 0x4a, 0x6e, 0xc0, 0x09, 0x90, 0xc7, 0x76, 0x10, 0x05, 0x76,
-	0xff, 0xe9, 0xfd, 0xf7, 0xf4, 0xe6, 0xcd, 0x47, 0x63, 0x2e, 0x34, 0x53, 0x74, 0x41, 0xb8, 0x98,
-	0x03, 0xa3, 0x46, 0x71, 0x6d, 0x31, 0xa5, 0x25, 0xa6, 0x52, 0x80, 0xc9, 0x99, 0xc2, 0xe5, 0x68,
-	0x3b, 0x47, 0x85, 0x92, 0x5a, 0xfa, 0x4f, 0xfe, 0xa1, 0x89, 0x28, 0x2d, 0xa3, 0xed, 0x5e, 0x39,
-	0x1a, 0x3c, 0xfd, 0x9f, 0x71, 0x39, 0xc2, 0x67, 0x5c, 0xb1, 0xda, 0x6b, 0xf0, 0x28, 0x93, 0x32,
-	0x5b, 0x31, 0xec, 0x50, 0x62, 0x3e, 0x62, 0x22, 0x6c, 0x43, 0xf5, 0x32, 0x99, 0x49, 0x37, 0xe2,
-	0x6a, 0x6a, 0x05, 0x54, 0x42, 0x2e, 0x61, 0x5e, 0x13, 0x35, 0x68, 0xa8, 0xe0, 0xba, 0x57, 0x6a,
-	0x14, 0xd1, 0x5c, 0x8a, 0x86, 0x1f, 0x5e, 0xe7, 0x35, 0xcf, 0x19, 0x68, 0x92, 0x17, 0xf5, 0x42,
-	0xf8, 0x0c, 0x3d, 0x7e, 0x43, 0x40, 0x9f, 0x28, 0x22, 0x20, 0xe7, 0x00, 0x5c, 0x8a, 0xe9, 0x4a,
-	0xd2, 0xe5, 0x2b, 0xc6, 0xb3, 0x85, 0xf6, 0x1f, 0xa0, 0xee, 0xc2, 0x4d, 0x7d, 0x6f, 0xdf, 0x3b,
-	0xd8, 0x89, 0x1b, 0x14, 0x7e, 0xf6, 0xd0, 0xfd, 0x99, 0x92, 0x00, 0xb3, 0xea, 0xb5, 0xa7, 0x64,
-	0xc5, 0x53, 0xa2, 0xa5, 0xf2, 0xfb, 0xe8, 0x36, 0x49, 0x53, 0xc5, 0x00, 0x9c, 0x60, 0x2f, 0x6e,
-	0xa1, 0xdf, 0x43, 0xb7, 0x0a, 0x79, 0xc6, 0x54, 0xff, 0x86, 0x33, 0xaa, 0x81, 0x4f, 0x50, 0xb7,
-	0x30, 0xc9, 0x92, 0xd9, 0xfe, 0xce, 0xbe, 0x77, 0xb0, 0x3b, 0xee, 0x45, 0x75, 0xe0, 0xa8, 0x0d,
-	0x1c, 0xbd, 0x14, 0x76, 0x3a, 0xf9, 0x79, 0x35, 0x7c, 0x68, 0x49, 0xbe, 0x7a, 0x1e, 0x56, 0x7d,
-	0x33, 0x01, 0x06, 0xe6, 0xb5, 0x2e, 0xfc, 0xfa, 0xe5, 0xb0, 0xd7, 0x54, 0x42, 0x95, 0x2d, 0xb4,
-	0x8c, 0x8e, 0x4d, 0xf2, 0x9a, 0xd9, 0xb8, 0x31, 0x0e, 0x35, 0xba, 0xf7, 0x96, 0x68, 0xa3, 0xb8,
-	0xc8, 0x4e, 0xdf, 0xcd, 0x8e, 0x09, 0x5d, 0x32, 0x5d, 0xa5, 0x29, 0x81, 0x1e, 0xa5, 0x2e, 0xe5,
-	0xcd, 0xb8, 0x06, 0xfe, 0x11, 0xba, 0x9b, 0xbb, 0x55, 0x6d, 0xe7, 0x55, 0x51, 0x2e, 0xeb, 0xee,
-	0x78, 0xf0, 0x57, 0xa8, 0x93, 0xb6, 0xc5, 0xe9, 0x9d, 0x8b, 0xab, 0x61, 0xe7, 0xfc, 0xfb, 0xd0,
-	0x8b, 0xf7, 0x5a, 0x69, 0x45, 0x4e, 0xdf, 0x5f, 0xac, 0x03, 0xef, 0x72, 0x1d, 0x78, 0x3f, 0xd6,
-	0x81, 0x77, 0xbe, 0x09, 0x3a, 0x97, 0x9b, 0xa0, 0xf3, 0x6d, 0x13, 0x74, 0x3e, 0xbc, 0xc8, 0xb8,
-	0x5e, 0x98, 0x24, 0xa2, 0x32, 0x6f, 0xfe, 0x12, 0xff, 0xbe, 0x9b, 0xc3, 0xed, 0xdd, 0x94, 0x13,
-	0xfc, 0xe9, 0xcf, 0xab, 0xd4, 0xb6, 0x60, 0x90, 0x74, 0x5d, 0x88, 0xc9, 0xaf, 0x00, 0x00, 0x00,
-	0xff, 0xff, 0xff, 0xc7, 0xac, 0x19, 0xc6, 0x02, 0x00, 0x00,
-}
-
-func (m *LastTransmissionBlockHeight) Marshal() (dAtA []byte, err error) {
-	size := m.Size()
-	dAtA = make([]byte, size)
-	n, err := m.MarshalToSizedBuffer(dAtA[:size])
-	if err != nil {
-		return nil, err
-	}
-	return dAtA[:n], nil
-}
-
-func (m *LastTransmissionBlockHeight) MarshalTo(dAtA []byte) (int, error) {
-	size := m.Size()
-	return m.MarshalToSizedBuffer(dAtA[:size])
-}
-
-func (m *LastTransmissionBlockHeight) MarshalToSizedBuffer(dAtA []byte) (int, error) {
-	i := len(dAtA)
-	_ = i
-	var l int
-	_ = l
-	if m.Height != 0 {
-		i = encodeVarintConsumer(dAtA, i, uint64(m.Height))
-		i--
-		dAtA[i] = 0x8
-	}
-	return len(dAtA) - i, nil
+	// 346 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x74, 0x51, 0x3d, 0x4e, 0xeb, 0x40,
+	0x18, 0xcc, 0xbe, 0xe8, 0xe5, 0x49, 0x7e, 0xaf, 0xf2, 0xb3, 0x84, 0x49, 0x61, 0xa2, 0x20, 0xa4,
+	0x34, 0xd9, 0x55, 0x92, 0x0e, 0x89, 0x82, 0xa4, 0xa4, 0x41, 0x29, 0x40, 0xa2, 0x89, 0xd6, 0xeb,
+	0xc5, 0xb1, 0x88, 0xf7, 0xb3, 0xf6, 0xc7, 0x61, 0x6f, 0xc1, 0x31, 0x38, 0x00, 0x87, 0x40, 0x54,
+	0x29, 0xa9, 0x10, 0x4a, 0x6e, 0xc0, 0x09, 0x50, 0xb2, 0x4e, 0x10, 0x08, 0xba, 0x19, 0x8d, 0x67,
+	0xbe, 0xf1, 0xac, 0xd7, 0xcf, 0x84, 0xe6, 0x92, 0x4d, 0x69, 0x26, 0x26, 0x8a, 0x33, 0x23, 0x33,
+	0x6d, 0x09, 0x63, 0x25, 0x61, 0x20, 0x94, 0xc9, 0xb9, 0x24, 0x65, 0x6f, 0x87, 0x71, 0x21, 0x41,
+	0x83, 0x7f, 0xf8, 0x8d, 0x07, 0x33, 0x56, 0xe2, 0xdd, 0x77, 0x65, 0xaf, 0x79, 0xf4, 0x53, 0x70,
+	0xd9, 0x23, 0xf3, 0x4c, 0x72, 0x97, 0xd5, 0xdc, 0x4f, 0x01, 0xd2, 0x19, 0x27, 0x1b, 0x16, 0x9b,
+	0x6b, 0x42, 0x85, 0xad, 0xa4, 0x20, 0x85, 0x14, 0x36, 0x90, 0xac, 0xd1, 0xd6, 0xc0, 0x40, 0xe5,
+	0xa0, 0x26, 0x4e, 0x70, 0xa4, 0x92, 0xa2, 0xaf, 0x59, 0x89, 0x91, 0x54, 0x67, 0x20, 0x9c, 0xde,
+	0xbe, 0x47, 0xde, 0xff, 0x91, 0x04, 0xa5, 0x46, 0xeb, 0x56, 0x17, 0x74, 0x96, 0x25, 0x54, 0x83,
+	0xf4, 0x43, 0xef, 0x0f, 0x4d, 0x12, 0xc9, 0x95, 0x0a, 0x51, 0x0b, 0x75, 0xfe, 0x8d, 0xb7, 0xd4,
+	0x0f, 0xbc, 0xdf, 0x05, 0xcc, 0xb9, 0x0c, 0x7f, 0xb5, 0x50, 0xa7, 0x3e, 0x76, 0xc4, 0xa7, 0x5e,
+	0xa3, 0x30, 0xf1, 0x0d, 0xb7, 0x61, 0xbd, 0x85, 0x3a, 0x7f, 0xfb, 0x01, 0x76, 0x87, 0xf1, 0xf6,
+	0x30, 0x3e, 0x15, 0x76, 0x38, 0x78, 0x7b, 0x39, 0xd8, 0xb3, 0x34, 0x9f, 0x1d, 0xb7, 0xd7, 0xbb,
+	0x70, 0xa1, 0x8c, 0x9a, 0x38, 0x5f, 0xfb, 0xe9, 0xa1, 0x1b, 0x54, 0xd5, 0x99, 0xb4, 0x85, 0x06,
+	0x7c, 0x6e, 0xe2, 0x33, 0x6e, 0xc7, 0x55, 0xf0, 0xf0, 0xf2, 0x71, 0x19, 0xa1, 0xc5, 0x32, 0x42,
+	0xaf, 0xcb, 0x08, 0xdd, 0xad, 0xa2, 0xda, 0x62, 0x15, 0xd5, 0x9e, 0x57, 0x51, 0xed, 0xea, 0x24,
+	0xcd, 0xf4, 0xd4, 0xc4, 0x98, 0x41, 0x5e, 0xfd, 0x3d, 0xf9, 0x58, 0xba, 0xbb, 0x5b, 0xba, 0x1c,
+	0x90, 0xdb, 0xcf, 0xef, 0xa8, 0x6d, 0xc1, 0x55, 0xdc, 0xd8, 0x74, 0x1c, 0xbc, 0x07, 0x00, 0x00,
+	0xff, 0xff, 0x0f, 0xc7, 0xd1, 0x61, 0xf8, 0x01, 0x00, 0x00,
 }
 
 func (m *CrossChainValidator) Marshal() (dAtA []byte, err error) {
@@ -317,42 +173,6 @@ func (m *CrossChainValidator) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	return len(dAtA) - i, nil
 }
 
-func (m *MaturingVSCPacket) Marshal() (dAtA []byte, err error) {
-	size := m.Size()
-	dAtA = make([]byte, size)
-	n, err := m.MarshalToSizedBuffer(dAtA[:size])
-	if err != nil {
-		return nil, err
-	}
-	return dAtA[:n], nil
-}
-
-func (m *MaturingVSCPacket) MarshalTo(dAtA []byte) (int, error) {
-	size := m.Size()
-	return m.MarshalToSizedBuffer(dAtA[:size])
-}
-
-func (m *MaturingVSCPacket) MarshalToSizedBuffer(dAtA []byte) (int, error) {
-	i := len(dAtA)
-	_ = i
-	var l int
-	_ = l
-	n2, err2 := github_com_cosmos_gogoproto_types.StdTimeMarshalTo(m.MaturityTime, dAtA[i-github_com_cosmos_gogoproto_types.SizeOfStdTime(m.MaturityTime):])
-	if err2 != nil {
-		return 0, err2
-	}
-	i -= n2
-	i = encodeVarintConsumer(dAtA, i, uint64(n2))
-	i--
-	dAtA[i] = 0x12
-	if m.VscId != 0 {
-		i = encodeVarintConsumer(dAtA, i, uint64(m.VscId))
-		i--
-		dAtA[i] = 0x8
-	}
-	return len(dAtA) - i, nil
-}
-
 func encodeVarintConsumer(dAtA []byte, offset int, v uint64) int {
 	offset -= sovConsumer(v)
 	base := offset
@@ -364,18 +184,6 @@ func encodeVarintConsumer(dAtA []byte, offset int, v uint64) int {
 	dAtA[offset] = uint8(v)
 	return base
 }
-func (m *LastTransmissionBlockHeight) Size() (n int) {
-	if m == nil {
-		return 0
-	}
-	var l int
-	_ = l
-	if m.Height != 0 {
-		n += 1 + sovConsumer(uint64(m.Height))
-	}
-	return n
-}
-
 func (m *CrossChainValidator) Size() (n int) {
 	if m == nil {
 		return 0
@@ -396,94 +204,11 @@ func (m *CrossChainValidator) Size() (n int) {
 	return n
 }
 
-func (m *MaturingVSCPacket) Size() (n int) {
-	if m == nil {
-		return 0
-	}
-	var l int
-	_ = l
-	if m.VscId != 0 {
-		n += 1 + sovConsumer(uint64(m.VscId))
-	}
-	l = github_com_cosmos_gogoproto_types.SizeOfStdTime(m.MaturityTime)
-	n += 1 + l + sovConsumer(uint64(l))
-	return n
-}
-
 func sovConsumer(x uint64) (n int) {
 	return (math_bits.Len64(x|1) + 6) / 7
 }
 func sozConsumer(x uint64) (n int) {
 	return sovConsumer(uint64((x << 1) ^ uint64((int64(x) >> 63))))
-}
-func (m *LastTransmissionBlockHeight) Unmarshal(dAtA []byte) error {
-	l := len(dAtA)
-	iNdEx := 0
-	for iNdEx < l {
-		preIndex := iNdEx
-		var wire uint64
-		for shift := uint(0); ; shift += 7 {
-			if shift >= 64 {
-				return ErrIntOverflowConsumer
-			}
-			if iNdEx >= l {
-				return io.ErrUnexpectedEOF
-			}
-			b := dAtA[iNdEx]
-			iNdEx++
-			wire |= uint64(b&0x7F) << shift
-			if b < 0x80 {
-				break
-			}
-		}
-		fieldNum := int32(wire >> 3)
-		wireType := int(wire & 0x7)
-		if wireType == 4 {
-			return fmt.Errorf("proto: LastTransmissionBlockHeight: wiretype end group for non-group")
-		}
-		if fieldNum <= 0 {
-			return fmt.Errorf("proto: LastTransmissionBlockHeight: illegal tag %d (wire type %d)", fieldNum, wire)
-		}
-		switch fieldNum {
-		case 1:
-			if wireType != 0 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Height", wireType)
-			}
-			m.Height = 0
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowConsumer
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				m.Height |= int64(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-		default:
-			iNdEx = preIndex
-			skippy, err := skipConsumer(dAtA[iNdEx:])
-			if err != nil {
-				return err
-			}
-			if (skippy < 0) || (iNdEx+skippy) < 0 {
-				return ErrInvalidLengthConsumer
-			}
-			if (iNdEx + skippy) > l {
-				return io.ErrUnexpectedEOF
-			}
-			iNdEx += skippy
-		}
-	}
-
-	if iNdEx > l {
-		return io.ErrUnexpectedEOF
-	}
-	return nil
 }
 func (m *CrossChainValidator) Unmarshal(dAtA []byte) error {
 	l := len(dAtA)
@@ -600,108 +325,6 @@ func (m *CrossChainValidator) Unmarshal(dAtA []byte) error {
 				m.Pubkey = &types.Any{}
 			}
 			if err := m.Pubkey.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
-				return err
-			}
-			iNdEx = postIndex
-		default:
-			iNdEx = preIndex
-			skippy, err := skipConsumer(dAtA[iNdEx:])
-			if err != nil {
-				return err
-			}
-			if (skippy < 0) || (iNdEx+skippy) < 0 {
-				return ErrInvalidLengthConsumer
-			}
-			if (iNdEx + skippy) > l {
-				return io.ErrUnexpectedEOF
-			}
-			iNdEx += skippy
-		}
-	}
-
-	if iNdEx > l {
-		return io.ErrUnexpectedEOF
-	}
-	return nil
-}
-func (m *MaturingVSCPacket) Unmarshal(dAtA []byte) error {
-	l := len(dAtA)
-	iNdEx := 0
-	for iNdEx < l {
-		preIndex := iNdEx
-		var wire uint64
-		for shift := uint(0); ; shift += 7 {
-			if shift >= 64 {
-				return ErrIntOverflowConsumer
-			}
-			if iNdEx >= l {
-				return io.ErrUnexpectedEOF
-			}
-			b := dAtA[iNdEx]
-			iNdEx++
-			wire |= uint64(b&0x7F) << shift
-			if b < 0x80 {
-				break
-			}
-		}
-		fieldNum := int32(wire >> 3)
-		wireType := int(wire & 0x7)
-		if wireType == 4 {
-			return fmt.Errorf("proto: MaturingVSCPacket: wiretype end group for non-group")
-		}
-		if fieldNum <= 0 {
-			return fmt.Errorf("proto: MaturingVSCPacket: illegal tag %d (wire type %d)", fieldNum, wire)
-		}
-		switch fieldNum {
-		case 1:
-			if wireType != 0 {
-				return fmt.Errorf("proto: wrong wireType = %d for field VscId", wireType)
-			}
-			m.VscId = 0
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowConsumer
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				m.VscId |= uint64(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-		case 2:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field MaturityTime", wireType)
-			}
-			var msglen int
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowConsumer
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				msglen |= int(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			if msglen < 0 {
-				return ErrInvalidLengthConsumer
-			}
-			postIndex := iNdEx + msglen
-			if postIndex < 0 {
-				return ErrInvalidLengthConsumer
-			}
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			if err := github_com_cosmos_gogoproto_types.StdTimeUnmarshal(&m.MaturityTime, dAtA[iNdEx:postIndex]); err != nil {
 				return err
 			}
 			iNdEx = postIndex
