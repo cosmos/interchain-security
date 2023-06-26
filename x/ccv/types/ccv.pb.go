a42229a465fd6f8f3f4fc14dcb4a5285ba1c4b84
Shawn <44221603+smarshall-spitzbart@users.noreply.github.com>
2023-06-26 11:01:06 -0700

    comment

--- x/ccv/types/ccv.pb.go
+++ x/ccv/types/ccv.pb.go
@@ -429,10 +429,8 @@ func (*ConsumerPacketData) XXX_OneofWrappers() []interface{} {
 	}
 }
 
-// [Depreciated] favor using []ConsumerPacketData directly, which can be stored more efficiently.
-//
 // ConsumerPacketDataList is a list of consumer packet data packets.
-// It is only used for genesis to ensure backwards compatibility with older versions of ICS.
+// NOTE: It is only used for exporting / importing state in InitGenesis and ExportGenesis.
 type ConsumerPacketDataList struct {
 	List []ConsumerPacketData `protobuf:"bytes,1,rep,name=list,proto3" json:"list"`
 }
