ee5c6f61507cd66854e497aec9c7eaa1b3718310
Yaru Wang <yaru@informal.systems>
2023-07-21 13:28:09 +0200

    feat: add provider info query

--- x/ccv/consumer/keeper/grpc_query.go
+++ x/ccv/consumer/keeper/grpc_query.go
@@ -7,7 +7,7 @@ import (
 	"google.golang.org/grpc/status"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-
+	ibctm "github.com/cosmos/ibc-go/v7/modules/light-clients/07-tendermint"
 	"github.com/cosmos/interchain-security/v3/x/ccv/consumer/types"
 )
 
@@ -39,4 +39,28 @@ func (k Keeper) QueryParams(c context.Context,
 	p := k.GetConsumerParams(ctx)
 
 	return &types.QueryParamsResponse{Params: p}, nil
+}
+
+func (k Keeper) QueryProviderChainInfo(c context.Context,
+	req *types.QueryProviderInfoRequest,
+) (*types.QueryProviderInfoResponse, error) {
+	ctx := sdk.UnwrapSDKContext(c)
+	if req == nil {
+		return nil, status.Errorf(codes.InvalidArgument, "empty request")
+	}
+
+	providerClientID, found := k.GetProviderClientID(ctx)
+	if !found {
+		return nil, nil
+	}
+	clientState, found := k.clientKeeper.GetClientState(ctx, providerClientID)
+	if !found {
+		return nil, nil
+	}
+	providerChainID := clientState.(*ibctm.ClientState).ChainId
+	resp := types.QueryProviderInfoResponse{
+		ChainID: providerChainID,
+	}
+
+	return &resp, nil
 }
