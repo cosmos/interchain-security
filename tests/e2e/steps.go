5457e718fbf292e0fa6d9947e2530e7d3c4f492f
MSalopek <matija.salopek994@gmail.com>
2023-08-02 12:53:58 +0200

    test: update steps and fix lint

--- tests/e2e/steps.go
+++ tests/e2e/steps.go
@@ -22,9 +22,10 @@ var happyPathSteps = concatSteps(
 	stepsRedelegateForOptOut("consu"),
 	stepsDowntimeWithOptOut("consu"),
 	stepsRedelegate("consu"),
-	stepsDoubleSignOnProviderAndConsumerAfterOptOut("consu"), // carol double signs on provider, bob double signs on consumer
-	stepsSubmitEquivocationProposal("consu", 2),              // now prop to tombstone bob is submitted and accepted
-	stepsStopChain("consu", 3),                               // stop chain
+	stepsRejectEquivocationProposal("consu", 2),   // prop to tombstone bob is rejected
+	stepsDoubleSignOnProviderAndConsumer("consu"), // carol double signs on provider, bob double signs on consumer
+	stepsSubmitEquivocationProposal("consu", 2),   // now prop to tombstone bob is submitted and accepted
+	stepsStopChain("consu", 3),                    // stop chain
 )
 
 var shortHappyPathSteps = concatSteps(
