4dbbf389ef240a9bb84bdf053e4935a44c683901
Shawn <44221603+smarshall-spitzbart@users.noreply.github.com>
2023-08-17 16:09:34 -0700

    it works

---
+++ x/ccv/cool/cool.go
@@ -0,0 +1,13 @@
+package cool
+
+import (
+	"fmt"
+
+	types "github.com/cosmos/interchain-security/x/types"
+)
+
+func Yo() {
+	fmt.Println("cool")
+	test := types.V1Result
+	fmt.Println(test)
+}
---
+++ x/ccv/cool/cool_test.go
@@ -0,0 +1,11 @@
+package cool_test
+
+import (
+	"testing"
+
+	cool "github.com/cosmos/interchain-security/x/ccv/cool"
+)
+
+func TestYo(t *testing.T) {
+	cool.Yo()
+}
