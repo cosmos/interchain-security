b68493c8dd7615c0b486621f75b05ce6d6c2f8bc
Shawn <44221603+smarshall-spitzbart@users.noreply.github.com>
2023-09-18 11:19:52 -0700

    add action

--- tests/e2e/main.go
+++ tests/e2e/main.go
@@ -244,6 +244,8 @@ func (tr *TestRun) runStep(step Step, verbose bool) {
 		tr.assignConsumerPubKey(action, verbose)
 	case slashMeterReplenishmentAction:
 		tr.waitForSlashMeterReplenishment(action, verbose)
+	case slashPacketRetryAction:
+		tr.waitForSlashRetry(action, verbose)
 	case startRelayerAction:
 		tr.startRelayer(action, verbose)
 	case registerConsumerRewardDenomAction:
