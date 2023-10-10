90f6652ce1584fa4f02dd6c6bc53597b3b86c178
Philip Offtermatt <p.offtermatt@gmail.com>
2023-10-10 16:47:39 +0200

    Run tests before running invariants

--- tests/difference/core/quint_model/run_invariants.sh
+++ tests/difference/core/quint_model/run_invariants.sh
@@ -1,1 +1,4 @@
-quint run --invariant "all{ValidatorUpdatesArePropagatedInv,ValidatorSetHasExistedInv,SameVSCPacketsInv,MatureOnTimeInv,EventuallyMatureOnProviderInv}" ccv_statemachine.qnt --main CCVDefaultStateMachine --max-steps 500 --max-samples 200 --out out.log
+#! /bin/sh
+
+quint test ccv_statemachine.qnt --main CCVDefaultStateMachine
+quint run --invariant "all{ValidatorUpdatesArePropagatedInv,ValidatorSetHasExistedInv,SameVscPacketsInv,MatureOnTimeInv,EventuallyMatureOnProviderInv}" ccv_statemachine.qnt --main CCVDefaultStateMachine --max-steps 500 --max-samples 200
