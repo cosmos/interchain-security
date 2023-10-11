1d99b354f4fbe1a833c562630ccf41ce538ba3e9
Bernd Mueller <bernd@informal.systems>
2023-10-11 17:08:01 +0200

    Fix lint issues

--- x/ccv/consumer/types/events.go
+++ x/ccv/consumer/types/events.go
@@ -9,10 +9,10 @@ const (
 	EventTypeConsumerSlashRequest     = "consumer_slash_request"
 	EventTypeFeeTransferChannelOpened = "fee_transfer_channel_opened"
 
-	//#nosec G101 -- (false positive) this is not a hardcoded credential
 	AttributeDistributionCurrentHeight = "current_distribution_height"
-	AttributeDistributionNextHeight    = "next_distribution_height"
-	AttributeDistributionFraction      = "distribution_fraction"
-	AttributeDistributionTotal         = "total"
-	AttributeDistributionToProvider    = "provider_amount"
+	//#nosec G101 -- (false positive) this is not a hardcoded credential
+	AttributeDistributionNextHeight = "next_distribution_height"
+	AttributeDistributionFraction   = "distribution_fraction"
+	AttributeDistributionTotal      = "total"
+	AttributeDistributionToProvider = "provider_amount"
 )
--- x/ccv/types/codec.go
+++ x/ccv/types/codec.go
@@ -5,14 +5,12 @@ import (
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 )
 
-var (
-	// ModuleCdc references the global x/ibc-transfer module codec. Note, the codec
-	// should ONLY be used in certain instances of tests and for JSON encoding.
-	//
-	// The actual codec used for serialization should be provided to x/ibc transfer and
-	// defined at the application level.
-	ModuleCdc = codec.NewProtoCodec(codectypes.NewInterfaceRegistry())
-)
+// ModuleCdc references the global x/ibc-transfer module codec. Note, the codec
+// should ONLY be used in certain instances of tests and for JSON encoding.
+//
+// The actual codec used for serialization should be provided to x/ibc transfer and
+// defined at the application level.
+var ModuleCdc = codec.NewProtoCodec(codectypes.NewInterfaceRegistry())
 
 func init() {
 }
