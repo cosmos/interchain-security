5675f8c6360b1288fe60ffc84f68c04a5ca9bcec
mpoke <marius.poke@posteo.de>
2023-11-17 10:39:36 +0100

    cleanup state on chain removal

--- x/ccv/provider/keeper/proposal.go
+++ x/ccv/provider/keeper/proposal.go
@@ -167,6 +167,7 @@ func (k Keeper) StopConsumerChain(ctx sdk.Context, chainID string, closeChan boo
 	k.DeleteInitTimeoutTimestamp(ctx, chainID)
 	// Note: this call panics if the key assignment state is invalid
 	k.DeleteKeyAssignments(ctx, chainID)
+	k.DeleteEquivocationEvidenceMinHeight(ctx, chainID)
 
 	// close channel and delete the mappings between chain ID and channel ID
 	if channelID, found := k.GetChainToChannel(ctx, chainID); found {
