12e69e13714902ab95a9f8bb8b3ff7db2706b49c
Philip Offtermatt <p.offtermatt@gmail.com>
2024-03-07 10:54:01 +0100

    Fix model of slashing

--- tests/mbt/run_invariants.sh
+++ tests/mbt/run_invariants.sh
@@ -5,8 +5,8 @@ set -e
 
 quint test ccv_model.qnt
 quint test ccv_test.qnt
-quint run --invariant "all{ValidatorUpdatesArePropagatedInv,ValidatorSetHasExistedInv,SameVscPacketsInv,MatureOnTimeInv,EventuallyMatureOnProviderInv}" ccv_model.qnt --max-steps 200 --max-samples 200
-quint run --invariant "all{ValidatorUpdatesArePropagatedKeyAssignmentInv,ValidatorSetHasExistedKeyAssignmentInv,SameVscPacketsKeyAssignmentInv,MatureOnTimeInv,EventuallyMatureOnProviderInv,KeyAssignmentRulesInv}" ccv_model.qnt --step stepKeyAssignment --max-steps 200 --max-samples 200
+quint run --invariant "all{ValidatorUpdatesArePropagatedInv,ValidatorSetHasExistedInv,SameVscPacketsInv,MatureOnTimeInv,EventuallyMatureOnProviderInv,WaitingForSlashPacketAckInv}" ccv_model.qnt --max-steps 200 --max-samples 200
+quint run --invariant "all{ValidatorUpdatesArePropagatedKeyAssignmentInv,ValidatorSetHasExistedKeyAssignmentInv,SameVscPacketsKeyAssignmentInv,MatureOnTimeInv,EventuallyMatureOnProviderInv,KeyAssignmentRulesInv,WaitingForSlashPacketAckInv}" ccv_model.qnt --step stepKeyAssignment --max-steps 200 --max-samples 200
 
 
 # do not stop on errors anymore, so we can give better output if we error
