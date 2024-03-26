72f5f66b9acac46691daaf1640b2dbc7467191be
Simon Noetzlin <simon.ntz@gmail.com>
2024-03-26 10:14:48 +0100

    feat!: update PSS cli (#1708)
    finalize PSS CLI cmds

--- x/ccv/provider/client/cli/tx.go
+++ x/ccv/provider/client/cli/tx.go
@@ -288,3 +288,48 @@ func NewOptOutCmd() *cobra.Command {
 
 	return cmd
 }
+
+func NewSetConsumerCommissionRate() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "set-consumer-commission-rate [consumer-chain-id] [commission-rate]",
+		Short: "set a per-consumer chain commission",
+		Long: strings.TrimSpace(
+			fmt.Sprintf(`Note that the "commission-rate" argument is a fraction and should be in the range [0,1].
+			Example:
+			%s set-consumer-commission-rate consumer-1 0.5 --from node0 --home ../node0`,
+				version.AppName),
+		),
+		Args: cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			txf, err := tx.NewFactoryCLI(clientCtx, cmd.Flags())
+			if err != nil {
+				return err
+			}
+			txf = txf.WithTxConfig(clientCtx.TxConfig).WithAccountRetriever(clientCtx.AccountRetriever)
+
+			providerValAddr := clientCtx.GetFromAddress()
+
+			commission, err := sdk.NewDecFromStr(args[1])
+			if err != nil {
+				return err
+			}
+			msg := types.NewMsgSetConsumerCommissionRate(args[0], commission, sdk.ValAddress(providerValAddr))
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+
+			return tx.GenerateOrBroadcastTxWithFactory(clientCtx, txf, msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+
+	_ = cmd.MarkFlagRequired(flags.FlagFrom)
+
+	return cmd
+}
