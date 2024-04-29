f3f2e4d2943f4af45ccf3f1a88fa3f21f7fb7508
MSalopek <matija.salopek994@gmail.com>
2024-04-29 21:54:51 +0200

    build: update docs build script

--- docs/build_deploy.sh
+++ docs/build_deploy.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
-# pull in the versions from versions.json
-source ./sync_versions.sh
-
 echo "building docusaurus main docs"
 npm ci && npm run build
 # copy legacy docs to build folder
-git fetch origin/legacy-docs-page
+git fetch origin legacy-docs-page
 git checkout origin/legacy-docs-page -- legacy
-cp -r ../legacy ./build/
+cp -r ./legacy ./build/
 mv build ~/output
 echo "done building docusaurus main docs"
