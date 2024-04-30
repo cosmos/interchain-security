fa188fd63344f86219cda66c17a19c0d2a3edfb2
Philip Offtermatt <p.offtermatt@gmail.com>
2024-04-30 15:08:36 +0200

    Fix migration

---
+++ x/ccv/provider/migrations/v6/migrations.go
@@ -0,0 +1,33 @@
+package v6
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	stakingkeeper "github.com/cosmos/cosmos-sdk/x/staking/keeper"
+	providerkeeper "github.com/cosmos/interchain-security/v4/x/ccv/provider/keeper"
+)
+
+func MigrateTopNForRegisteredChains(ctx sdk.Context, providerKeeper providerkeeper.Keeper, stakingKeeper stakingkeeper.Keeper) {
+	// get all consumer chains
+	registeredConsumerChains := providerKeeper.GetAllRegisteredAndProposedChainIDs(ctx)
+
+	for _, chain := range registeredConsumerChains {
+		// get the top N
+		topN, found := providerKeeper.GetTopN(ctx, chain)
+		if !found {
+			providerKeeper.Logger(ctx).Error("failed to get top N", "chain", chain)
+			continue
+		} else if topN == 0 {
+			providerKeeper.Logger(ctx).Info("top N is 0, not setting minimal power", "chain", chain)
+		} else {
+			// set the minimal power in the top N
+			bondedValidators := stakingKeeper.GetLastValidators(ctx)
+			minPower, err := providerKeeper.ComputeMinPowerInTopN(ctx, bondedValidators, 95)
+			if err != nil {
+				providerKeeper.Logger(ctx).Error("failed to compute min power in top N", "chain", chain, "error", err)
+				continue
+			}
+			providerKeeper.SetMinimumPowerInTopN(ctx, chain, minPower)
+		}
+	}
+}
