b7a274bffd8431c5dd4f949b38b0316b95269823
Philip Offtermatt <p.offtermatt@gmail.com>
2024-04-30 15:08:36 +0200

    Add migration for MinPowerInTopN

--- x/ccv/provider/migrations/vPSS/migrations.go
+++ x/ccv/provider/migrations/vPSS/migrations.go
@@ -3,19 +3,25 @@ package vPSS
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
+	stakingkeeper "github.com/cosmos/cosmos-sdk/x/staking/keeper"
 	providerkeeper "github.com/cosmos/interchain-security/v4/x/ccv/provider/keeper"
 )
 
 // This migration only takes already registered chains into account.
 // If a chain is in voting while the upgrade happens, this is not sufficient,
 // and a migration to rewrite the proposal is needed.
-func MigrateTopNForRegisteredChains(ctx sdk.Context, providerKeeper providerkeeper.Keeper) {
+func MigrateTopNForRegisteredChains(ctx sdk.Context, providerKeeper providerkeeper.Keeper, stakingKeeper stakingkeeper.Keeper) {
 	// get all consumer chains
 	registeredConsumerChains := providerKeeper.GetAllConsumerChains(ctx)
 
-	// Set the topN of each chain to 95
 	for _, chain := range registeredConsumerChains {
+		// Set the topN of each chain to 95
 		providerKeeper.SetTopN(ctx, chain.ChainId, 95)
+
+		// set the minimal power in the top N
+		bondedValidators := stakingKeeper.GetLastValidators(ctx)
+		minPower := providerKeeper.ComputeMinPowerInTopN(ctx, chain.ChainId, bondedValidators, 95)
+		providerKeeper.SetMinimumPowerInTopN(ctx, chain.ChainId, minPower)
 	}
 }
 
