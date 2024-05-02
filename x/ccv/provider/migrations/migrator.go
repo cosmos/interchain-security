fe8dc1ab57ea8f0ccb504ae77f10eee229f3d77a
Philip Offtermatt <p.offtermatt@gmail.com>
2024-05-02 08:49:25 +0200

    Fix migration function name

--- x/ccv/provider/migrations/migrator.go
+++ x/ccv/provider/migrations/migrator.go
@@ -41,9 +41,9 @@ func (m Migrator) Migrate3to4(ctx sdktypes.Context) error {
 	return nil
 }
 
-// MigrateXtoY migrates x/ccvprovider state from consensus version X to Y.
+// Migrate4to5 migrates x/ccvprovider state from consensus version 4 to 5.
 // The migration consists of setting a top N of 95 for all registered consumer chains.
-func (m Migrator) MigrateXtoY(ctx sdktypes.Context) error {
+func (m Migrator) Migrate4to5(ctx sdktypes.Context) error {
 	v5.MigrateTopNForRegisteredChains(ctx, m.providerKeeper)
 	return nil
 }
