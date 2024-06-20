0b234324a7e76188842a47ec8e36a93465d80ad9
mpoke <marius.poke@posteo.de>
2024-06-20 12:01:27 +0200

    add migration for ConsumerAddrsToPrune index

--- x/ccv/provider/migrations/vX/migrations.go
+++ x/ccv/provider/migrations/vX/migrations.go
@@ -2,6 +2,7 @@ package vX
 
 import (
 	"encoding/binary"
+	"time"
 
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -24,18 +25,52 @@ func CompleteUnbondingOps(ctx sdk.Context, store storetypes.KVStore, pk provider
 	}
 }
 
+// MigrateConsumerAddrsToPrune migrates the ConsumerAddrsToPrune index to ConsumerAddrsToPruneV2.
+// Note: This migration must be done before removing the VscSendTimestamp index
+func MigrateConsumerAddrsToPrune(ctx sdk.Context, store storetypes.KVStore, pk providerkeeper.Keeper, sk ccv.StakingKeeper) {
+	iterator := sdk.KVStorePrefixIterator(store, []byte{providertypes.ConsumerAddrsToPruneBytePrefix})
+	defer iterator.Close()
+
+	for ; iterator.Valid(); iterator.Next() {
+		chainID, vscID, err := providertypes.ParseChainIdAndUintIdKey(providertypes.ConsumerAddrsToPruneBytePrefix, iterator.Key())
+		if err != nil {
+			pk.Logger(ctx).Error("ParseChainIdAndUintIdKey failed", "key", string(iterator.Key()))
+		}
+		// use the VscSendTimestamp index to compute the timestamp after which this consumer address can be pruned
+		vscSendTimestampKey := providertypes.ChainIdAndUintIdKey(providertypes.VscSendTimestampBytePrefix, chainID, vscID)
+		sentTime := time.Time{}
+		if timeBz := store.Get(vscSendTimestampKey); timeBz != nil {
+			if ts, err := sdk.ParseTimeBytes(timeBz); err == nil {
+				sentTime = ts
+			}
+		}
+		pruneAfterTs := sentTime.Add(sk.UnbondingTime(ctx))
+
+		var addrs providertypes.AddressList
+		err = addrs.Unmarshal(iterator.Value())
+		if err != nil {
+			pk.Logger(ctx).Error("MigrateConsumerAddrsToPrune failed unmarshaling data from store", "key", string(iterator.Value()))
+		}
+
+		for _, addr := range addrs.Addresses {
+			consumerAddr := providertypes.NewConsumerConsAddress(addr)
+			pk.AppendConsumerAddrsToPruneV2(ctx, chainID, pruneAfterTs, consumerAddr)
+		}
+	}
+}
+
 // CleanupState removes deprecated state
 func CleanupState(ctx sdk.Context, store storetypes.KVStore) error {
-	removePrefix(ctx, store, providertypes.MaturedUnbondingOpsByteKey)
-	removePrefix(ctx, store, providertypes.UnbondingOpBytePrefix)
-	removePrefix(ctx, store, providertypes.UnbondingOpIndexBytePrefix)
-	removePrefix(ctx, store, providertypes.VscSendTimestampBytePrefix)
-	removePrefix(ctx, store, providertypes.VSCMaturedHandledThisBlockBytePrefix)
+	removePrefix(store, providertypes.MaturedUnbondingOpsByteKey)
+	removePrefix(store, providertypes.UnbondingOpBytePrefix)
+	removePrefix(store, providertypes.UnbondingOpIndexBytePrefix)
+	removePrefix(store, providertypes.VscSendTimestampBytePrefix)
+	removePrefix(store, providertypes.VSCMaturedHandledThisBlockBytePrefix)
 
 	return nil
 }
 
-func removePrefix(ctx sdk.Context, store storetypes.KVStore, prefix byte) error {
+func removePrefix(store storetypes.KVStore, prefix byte) error {
 	iterator := sdk.KVStorePrefixIterator(store, []byte{prefix})
 	defer iterator.Close()
 
