af1b7d40b1941b3ecc43042124b55e31161faa6f
Philip Offtermatt <p.offtermatt@gmail.com>
2024-06-27 09:45:14 +0200

    Change wrapped staking to conform to EndBlocker interface

--- x/ccv/no_valupdates_staking/module.go
+++ x/ccv/no_valupdates_staking/module.go
@@ -1,8 +1,10 @@
 package staking
 
 import (
+	"context"
 	"encoding/json"
 
+	"cosmossdk.io/core/appmodule"
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
@@ -18,6 +20,14 @@ var (
 	_ module.AppModule           = AppModule{}
 	_ module.AppModuleBasic      = AppModuleBasic{}
 	_ module.AppModuleSimulation = AppModule{}
+
+	_ module.HasServices     = AppModule{}
+	_ module.HasInvariants   = AppModule{}
+	_ module.HasABCIGenesis  = AppModule{}
+	_ module.HasABCIEndBlock = AppModule{}
+
+	_ appmodule.AppModule       = AppModule{}
+	_ appmodule.HasBeginBlocker = AppModule{}
 )
 
 // AppModule embeds the Cosmos SDK's x/staking AppModuleBasic.
@@ -67,7 +77,7 @@ func (am AppModule) InitGenesis(ctx sdk.Context, cdc codec.JSONCodec, data json.
 
 // EndBlock delegates the EndBlock call to the underlying x/staking module,
 // however, it returns no validator updates as validator updates will be provided by the provider module.
-func (am AppModule) EndBlock(ctx sdk.Context) ([]abci.ValidatorUpdate, error) {
+func (am AppModule) EndBlock(ctx context.Context) ([]abci.ValidatorUpdate, error) {
 	_, err := am.keeper.EndBlocker(ctx)
 	// return the error, but no validator updates
 	return []abci.ValidatorUpdate{}, err
