03af8ad9b18dc0aaf58c6875b872f54b92253c6f
insumity <karolos@informal.systems>
2024-06-28 11:14:48 +0200

    brought one more test from #1732

--- x/ccv/provider/handler_test.go
+++ x/ccv/provider/handler_test.go
@@ -34,6 +34,10 @@ func TestAssignConsensusKeyMsgHandling(t *testing.T) {
 	providerCryptoId := testcrypto.NewCryptoIdentityFromIntSeed(0)
 	providerConsAddr := providerCryptoId.ProviderConsAddress()
 
+	// a different providerConsAddr, to simulate different validators having assigned keys
+	providerCryptoId2 := testcrypto.NewCryptoIdentityFromIntSeed(10)
+	providerConsAddr2 := providerCryptoId2.ProviderConsAddress()
+
 	consumerCryptoId := testcrypto.NewCryptoIdentityFromIntSeed(1)
 	consumerConsAddr := consumerCryptoId.ConsumerConsAddress()
 	consumerKeyBz := base64.StdEncoding.EncodeToString(consumerCryptoId.ConsensusSDKPubKey().Bytes())
@@ -124,7 +128,7 @@ func TestAssignConsensusKeyMsgHandling(t *testing.T) {
 			chainID:  "chainid",
 		},
 		{
-			name: "fail: consumer key in use",
+			name: "fail: consumer key in use by the same validator",
 			setup: func(ctx sdk.Context,
 				k keeper.Keeper, mocks testkeeper.MockedKeepers,
 			) {
@@ -141,6 +145,32 @@ func TestAssignConsensusKeyMsgHandling(t *testing.T) {
 					mocks.MockStakingKeeper.EXPECT().GetValidatorByConsAddr(ctx,
 						consumerConsAddr.ToSdkConsAddr(),
 					).Return(stakingtypes.Validator{}, stakingtypes.ErrNoValidatorFound),
+				)
+			},
+			expError: true,
+			chainID:  "chainid",
+		},
+		{
+			name: "fail: consumer key in use by other validator",
+			setup: func(ctx sdk.Context,
+				k keeper.Keeper, mocks testkeeper.MockedKeepers,
+			) {
+				k.SetPendingConsumerAdditionProp(ctx, &providertypes.ConsumerAdditionProposal{
+					ChainId: "chainid",
+				})
+
+				// Use the consumer key already used by some other validator
+				k.SetValidatorByConsumerAddr(ctx, "chainid", consumerConsAddr, providerConsAddr2)
+
+				gomock.InOrder(
+					mocks.MockStakingKeeper.EXPECT().GetValidator(
+						ctx, providerCryptoId.SDKValOpAddress(),
+						// validator should not be missing
+					).Return(providerCryptoId.SDKStakingValidator(), true).Times(1),
+					mocks.MockStakingKeeper.EXPECT().GetValidatorByConsAddr(ctx,
+						consumerConsAddr.ToSdkConsAddr(),
+						// return false - no other validator uses the consumer key to validate *on the provider*
+					).Return(stakingtypes.Validator{}, false),
 				)
 			},
 			expError: true,
