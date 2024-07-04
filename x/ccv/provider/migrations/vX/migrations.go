b9c408dfbe40d241496115be5dc7927056e58ade
Philip Offtermatt <p.offtermatt@gmail.com>
2024-07-04 08:55:10 +0200

    Add initialization for validator cap

---
+++ x/ccv/provider/migrations/vX/migrations.go
@@ -0,0 +1,16 @@
+package v6
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	providerkeeper "github.com/cosmos/interchain-security/v5/x/ccv/provider/keeper"
+)
+
+// MigrateMaxValidatorsForExistingConsumers initializes the max validators
+// parameter for existing consumers to the MaxProviderConsensusValidators parameter.
+// This is necessary to avoid those consumer chains having an excessive amount of validators.
+func InitializeMaxValidatorsForExistingConsumers(ctx sdk.Context, providerKeeper providerkeeper.Keeper) {
+	maxVals := providerKeeper.GetParams(ctx).MaxProviderConsensusValidators
+	for _, chainID := range providerKeeper.GetAllRegisteredConsumerChainIDs(ctx) {
+		providerKeeper.SetValidatorSetCap(ctx, chainID, uint32(maxVals))
+	}
+}
