e5f982e67957ecf1008aef8edb4e6e6e2594fc7f
Simon Noetzlin <simon.ntz@gmail.com>
2024-07-09 09:06:26 +0200

    Update app/consumer-democracy/proposals_whitelisting.go
    Co-authored-by: bernd-m <43466467+bermuell@users.noreply.github.com>

--- app/consumer-democracy/proposals_whitelisting.go
+++ app/consumer-democracy/proposals_whitelisting.go
@@ -31,8 +31,8 @@ type legacyParamChangeKey struct {
 
 // these parameters don't exist in the consumer app -- keeping them as an example
 var LegacyWhitelistedParams = map[legacyParamChangeKey]struct{}{
-	// add whitlisted legacy parameters here [cosmos-sdk <= 0.47]
-	// commented parameters are just an example - most params have been moved to their respecitve modules
+	// add whitelisted legacy parameters here [cosmos-sdk <= 0.47]
+	// commented parameters are just an example - most params have been moved to their respective modules
 	// and they cannot be changed through legacy governance proposals
 	{Subspace: banktypes.ModuleName, Key: "SendEnabled"}: {},
 }
