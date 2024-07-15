d9d4305bcb8b7e05156b229219600f88c9f6c9f1
Philip Offtermatt <57488781+p-offtermatt@users.noreply.github.com>
2024-07-15 13:26:10 +0200

    Fix inconsistent naming

--- x/ccv/provider/migrations/vX/migrations.go
+++ x/ccv/provider/migrations/vX/migrations.go
@@ -5,7 +5,7 @@ import (
 	providerkeeper "github.com/cosmos/interchain-security/v5/x/ccv/provider/keeper"
 )
 
-// MigrateMaxValidatorsForExistingConsumers initializes the max validators
+// InitializeMaxValidatorsForExistingConsumers initializes the max validators
 // parameter for existing consumers to the MaxProviderConsensusValidators parameter.
 // This is necessary to avoid those consumer chains having an excessive amount of validators.
 func InitializeMaxValidatorsForExistingConsumers(ctx sdk.Context, providerKeeper providerkeeper.Keeper) {
