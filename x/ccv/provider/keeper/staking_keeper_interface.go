d6bcfe0bcf93661b5c230a27395ccdd365c3b07f
Philip Offtermatt <p.offtermatt@gmail.com>
2024-07-18 10:21:01 +0200

    Fix docstrings

--- x/ccv/provider/keeper/staking_keeper_interface.go
+++ x/ccv/provider/keeper/staking_keeper_interface.go
@@ -8,7 +8,7 @@ import (
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 )
 
-// Iterates over the consensus-active validators by power.
+// IterateBondedValidatorsByPower iterates over the consensus-active validators by power.
 // The same as IterateBondedValidatorsByPower in the StakingKeeper,
 // but only returns the first MaxProviderConsensusValidators validators.
 // This is used to implement the interface of the staking keeper to interface with
@@ -25,10 +25,9 @@ func (k Keeper) IterateBondedValidatorsByPower(ctx context.Context, fn func(inde
 	})
 }
 
-// Gets the amount of bonded tokens, which is equal
-// to the amount of tokens of the consensus-active validators.
-// The same as TotalBondedTokens, but only counts
-// tokens of the first MaxProviderConsensusValidators validators.
+// TotalBondedTokens gets the amount of tokens of the consensus-active validators.
+// The same as TotalBondedTokens in the StakingKeeper, but only counts bonded tokens
+// of the first MaxProviderConsensusValidators bonded validators.
 // This is used to implement the interface of the staking keeper to interface with
 // modules that need to reference the consensus-active validators.
 func (k Keeper) TotalBondedTokens(ctx context.Context) (math.Int, error) {
@@ -44,21 +43,21 @@ func (k Keeper) TotalBondedTokens(ctx context.Context) (math.Int, error) {
 	return totalBondedTokens, nil
 }
 
-// The same as IterateDelegations in the StakingKeeper.
+// IterateDelegations is the same as IterateDelegations in the StakingKeeper.
 // Necessary to implement the interface of the staking keeper to interface with
 // other modules.
 func (k Keeper) IterateDelegations(ctx context.Context, delegator sdk.AccAddress, fn func(index int64, delegation stakingtypes.DelegationI) (stop bool)) error {
 	return k.stakingKeeper.IterateDelegations(ctx, delegator, fn)
 }
 
-// The same as StakingTotalSupply in the StakingKeeper.
+// StakingTokenSupply is the same as StakingTotalSupply in the StakingKeeper.
 // Necessary to implement the interface of the staking keeper to interface with
 // other modules.
 func (k Keeper) StakingTokenSupply(ctx context.Context) (math.Int, error) {
 	return k.stakingKeeper.StakingTokenSupply(ctx)
 }
 
-// Gets the ratio of tokens staked to validators active in the consensus
+// BondedRatio gets the ratio of tokens staked to validators active in the consensus
 // to the total supply of tokens.
 // Same as BondedRatio in the StakingKeeper, but only counts
 // tokens of the first MaxProviderConsensusValidators bonded validators.
