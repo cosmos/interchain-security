bb3985a91096453a6b840145c2e090a9879bd8d4
kirdatatjana <116630536+kirdatatjana@users.noreply.github.com>
2024-08-22 09:20:28 +0200

    feat: Implemented consumer validators query including commission rate  (#2162)
    * Implemented validator commission rate query in consumer chain
    
    * Removed unnecessary check
    
    * Removed unused code line

--- x/ccv/provider/keeper/grpc_query.go
+++ x/ccv/provider/keeper/grpc_query.go
@@ -8,6 +8,7 @@ import (
 	"google.golang.org/grpc/status"
 
 	errorsmod "cosmossdk.io/errors"
+	"cosmossdk.io/math"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
@@ -330,14 +331,33 @@ func (k Keeper) QueryConsumerValidators(goCtx context.Context, req *types.QueryC
 	if err != nil {
 		return nil, status.Error(codes.Internal, err.Error())
 	}
+
 	for _, v := range consumerValSet {
+
+		consAddr, err := sdk.ConsAddressFromBech32(sdk.ConsAddress(v.ProviderConsAddr).String())
+		if err != nil {
+			return nil, status.Error(codes.InvalidArgument, "invalid provider address")
+		}
+
+		var rate math.LegacyDec
+		consumerRate, found := k.GetConsumerCommissionRate(ctx, consumerChainID, types.NewProviderConsAddress(consAddr))
+		if found {
+			rate = consumerRate
+		} else {
+			v, err := k.stakingKeeper.GetValidatorByConsAddr(ctx, consAddr)
+			if err != nil {
+				return nil, status.Error(codes.InvalidArgument, fmt.Sprintf("unknown validator: %s", consAddr.String()))
+			}
+			rate = v.Commission.Rate
+		}
+
 		validators = append(validators, &types.QueryConsumerValidatorsValidator{
 			ProviderAddress: sdk.ConsAddress(v.ProviderConsAddr).String(),
 			ConsumerKey:     v.PublicKey,
 			Power:           v.Power,
+			Rate:            rate,
 		})
 	}
-
 	return &types.QueryConsumerValidatorsResponse{
 		Validators: validators,
 	}, nil
