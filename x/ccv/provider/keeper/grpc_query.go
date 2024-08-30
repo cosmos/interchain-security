bbbb2bf80d2ac5796ea7d8d9483f989d821ab064
Bernd Mueller <bernd@informal.systems>
2024-08-30 13:05:37 +0200

    addressed comments

--- x/ccv/provider/keeper/grpc_query.go
+++ x/ccv/provider/keeper/grpc_query.go
@@ -137,10 +137,7 @@ func (k Keeper) GetConsumerChain(ctx sdk.Context, consumerId string) (types.Chai
 		strDenylist[i] = addr.String()
 	}
 
-	metadata, err := k.GetConsumerMetadata(ctx, consumerId)
-	if err != nil {
-		k.Logger(ctx).Error("cannot get metadata for consumer (%s): %w", consumerId, err)
-	}
+	metadata, _ := k.GetConsumerMetadata(ctx, consumerId)
 
 	return types.Chain{
 		ChainId:            chainID,
