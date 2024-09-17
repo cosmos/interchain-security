66e209cbbb33ff85cd28b764aa55af07c9d22714
Marius Poke <marius.poke@posteo.de>
2024-09-17 12:48:00 +0200

    refactor:  change byte prefixing scheme for consumer (#2148)
    * update consumer keys prefixing scheme
    
    * make changes consistent
    
    * mark PendingDataPackets key as v1
    
    * apply review suggestions
    
    * fix import

--- x/ccv/consumer/migrations/v2/migration_test.go
+++ x/ccv/consumer/migrations/v2/migration_test.go
@@ -83,5 +83,5 @@ func setPendingPackets(ctx sdk.Context, store storetypes.KVStore, packets consum
 	if err != nil {
 		panic(fmt.Errorf("failed to marshal ConsumerPacketDataList: %w", err))
 	}
-	store.Set([]byte{consumertypes.PendingDataPacketsBytePrefix}, bz)
+	store.Set(consumertypes.PendingDataPacketsV1KeyPrefix(), bz)
 }
