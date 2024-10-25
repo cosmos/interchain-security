a49866c13fc8e06cc561dfd3437b89bfa9783590
kirdatatjana <kirda.tatjana@gmail.com>
2024-10-25 15:39:49 +0200

    Revert changes made by the merge

--- x/ccv/provider/client/cli/tx.go
+++ x/ccv/provider/client/cli/tx.go
@@ -251,11 +251,11 @@ where create_consumer.json has the following structure:
     "top_N": 0,
     "validators_power_cap": 10,
     "validator_set_cap": 0,
-    "allowlist": [],
-    "denylist": [],
-    "min_stake": "0",
+    "allowlist": ["cosmosvalcons..."],
+    "denylist": ["cosmosvalcons..."],
+    "min_stake": 0,
     "allow_inactive_vals": false,
-    "prioritylist": []
+    "prioritylist": ["cosmosvalcons..."]
   },
   "allowlisted_reward_denoms": {
     "denoms": ["ibc/...", "ibc/..."]
@@ -350,11 +350,11 @@ where update_consumer.json has the following structure:
     "top_N": 0,
     "validators_power_cap": 10,
     "validator_set_cap": 0,
-    "allowlist": [],
-    "denylist": [],
-    "min_stake": "0",
+    "allowlist": ["cosmosvalcons..."],
+    "denylist": ["cosmosvalcons..."],
+    "min_stake": 0,
     "allow_inactive_vals": false,
-    "prioritylist": []
+    "prioritylist": ["cosmosvalcons..."]
    },
   "allowlisted_reward_denoms": {
     "denoms": ["ibc/...", "ibc/..."]
