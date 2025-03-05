78372f4b6b66b73f96f10883c903ba28e00e14e0
leopardracer <136604165+leopardracer@users.noreply.github.com>
2025-03-05 10:24:02 +0200

    Update app.go

--- app/consumer/app.go
+++ app/consumer/app.go
@@ -372,6 +372,7 @@ func New(
 		app.GetSubspace(ibctransfertypes.ModuleName),
 		app.IBCKeeper.ChannelKeeper,
 		app.IBCKeeper.ChannelKeeper,
+		app.IBCKeeper.ChannelKeeperV2
 		app.MsgServiceRouter(),
 		app.AccountKeeper,
 		app.BankKeeper,
