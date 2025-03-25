7e4c0dd0905ea4ec371c545a51664838d11d7149
leopardracer <136604165+leopardracer@users.noreply.github.com>
2025-03-25 09:40:53 +0200

    Update app.go

--- app/consumer/app.go
+++ app/consumer/app.go
@@ -371,7 +371,6 @@ func New(
 		runtime.NewKVStoreService(keys[ibctransfertypes.StoreKey]),
 		app.GetSubspace(ibctransfertypes.ModuleName),
 		app.IBCKeeper.ChannelKeeper,
-		app.IBCKeeper.ChannelKeeper,
 		app.IBCKeeper.ChannelKeeperV2
 		app.MsgServiceRouter(),
 		app.AccountKeeper,
