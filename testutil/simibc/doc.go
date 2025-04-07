25887a571b4b12b87a92416034f04302cd9d829e
Tomás Andróil <tomasandroil@gmail.com>
2025-04-07 17:43:38 +0200

    Update doc.go

--- testutil/simibc/doc.go
+++ testutil/simibc/doc.go
@@ -1,4 +1,4 @@
 /*
-simibc is a collection of utilities wrapping the ibc-go testing framework which make is easier to write test scenarios involving precise orders of packet and ack delivery and calls to BeginBlock and EndBlock.
+simibc is a collection of utilities wrapping the ibc-go testing framework which make it easier to write test scenarios involving precise orders of packet and ack delivery and calls to BeginBlock and EndBlock.
 */
 package simibc
